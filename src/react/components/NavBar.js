47ab3013bf7d240b8b384933de1857aa1f943043
Olli-Pekka Pohjola <olli-pekka.pohjola@houston-inc.com>
2019-12-07 21:47:42 +0200

    Set navbar specific lens data in navbar hook

--- src/react/components/NavBar.js
+++ src/react/components/NavBar.js
@@ -1,45 +1,61 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import { Pane, IconButton, SearchInput, Tablist, Tab, Icon } from 'evergreen-ui'
+
+import { useLenses } from './lenses'
+
+const useNavBarLenses = () => {
+  const {
+    search,
+    selectLens,
+    ...lenses
+  } = useLenses()
+
+  const { id, key, text, image } = search
+  return {
+    lenses,
+    search: {
+      id,
+      key,
+      text,
+      image,
+      placeholder: 'Search diories...',
+      onFocus: () => selectLens(id),
+    },
+  }
+}
 
 const NavBar = ({
   back,
   forward,
-  lenses: { search, ...lenses } = {},
-  create
-}) => (
-  <Pane display="flex" justifyContent="space-between" padding={8} background="tint2">
-    <Pane display="flex" alignSelf="center">
-      <IconButton appearance="minimal" icon="arrow-left" {...back}/>
-      <IconButton appearance="minimal" icon="arrow-right" {...forward}/>
+  create,
+}) => {
+  const { search, lenses } = useNavBarLenses()
+  return (
+    <Pane display="flex" justifyContent="space-between" padding={8} background="tint2">
+      <Pane display="flex" alignSelf="center">
+        <IconButton appearance="minimal" icon="arrow-left" {...back}/>
+        <IconButton appearance="minimal" icon="arrow-right" {...forward}/>
+      </Pane>
+      <Pane display="flex">
+        <SearchInput marginRight={16} width={150} {...search}/>
+        <Tablist alignSelf="center">
+          {Object.values(lenses).map(({diory, ...lens}) => (
+            <Tab
+              {...lens}
+              alignSelf="center"
+              aria-controls={`panel-${lens.id}`}
+            >
+              <Icon icon={diory.image} marginRight={4}/>
+              {diory.text}
+            </Tab>
+          ))}
+        </Tablist>
+      </Pane>
+      <Pane>
+        <IconButton appearance="minimal" icon="plus" {...create}/>
+      </Pane>
     </Pane>
-    <Pane display="flex">
-      <SearchInput marginRight={16} width={150} {...search}/>
-      <Tablist alignSelf="center">
-        {Object.values(lenses).map(({ diory, ...lens }) => (
-          <Tab
-            {...lens}
-            key={diory.id}
-            alignSelf="center"
-            aria-controls={`panel-${diory.id}`}
-          >
-            <Icon icon={diory.data.icon} marginRight={4}/>
-            {diory.text}
-          </Tab>
-        ))}
-      </Tablist>
-    </Pane>
-    <Pane>
-      <IconButton appearance="minimal" icon="plus" {...create}/>
-    </Pane>
-  </Pane>
-)
-
-NavBar.defaultProps = {}
-
-NavBar.propTypes = {
-  lenses: PropTypes.array,
-  search: PropTypes.object,
+  )
 }
 
 export default NavBar
