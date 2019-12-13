38bbe8bd22d036648ccb5be61362897a3f394fa6
Olli-Pekka Pohjola <olli-pekka.pohjola@houston-inc.com>
2019-12-13 23:56:10 +0200

    Add util scripts

---
+++ src/react/scripts/flattenDiograph.js
@@ -0,0 +1,42 @@
+import uuid from 'uuid'
+
+const getData = (diorys) => Object.entries(diorys).reduce((obj, [key, diory]) => {
+  const id = diory.id || uuid()
+  return {
+    ...obj,
+    [key]: { id, ...diory }
+  }
+}, {})
+
+const getLinks = (diorys) => Object.entries(diorys).reduce((obj, [key, { id }]) => ({
+    ...obj,
+    [key]: { id }
+  }), {})
+
+const getDiorys = (diorys) => Object.values(diorys).reduce((obj, diory) => ({
+  ...obj,
+  [diory.id]: diory
+}), {})
+
+const flattenDiograph = (diograph) => {
+  const results = Object.entries(diograph).reduce((obj, [key, { diorys, ...diory }]) => {
+    if (diorys) {
+      const data = getData(diorys)
+      const links = getLinks(data)
+      const linkedDiorys = getDiorys(data)
+      return {
+        ...obj,
+        ...linkedDiorys,
+        [key]: {...diory, links},
+      }
+    }
+    return {
+      ...obj,
+      [diory.id]: diory,
+    }
+  }, {})
+
+  console.log(JSON.stringify(results))
+}
+
+export default flattenDiograph
---
+++ src/react/scripts/index.js
@@ -0,0 +1,4 @@
+// import flattenDiograph from '../scripts/flatten-diograph'
+// flattenDiograph(diograph)
+// import geoFromDataToDiory from '../scripts/geoFromDataToDiory'
+// geoFromDataToDiory(diograph)
