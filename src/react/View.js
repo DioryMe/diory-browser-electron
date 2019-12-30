ce2cec42a2ef5c7af3581179c1267cac00e62039
Olli-Pekka Pohjola <olli-pekka.pohjola@houston-inc.com>
2019-12-30 12:18:22 +0200

    Add <FullScreenView/>

--- src/react/View.js
+++ src/react/View.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useStore } from './store'
 
+import FullScreenView from './components/FullScreenView'
 import SelectedLens from './features/lenses/SelectedLens'
 import Room from './features/room/Room'
 import HomeView from './features/home/HomeView'
@@ -10,7 +11,11 @@ const View = () => {
   const [{ room }] = useStore(state => state.navigation)
 
   if (selectedLensId) {
-    return <SelectedLens/>
+    return (
+      <FullScreenView marginTop={48}>
+        <SelectedLens/>
+      </FullScreenView>
+    )
   }
 
   if (room) {
