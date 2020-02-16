2d14e460996a5ee472747d6ba1d7c47336b2484d
Jouni Alanen <jvalanen@gmail.com>
2020-02-16 18:53:44 +0200

    Switch to alternative FIXME-version

--- src/react/features/home/hooks.js
+++ src/react/features/home/hooks.js
@@ -8,15 +8,15 @@ export const useHomeChannel = () => {
   const dispatch = useStore()[1]
   useEffect(() => {
     // FIXME: For some reason this doesn't work...
-    // fetchData(channels.GET_HOME).then(({ rooms }) => {
-    //   dispatch(setRooms(rooms))
-    // })
-    // ...but with this tests pass but localhost:3300 doesn't work...
-    ipcRenderer.send(channels.GET_HOME)
-    ipcRenderer.on(channels.GET_HOME, (event, { rooms }) =>
+    fetchData(channels.GET_HOME).then(({ rooms }) => {
       dispatch(setRooms(rooms))
-    )
-    return () => ipcRenderer.removeAllListeners(channels.HOME)
+    })
+    // ...but with this cucumber tests pass but build or localhost:3300 doesn't work...
+    // ipcRenderer.send(channels.GET_HOME)
+    // ipcRenderer.on(channels.GET_HOME, (event, { rooms }) =>
+    //   dispatch(setRooms(rooms))
+    // )
+    // return () => ipcRenderer.removeAllListeners(channels.HOME)
     // end of FIXME
   }, [dispatch])
 }
