d8b954232d0ffc5f5946c78d1ff8f03e67560d8b
Olli-Pekka Pohjola <olli-pekka.pohjola@houston-inc.com>
2020-03-09 23:39:04 +0200

    DOC: Refactor create room to save room in room generation

--- electron/channels/create-room-channel.js
+++ electron/channels/create-room-channel.js
@@ -1,20 +1,11 @@
 const { ipcMain } = require('electron')
 const { channels } = require('../../src/shared/constants')
-const { getRoom, saveRoom } = require('../lib/room-util')
 const FolderTools = require('../lib/diograph-folder-tools')
 
 ipcMain.on(channels.CREATE_ROOM, (event, filePath) => {
   // DEFAULT PATH IS EXAMPLE-FOLDER PATH
   // filePath = path.join(__dirname, 'spec/example-folder')
   FolderTools.generateRoom(filePath).then(room => {
-    getRoom(room.id).then(retrievedRoom => {
-      saveRoom(retrievedRoom.id, retrievedRoom.diograph, err => {
-        if(err) {
-          console.log(err)
-          return event.sender.send(channels.CREATE_ROOM, null, err);
-        }
-        event.reply(channels.CREATE_ROOM, room)
-      })
-    })
+    event.reply(channels.CREATE_ROOM, room)
   })
 })
--- electron/channels/save-room-channel.js
+++ electron/channels/save-room-channel.js
@@ -3,11 +3,12 @@ const { channels } = require('../../src/shared/constants')
 const { saveRoom } = require('../lib/room-util')
 
 ipcMain.on(channels.SAVE_ROOM, (event, { id, diograph }) => {
-  saveRoom(id, diograph, err => {
-    if(err) {
+  saveRoom(id, diograph)
+    .then(() => {
+      event.sender.send(channels.SAVE_ROOM, true)
+    })
+    .catch((err) => {
       console.log(err)
       return event.sender.send(channels.SAVE_ROOM, null, err);
-    }
-    event.sender.send(channels.SAVE_ROOM, true);
-  })
+    })
 });
