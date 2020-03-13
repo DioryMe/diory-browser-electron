aba8eceb99132fbf271e7b65d3deea251f7eb758
Olli-Pekka Pohjola <olli-pekka.pohjola@houston-inc.com>
2020-03-14 01:10:26 +0200

    TECH: Add logging to channels

--- electron/channels/add-room-channel.js
+++ electron/channels/add-room-channel.js
@@ -2,10 +2,13 @@ const { ipcMain } = require('electron')
 const { channels } = require('../../src/shared/constants')
 const FolderTools = require('../lib/diograph-folder-tools')
 
-ipcMain.on(channels.CREATE_ROOM, (event, filePath) => {
+ipcMain.on(channels.ADD_ROOM, (event, path) => {
   // DEFAULT PATH IS EXAMPLE-FOLDER PATH
   // filePath = path.join(__dirname, 'spec/example-folder')
-  FolderTools.generateRoom(filePath).then(room => {
-    event.reply(channels.CREATE_ROOM, room)
+  console.log('ADD_ROOM', path)
+  FolderTools.generateRoom(path).then(room => {
+    console.log(room)
+    console.log('--------')
+    event.reply(channels.ADD_ROOM, room)
   })
 })
