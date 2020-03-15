2a722a610775019d67cf7b0ca14c302d4c612366
Olli-Pekka Pohjola <olli-pekka.pohjola@houston-inc.com>
2020-03-15 20:20:25 +0200

    TECH: Refactor navigation components

--- src/react/features/navigation/Navigation.js
+++ src/react/features/navigation/Navigation.js
@@ -1,28 +1,14 @@
 import React from 'react'
-import { Pane } from 'evergreen-ui'
-import NavigationButtons from './components/NavigationButtons'
-import NavigationLenses from './components/NavigationLenses'
-import NavigationSearch from './components/NavigationSearch'
+import NavigationBar from './components/NavigationBar'
 import NavigationToLeft from './components/NavigationToLeft'
 import NavigationToRight from './components/NavigationToRight'
 
-const Navigation = () => {
-  return (
-    <>
-      <Pane
-        display="flex"
-        justifyContent="space-between"
-        padding={8}
-        background="tint2"
-      >
-        <NavigationButtons display="flex" alignSelf="center" />
-        <NavigationLenses display="flex" />
-        <NavigationSearch />
-      </Pane>
-      <NavigationToLeft />
-      <NavigationToRight />
-    </>
-  )
-}
+const Navigation = () => (
+  <>
+    <NavigationBar />
+    <NavigationToLeft />
+    <NavigationToRight />
+  </>
+)
 
 export default Navigation
