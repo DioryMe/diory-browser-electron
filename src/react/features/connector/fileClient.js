69d57a82744cecd9788b729195f9fe653d8fd313
Olli-Pekka Pohjola <olli-pekka.pohjola@houston-inc.com>
2020-03-25 23:37:29 +0200

    TECH: Use channel mock responses

--- src/react/features/connector/fileClient.js
+++ src/react/features/connector/fileClient.js
@@ -1,8 +1,8 @@
 export const fetchFile = channel => {
   // TODO: Find good place for static data
   const data = {
-    GET_HOME: require('./mockResponses/home.json'),
-    GET_ROOM: require('./mockResponses/diograph.json'),
+    GET_HOME: require('./mockResponses/GET_HOME.json'),
+    GET_ROOM: require('./mockResponses/GET_ROOM.json'),
   }[channel]
 
   if (!data) {
