770be31bee5e648cea53ac558b8d0fb2ec002ef0
Olli-Pekka Pohjola <olli-pekka.pohjola@houston-inc.com>
2020-04-02 00:47:32 +0300

    DDA-6: Save diograph after generation

--- electron/channels/add-room-channel.js
+++ electron/channels/add-room-channel.js
@@ -1,14 +1,20 @@
 const { ipcMain } = require('electron')
 const { channels } = require('../../src/shared/constants')
-const FolderTools = require('../lib/diograph-folder-tools')
+const { generateDiograph } = require('../generators/diograph-generator')
+const { saveRoom } = require('../lib/room-util')
 
 ipcMain.on(channels.ADD_ROOM, (event, path) => {
-  // DEFAULT PATH IS EXAMPLE-FOLDER PATH
-  // filePath = path.join(__dirname, 'spec/example-folder')
   console.log('ADD_ROOM', path)
-  FolderTools.generateRoom(path).then(room => {
-    console.log(room)
+  generateDiograph(path).then(({ id, diograph }) => {
+    console.log(id, diograph)
     console.log('--------')
-    event.reply(channels.ADD_ROOM, room)
+    saveRoom(path, diograph)
+      .then(() => {
+        event.reply(channels.ADD_ROOM, diograph[id])
+      })
+      .catch(err => {
+        console.log(err)
+        return event.sender.send(channels.ADD_ROOM, null, err)
+      })
   })
 })
