021038da4418e90a0726174f37b6283bbc532dfc
Olli-Pekka Pohjola <olli-pekka.pohjola@houston-inc.com>
2020-04-08 22:51:48 +0300

    TECH: Refactor map hooks and tools

---
+++ src/react/features/lenses/map/hooks/useTogglePopup.js
@@ -0,0 +1,17 @@
+import { useEffect } from 'react'
+import { useDispatch, useStore } from '../../../../store'
+
+export const useTogglePopup = mapRef => {
+  const [{ focus }] = useStore(state => state.navigation)
+  const [{ active }] = useStore(state => state.tools)
+  const dispatch = useDispatch()
+  useEffect(() => {
+    mapRef.current.eachLayer(marker => {
+      if (marker.dioryId) {
+        marker.on('click', () => {
+          marker.togglePopup()
+        })
+      }
+    })
+  }, [mapRef, focus, active, dispatch])
+}
