7798ac4887af1f5ad150df612e46559e226d2fe9
Olli-Pekka Pohjola <olli-pekka.pohjola@houston-inc.com>
2020-04-12 22:38:25 +0300

    TECH: Remove save home as placeholder

--- src/react/features/connector/Connector.js
+++ src/react/features/connector/Connector.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import { useGetHome } from './hooks/useGetHome'
 import { useGetRoom } from './hooks/useGetRoom'
-import { useSaveHome } from './hooks/useSaveHome'
 import { useSaveRoom } from './hooks/useSaveRoom'
 
 import { useAddRoom } from './tools/useAddRoom'
 
 const Connector = () => {
   useGetHome()
-  useSaveHome()
   useGetRoom()
   useSaveRoom()
 
