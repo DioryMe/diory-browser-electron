81441598a1e2250aea2c7fed393719498f249045
Olli-Pekka Pohjola <olli-pekka.pohjola@houston-inc.com>
2020-04-16 22:53:34 +0300

    DDA-21: Remove home from navigation

--- src/react/View.js
+++ src/react/View.js
@@ -4,15 +4,9 @@ import { useStore } from './store'
 import FullScreenView from './components/FullScreenView'
 import SelectedLens from './features/lenses/SelectedLens'
 import RoomView from './features/room/RoomView'
-import HomeView from './features/home/HomeView'
 
 const View = () => {
   const [{ selectedLensId }] = useStore((state) => state.lenses)
-  const [{ roomId }] = useStore((state) => state.navigation)
-
-  if (!roomId) {
-    return <HomeView />
-  }
 
   if (selectedLensId) {
     return (
