fd802e180cc63a0b3200416726a6f8fdb866a7e5
Jouni Alanen <jvalanen@gmail.com>
2020-07-03 21:56:14 +0300

    DDA-39: Add --acl public-read

--- package-mac.sh
+++ package-mac.sh
@@ -9,8 +9,7 @@ echo "Installing dependencies and building the app..."
 yarn && yarn build && yarn build-electron
 echo "Create MacOS distribution package..."
 yarn package-mac
-# rm -r ~/temp-package-mac/diory-browser-electron/*
+# aws s3 cp dist/*.dmg s3://dda-downloads --acl public-read
+# cd ~
 # rm -r ~/temp-package-mac
 echo "All this was made with: yarn: $(yarn --version), node: $(node --version)"
-
-# aws --profile diory-electron s3 cp dist/*.dmg s3://jvalanen-codebuild-test
