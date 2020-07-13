e0aa787d24d38d1c3a86ca24f3a696350e924146
Olli-Pekka Pohjola <olli-pekka.pohjola@houston-inc.com>
2020-07-13 23:05:31 +0300

    DDA-45: Refactor views

--- src/react/View.js
+++ src/react/View.js
@@ -1,22 +1,21 @@
 import React from 'react'
 import { useStore } from './store'
 
-import FullScreenView from './components/FullScreenView'
-import SelectedLens from './features/lenses/SelectedLens'
+import LensView from './features/lenses/LensView'
 import RoomView from './features/room/RoomView'
 
 const View = () => {
   const [{ selectedLensId }] = useStore((state) => state.lenses)
 
   if (selectedLensId) {
-    return (
-      <FullScreenView marginTop={48}>
-        <SelectedLens />
-      </FullScreenView>
-    )
+    return <LensView />
   }
 
-  return <RoomView />
+  return (
+    <>
+      <RoomView />
+    </>
+  )
 }
 
 export default View
