8268d68bb6284cab633a481c3689f4c99390da7d
Olli-Pekka Pohjola <olli-pekka.pohjola@houston-inc.com>
2020-07-13 23:05:31 +0300

    DDA-45: Refactor views

--- src/react/components/Fullscreen.js
+++ src/react/components/Fullscreen.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import Box from 'ui-box'
 
-const FullScreenView = ({ children, ...props }) => (
+const Fullscreen = ({ children, ...props }) => (
   <Box position="absolute" top={0} right={0} bottom={0} left={0} children={children} {...props} />
 )
 
-export default FullScreenView
+export default Fullscreen
