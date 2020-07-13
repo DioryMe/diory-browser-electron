9c03d8e2cabce6c01f33331b8f87db896e5731ae
Olli-Pekka Pohjola <olli-pekka.pohjola@houston-inc.com>
2020-07-13 22:11:17 +0300

    DDA-45: Create fullscreen button to navigation

--- src/react/features/navigation/Navigation.js
+++ src/react/features/navigation/Navigation.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import NavigationBar from './components/NavigationBar'
+import NavigationFullscreen from './components/NavigationFullscreen'
 import NavigationToLeft from './components/NavigationToLeft'
 import NavigationToRight from './components/NavigationToRight'
 
 const Navigation = () => (
   <>
     <NavigationBar />
+    <NavigationFullscreen />
     <NavigationToLeft />
     <NavigationToRight />
   </>
---
+++ src/react/features/navigation/components/NavigationFullscreen.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import { Icon } from 'evergreen-ui'
+import { useDispatch, useStore } from '../../../store'
+import { setFullscreen } from '../actions'
+
+const useFullscreen = () => {
+  const dispatch = useDispatch()
+  const [{ fullscreen }] = useStore((state) => state.navigation)
+  const icon = !fullscreen
+    ? {
+        icon: 'fullscreen',
+        size: 24,
+        style: {
+          margin: '12px',
+        },
+      }
+    : {
+        icon: 'cross',
+        size: 36,
+        style: {
+          margin: '6px',
+        },
+      }
+  return {
+    icon,
+    onClick: () => dispatch(setFullscreen(!fullscreen)),
+  }
+}
+
+const NavigationFullscreen = () => {
+  const { icon, onClick } = useFullscreen()
+  return (
+    <div
+      data-testid="navigation-fullscreen"
+      onClick={onClick}
+      style={{
+        position: 'fixed',
+        zIndex: 1000,
+        top: 48,
+        right: 0,
+        height: 50,
+        width: 50,
+        cursor: 'pointer',
+      }}
+    >
+      <Icon {...icon} color="disabled" />
+    </div>
+  )
+}
+
+export default NavigationFullscreen
