353de2f43a9d637c49ed0dfb3c2a7f777b73ce8f
Olli-Pekka Pohjola <olli-pekka.pohjola@houston-inc.com>
2020-07-13 23:06:11 +0300

    DDA-45: Add fullscreen view

--- src/react/View.js
+++ src/react/View.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import { useStore } from './store'
 
+import FullscreenView from './FullscreenView'
 import LensView from './features/lenses/LensView'
 import RoomView from './features/room/RoomView'
 
 const View = () => {
+  const [{ fullscreen }] = useStore((state) => state.navigation)
   const [{ selectedLensId }] = useStore((state) => state.lenses)
+
+  if (fullscreen) {
+    return <FullscreenView />
+  }
 
   if (selectedLensId) {
     return <LensView />
