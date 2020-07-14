bf7e62deb8e82e7a7dacced0fc4835cfb2470d3b
Olli-Pekka Pohjola <olli-pekka.pohjola@houston-inc.com>
2020-07-14 22:48:50 +0300

    DDA-46: Create text input component

---
+++ src/react/components/TextInput.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import { TextInputField } from 'evergreen-ui'
+
+const TextInput = ({ onChange, ...props }) => (
+  <TextInputField onChange={({ target: { value } }) => onChange(value)} {...props} />
+)
+
+TextInput.defaultProps = {
+  isInvalid: false,
+  required: false,
+}
+
+TextInput.propTypes = {
+  isInvalid: PropTypes.bool,
+  required: PropTypes.bool,
+  label: PropTypes.string,
+  description: PropTypes.string,
+  placeholder: PropTypes.string,
+  hint: PropTypes.string,
+  validationMessage: PropTypes.string,
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+}
+
+export default TextInput
