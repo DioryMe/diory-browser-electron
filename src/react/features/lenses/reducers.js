adae6a0b1a5437b791799c1913c13aefa6cbc567
Olli-Pekka Pohjola <olli-pekka.pohjola@houston-inc.com>
2020-07-14 10:14:54 +0300

    TECH: Change room view to grid lens

--- src/react/features/lenses/reducers.js
+++ src/react/features/lenses/reducers.js
@@ -1,14 +1,16 @@
 import { SELECT_LENS } from './actionsTypes'
 import { createReducer } from '../../store'
 
+const DEFAULT_LENS = 'grid'
+
 export const initialState = {
-  selectedLensId: undefined,
+  selectedLensId: DEFAULT_LENS,
   lenses: [],
 }
 
 export const selectLens = (state, { payload }) => ({
   ...state,
-  selectedLensId: payload.id,
+  selectedLensId: payload.id || DEFAULT_LENS,
 })
 
 export default createReducer({
