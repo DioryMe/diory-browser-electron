fe912907fa53a9d7ebebd0708333a49ff0059d4e
Olli-Pekka Pohjola <olli-pekka.pohjola@houston-inc.com>
2020-07-14 09:36:47 +0300

    DDA-45: Show fullscreen on lens

--- src/react/FullscreenView.js
+++ src/react/FullscreenView.js
@@ -1,17 +1,19 @@
 import React from 'react'
-
+import { useStore } from './store'
 import { useFocusDiory } from './features/room/hooks'
 
 import Fullscreen from './components/Fullscreen'
 import Diory from './components/diories/Diory'
 
 const FullscreenView = () => {
+  const [{ fullscreen }] = useStore((state) => state.navigation)
   const { diory } = useFocusDiory()
-  return (
-    <Fullscreen top={48}>
+
+  return fullscreen ? (
+    <Fullscreen top={48} zIndex={1000}>
       <Diory diory={diory} height={'100%'} />
     </Fullscreen>
-  )
+  ) : null
 }
 
 export default FullscreenView
