5b6729740d21d87257c3e706b6a56f4e74ae8d08
Olli-Pekka Pohjola <olli-pekka.pohjola@houston-inc.com>
2020-07-25 20:56:43 +0300

    DDA-47: Move buttons open state to store

--- src/react/features/buttons/actions.js
+++ src/react/features/buttons/actions.js
@@ -1,4 +1,4 @@
-import { ADD_BUTTONS, REMOVE_BUTTONS } from './actionsTypes'
+import { ADD_BUTTONS, REMOVE_BUTTONS, SET_OPEN } from './actionsTypes'
 
 export const addButtons = (buttons) => ({
   type: ADD_BUTTONS,
@@ -8,4 +8,9 @@ export const addButtons = (buttons) => ({
 export const removeButtons = (buttons) => ({
   type: REMOVE_BUTTONS,
   payload: { buttons },
+})
+
+export const setOpen = (open) => ({
+  type: SET_OPEN,
+  payload: { open },
 })
--- src/react/features/buttons/actionsTypes.js
+++ src/react/features/buttons/actionsTypes.js
@@ -1,2 +1,3 @@
 export const ADD_BUTTONS = 'ADD_BUTTONS'
 export const REMOVE_BUTTONS = 'REMOVE_BUTTONS'
+export const SET_OPEN = 'SET_OPEN'
