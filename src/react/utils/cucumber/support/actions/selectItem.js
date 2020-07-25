522ffc0e3668bf9ee86754bc47a1a1b9061fc4aa
Olli-Pekka Pohjola <olli-pekka.pohjola@houston-inc.com>
2020-07-25 20:57:13 +0300

    FIX: Feature tests

--- src/react/utils/cucumber/support/actions/selectItem.js
+++ src/react/utils/cucumber/support/actions/selectItem.js
@@ -1,5 +1,6 @@
 export function selectItem(arg1, arg2) {
   const testid = [arg1, arg2].join('-')
+  this.app.update()
   this.app.find(`[data-testid="${testid}"]`).last().simulate('click')
   this.app.update()
 }
