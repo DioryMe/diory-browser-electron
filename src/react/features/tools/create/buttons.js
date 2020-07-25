a2ba91b9498258f9abca15ce5bf3cafadf9346e6
Olli-Pekka Pohjola <olli-pekka.pohjola@houston-inc.com>
2020-07-25 23:59:03 +0300

    DDA-53: Create create tool

---
+++ src/react/features/tools/create/buttons.js
@@ -0,0 +1,12 @@
+export const CREATE_TOOL_BUTTON = 'CREATE_TOOL_BUTTON'
+
+export default [
+  {
+    id: CREATE_TOOL_BUTTON,
+    text: 'Create',
+    data: {
+      icon: 'plus',
+      testid: 'create',
+    },
+  },
+]
