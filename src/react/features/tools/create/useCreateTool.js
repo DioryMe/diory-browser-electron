cea312b050fa5c5c6c29fde3c0a7356552d22e9c
Olli-Pekka Pohjola <olli-pekka.pohjola@houston-inc.com>
2020-07-27 22:38:28 +0300

    TECH: Refactor create and update tools for clarity

--- src/react/features/tools/create/useCreateTool.js
+++ src/react/features/tools/create/useCreateTool.js
@@ -1,21 +1,22 @@
 import { v4 as uuid } from 'uuid'
 
-import { useDispatch } from '../../../store'
+import { useDispatch, useStore } from '../../../store'
 import { useFocusDiory } from '../../room/hooks'
 
 import { createDiory, createLink } from '../../room/actions'
+import { CREATE_TOOL_BUTTON } from './buttons'
 
 export const useCreateTool = () => {
+  const [{ active }] = useStore((state) => state.tools)
   const { diory } = useFocusDiory()
 
   const dispatch = useDispatch()
-  const onDone = (updatedFields) => {
-    const createdDiory = { id: uuid(), ...updatedFields }
-    dispatch(createDiory(createdDiory))
-    dispatch(createLink(diory, createdDiory))
-  }
-
   return {
-    onDone,
+    isShown: CREATE_TOOL_BUTTON === active,
+    onDone: (updatedFields) => {
+      const createdDiory = { id: uuid(), ...updatedFields }
+      dispatch(createDiory(createdDiory))
+      dispatch(createLink(diory, createdDiory))
+    },
   }
 }
