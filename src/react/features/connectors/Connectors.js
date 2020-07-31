ce82c79ac3cb2aa1182ed0695e56ae1e4d40425b
Olli-Pekka Pohjola <olli-pekka.pohjola@houston-inc.com>
2020-07-31 10:28:55 +0300

    TECH: Rename connectors

---
+++ src/react/features/connectors/Connectors.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import FolderConnector from './folder/FolderConnector'
+
+const Connectors = () => {
+  return (
+    <>
+      <FolderConnector/>
+    </>
+  )
+}
+
+export default Connectors
