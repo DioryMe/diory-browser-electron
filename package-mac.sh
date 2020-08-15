6e9400e7a0102784d38cdc2cafe9ad16599752e3
Jouni Alanen <jvalanen@gmail.com>
2020-08-15 11:08:20 +0300

    DDA-39: Add process.env.BINARY_BUILD=1 line to electron-main.js after it is built and before it is packaged

--- package-mac.sh
+++ package-mac.sh
@@ -5,8 +5,16 @@
 # echo "Cloning clean code from Github to $(pwd)..."
 # git clone git@github.com:DioryMe/diory-browser-electron.git
 # cd diory-browser-electron
+
 echo "Installing dependencies and building the app..."
 yarn && yarn build && yarn build-electron
+
+# Add app-package-envs to electron-main.js
+export APP_PACKAGE_ENVS="process.env.BINARY_BUILD=1";
+awk 'BEGIN{print ENVIRON["APP_PACKAGE_ENVS"]}{print}' build/electron-main.js > build/electron-main-tmp.js
+mv build/electron-main-tmp.js build/electron-main.js
+unset APP_PACKAGE_ENVS
+
 echo "Create MacOS distribution package..."
 yarn package-mac
 # aws s3 cp dist/*.dmg s3://dda-downloads --acl public-read
