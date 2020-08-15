30bf9e41efca42cfcd0dad0282f061df6749fc69
Olli-Pekka Pohjola <olli-pekka.pohjola@houston-inc.com>
2020-08-15 20:51:15 +0300

    TECH: Improve logging

--- src/react/utils/middleware/logger.js
+++ src/react/utils/middleware/logger.js
@@ -1,15 +1,12 @@
 export const initial = (state, action) => {
   console.log('-------------')
-  console.log('PreviousState:')
-  console.log(state)
-  console.log('Action:')
-  console.log(action)
+  console.log('PreviousState:', state)
+  console.log('Action:', action)
 
   return state
 }
 
 export const final = (state) => {
-  console.log('NextState:')
-  console.log(state)
+  console.log('NextState:', state)
   return state
 }
