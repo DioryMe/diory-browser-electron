4eef0adb11d74ca3673488c8cea9cd27b35c9c11
Olli-Pekka Pohjola <olli-pekka.pohjola@houston-inc.com>
2020-08-18 23:19:32 +0300

    DDA-55: Rename open room to add connection button

--- src/react/features/connectors/folder/buttons/buttons.js
+++ src/react/features/connectors/folder/buttons/buttons.js
@@ -1,9 +1,9 @@
-export const OPEN_ROOM_BUTTON = 'OPEN_ROOM_BUTTON'
+export const ADD_CONNECTION_BUTTON = 'ADD_CONNECTION_BUTTON'
 
 export const buttons = [
   {
-    id: OPEN_ROOM_BUTTON,
-    text: 'Open room',
+    id: ADD_CONNECTION_BUTTON,
+    text: 'Add connection',
     data: {
       icon: 'folder-open',
     },
