89b6f9df14a30bdb98167a7718e45bf61b111fed
Olli-Pekka Pohjola <olli-pekka.pohjola@houston-inc.com>
2020-08-18 23:27:05 +0300

    DDA-55: Store app state in home

--- electron/channels/generate-diograph-channel.js
+++ electron/channels/generate-diograph-channel.js
@@ -1,5 +1,4 @@
 const { ipcMain } = require('electron')
-const UserDataStore = require('electron-store')
 const { channels } = require('../../src/shared/constants')
 const { generateDiograph } = require('../generators/diograph-generator')
 const { saveRoom } = require('../lib/room-util')
@@ -9,14 +8,6 @@ ipcMain.on(channels.GENERATE_DIOGRAPH, (event, path) => {
   generateDiograph(path).then(({ id, diograph }) => {
     saveRoom(path, diograph)
       .then(() => {
-        // Store current roomId to user data
-        const store = new UserDataStore()
-        store.set({
-          roomInFocus: {
-            id,
-            path,
-          },
-        })
         event.reply(channels.GENERATE_DIOGRAPH, { id, diograph, path })
       })
       .catch((err) => {
