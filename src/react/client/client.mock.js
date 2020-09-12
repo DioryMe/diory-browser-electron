99099ff1dcec1e6d3530bd3da86196d8d4f8a79d
Jouni Alanen <jouni.alanen@checkout.fi>
2020-09-12 21:15:34 +0300

    Re-add GET_HOME to client.mock.js

--- src/react/client/client.mock.js
+++ src/react/client/client.mock.js
@@ -1,7 +1,9 @@
 import GET_ROOM from './mockResponses/GET_ROOM.json'
+import GET_HOME from './mockResponses/GET_HOME.json'
 
 export const mockResponse = (channel) => {
   const data = {
+    GET_HOME,
     GET_ROOM,
   }[channel]
 
