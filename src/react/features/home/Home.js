85a7e56d1b89177502968c32656b268b755f3981
Olli-Pekka Pohjola <olli-pekka.pohjola@houston-inc.com>
2020-10-11 11:57:33 +0300

    TECH: Disable remove room button from lenses

--- src/react/features/home/Home.js
+++ src/react/features/home/Home.js
@@ -1,57 +1,17 @@
 import React from 'react'
-import { Pane } from 'evergreen-ui'
-import { useStore } from '../../store'
-import { useButtons } from '../buttons'
 import { useFocusDiory } from '../room/hooks'
-import { removeRoom } from './actions'
 
 import { useGetHomeEffect } from './effects/useGetHomeEffect'
 import { useSaveHomeEffect } from './effects/useSaveHomeEffect'
 
-import { enterRoom, setFocus } from '../navigation/actions'
-
-import Room from '../../components/Room'
-
-import { buttons, REMOVE_ROOM } from './buttons'
-
-const useHome = () => {
-  const [{ rooms }, dispatch] = useStore((state) => state.home)
-  const [{ active }] = useStore((state) => state.buttons)
-
-  return {
-    rooms: Object.entries(rooms).map(([id, diory]) => ({
-      diory,
-      onClick: () => {
-        switch (active) {
-          case REMOVE_ROOM:
-            dispatch(removeRoom({ id }))
-            break
-          default:
-            dispatch(enterRoom({ id }))
-            dispatch(setFocus({ focus: diory.id }))
-        }
-      },
-    })),
-  }
-}
+import HomeView from './HomeView'
 
 const Home = () => {
-  useButtons(buttons)
-
   useGetHomeEffect()
   useSaveHomeEffect()
 
   const { diory } = useFocusDiory()
-  const { rooms } = useHome()
-  return (
-    !diory && (
-      <Pane height="100%" display="flex" flexWrap="wrap" padding={24}>
-        {rooms.map((room) => (
-          <Room {...room} />
-        ))}
-      </Pane>
-    )
-  )
+  return !diory && <HomeView />
 }
 
 export default Home
---
+++ src/react/features/home/HomeView.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { Pane } from 'evergreen-ui'
+import { useStore } from '../../store'
+import { useButtons } from '../buttons'
+import { removeRoom } from './actions'
+
+import { enterRoom, setFocus } from '../navigation/actions'
+
+import Room from '../../components/Room'
+
+import { buttons, REMOVE_ROOM } from './buttons'
+
+const useHome = () => {
+  const [{ rooms }, dispatch] = useStore((state) => state.home)
+  const [{ active }] = useStore((state) => state.buttons)
+
+  return {
+    rooms: Object.entries(rooms).map(([id, diory]) => ({
+      diory,
+      onClick: () => {
+        switch (active) {
+          case REMOVE_ROOM:
+            dispatch(removeRoom({ id }))
+            break
+          default:
+            dispatch(enterRoom({ id }))
+            dispatch(setFocus({ focus: diory.id }))
+        }
+      },
+    })),
+  }
+}
+
+const HomeView = () => {
+  useButtons(buttons)
+  const { rooms } = useHome()
+  return (
+    <Pane height="100%" display="flex" flexWrap="wrap" padding={24}>
+      {rooms.map((room) => (
+        <Room {...room} />
+      ))}
+    </Pane>
+  )
+}
+
+export default HomeView
