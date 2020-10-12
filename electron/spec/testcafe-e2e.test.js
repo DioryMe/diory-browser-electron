326c816c6c93a48ae494e08e1ad4b0b9ce539351
Jouni Alanen <jvalanen@gmail.com>
2020-10-12 21:40:15 +0300

    App binary is opened and controlled by Testcafe

--- electron/spec/testcafe-e2e.test.js
+++ electron/spec/testcafe-e2e.test.js
@@ -1,9 +1,20 @@
 import { Selector } from 'testcafe'
+// import { setElectronDialogHandler } from 'testcafe-browser-provider-electron'
 
 // eslint-disable-next-line no-undef
 fixture`Electron test`.page('../../build/index.html')
 
 test('Test test', async (t) => {
+  // await setElectronDialogHandler((type, browserWindow, options) => {
+  //   // browserWindow, options are standard arguments of the opening dialog, you can use it for your purposes
+  //   if (type !== 'open-dialog') {
+  //     return
+  //   }
+  //   // it returns the file path from the open dialog
+  //   const path = '/Users/Jouni/Code/diory-browser-electron/public/development-content-room'
+  //   return [path]
+  // })
+
   const dioryCount = Selector('.ub-color_white').count
 
   await t
