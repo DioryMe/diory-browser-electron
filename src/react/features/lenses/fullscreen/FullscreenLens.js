30f03b95b17c4a11b7fba630d63385a0ff981617
Olli-Pekka Pohjola <olli-pekka.pohjola@houston-inc.com>
2020-10-13 21:34:33 +0300

    POC-59: Use normal diory in fullscreen lens

--- src/react/features/lenses/fullscreen/FullscreenLens.js
+++ src/react/features/lenses/fullscreen/FullscreenLens.js
@@ -1,10 +1,10 @@
 import React from 'react'
+import Diory from '../../../components/diories/Diory'
 import { useFocusDiory } from '../../room/hooks'
-import BackgroundDiory from '../../../components/diories/BackgroundDiory'
 
 const FullscreenLens = () => {
   const { diory } = useFocusDiory()
-  return <BackgroundDiory diory={diory} height="100%" />
+  return <Diory diory={diory} height="100%" />
 }
 
 export default FullscreenLens
