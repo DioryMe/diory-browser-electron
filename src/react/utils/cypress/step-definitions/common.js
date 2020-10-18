641adf1b91705427ed5ad22c7cf4166bfea5d1f7
Jouni Alanen <jvalanen@gmail.com>
2020-10-18 22:47:01 +0300

    Cleanup room.feature + remove all the obsolete step definitions

--- src/react/utils/cypress/step-definitions/common.js
+++ src/react/utils/cypress/step-definitions/common.js
@@ -4,7 +4,6 @@ const url = 'http://localhost:3300'
 
 Given('I am at home', () => {
   cy.visit(url)
-  // cy.waitForReact()
 })
 
 Given('I select {word} button', (toolName) => {
@@ -20,24 +19,10 @@ Then('{word} tool dialog is opened', (toolName) => {
   cy.contains(`${toolName} diory`).should('have.length', 1)
 })
 
-When('I take {word} {int} in focus', (idPrefix, id) => {
-  cy.get(`div#${idPrefix}${id}`).click()
-})
-
-Then('I see {word} {int} in view', (idPrefix, id) => {
-  cy.get(`div#${idPrefix}${id}`).should('have.length', 1)
-})
-
-Then('I see {int} linked diory(s)', (amount) => {
-  // CURRENTLY DOESN'T WORK
-  // cy.react('Diory').should('have.length', amount)
-  // expect(this.app.find('Diory')).toHaveLength(amount)
+When('I take {string} in focus', (dioryName) => {
+  cy.contains(dioryName).click()
 })
 
 Then('I see {string} diory', (dioryName) => {
   cy.contains(dioryName).should('have.length', 1)
-})
-
-Then("I don't see {string} diory", (dioryName) => {
-  cy.contains(dioryName).should('have.length', 0)
 })
