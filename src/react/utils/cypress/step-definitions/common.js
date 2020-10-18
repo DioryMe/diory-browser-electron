5624357ea33f6fa988526c306ca55af4cb53865c
Jouni Alanen <jvalanen@gmail.com>
2020-10-18 22:41:21 +0300

    DDA-65: First createTool and updateTool stories enabled

--- src/react/utils/cypress/step-definitions/common.js
+++ src/react/utils/cypress/step-definitions/common.js
@@ -7,6 +7,19 @@ Given('I am at home', () => {
   // cy.waitForReact()
 })
 
+Given('I select {word} button', (toolName) => {
+  cy.get(`div[data-testid=${toolName}-button]`).click()
+})
+
+Then('{word} tool is active', (toolName) => {
+  cy.get(`div[data-testid=${toolName}-button]`).should('have.length', 0)
+  cy.get(`div[data-testid=${toolName}-button--active]`).should('have.length', 1)
+})
+
+Then('{word} tool dialog is opened', (toolName) => {
+  cy.contains(`${toolName} diory`).should('have.length', 1)
+})
+
 When('I take {word} {int} in focus', (idPrefix, id) => {
   cy.get(`div#${idPrefix}${id}`).click()
 })
