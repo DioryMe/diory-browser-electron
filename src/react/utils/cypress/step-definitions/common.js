f4d12ff6bbf72295880e51e546dbe0d2107704e0
Jouni Alanen <jvalanen@gmail.com>
2020-10-19 21:19:49 +0300

    DDA-65: Convert navigation tests to use Cypress & new development content

--- src/react/utils/cypress/step-definitions/common.js
+++ src/react/utils/cypress/step-definitions/common.js
@@ -2,6 +2,8 @@ import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps'
 
 const url = 'http://localhost:3300'
 
+// GIVEN
+
 Given('I am at home', () => {
   cy.visit(url)
 })
@@ -9,6 +11,34 @@ Given('I am at home', () => {
 Given('I select {word} button', (toolName) => {
   cy.get(`div[data-testid=${toolName}-button]`).click()
 })
+
+// WHEN
+
+When('I take {string} in focus', (dioryName) => {
+  cy.contains(dioryName).click()
+})
+
+When('I navigate(d) backward', () => {
+  cy.get('button[data-testid="navigate-left"]').click()
+})
+
+When('I navigate(d) forward', () => {
+  cy.get('button[data-testid="navigate-right"]').click()
+})
+
+When('I navigate(d) to home', () => {
+  cy.get('button[data-testid="home"]').click()
+})
+
+When('I navigate(d) to right', () => {
+  cy.get('div[data-testid="navigate-right"]').click()
+})
+
+When('I navigate(d) to left', () => {
+  cy.get('div[data-testid="navigate-left"]').click()
+})
+
+// THEN
 
 Then('{word} tool is active', (toolName) => {
   cy.get(`div[data-testid=${toolName}-button]`).should('have.length', 0)
@@ -19,10 +49,10 @@ Then('{word} tool dialog is opened', (toolName) => {
   cy.contains(`${toolName} diory`).should('have.length', 1)
 })
 
-When('I take {string} in focus', (dioryName) => {
-  cy.contains(dioryName).click()
+Then('I see {string} in view', (dioryName) => {
+  cy.contains(dioryName).should('have.length', 1)
 })
 
-Then('I see {string} diory', (dioryName) => {
-  cy.contains(dioryName).should('have.length', 1)
+Then('I do not see {word} to {word} button', (word1, word2) => {
+  cy.get(`button[${word1}-${word2}]`).should('have.length', 0)
 })
