89d36997f8dde2865f92bba1f91e00b71eb5410c
Jouni Alanen <jvalanen@gmail.com>
2020-12-20 21:25:12 +0200

    For tests, load ipcRenderer from 'electron' instead of window

--- src/react/client/client.js
+++ src/react/client/client.js
@@ -1,8 +1,21 @@
 import { mockResponse } from './client.mock'
 
-const { ipcRenderer } = window
+// Production loading
+let { ipcRenderer } = window
+
+// Tests / development don't find it from window
+if (!ipcRenderer) {
+  try {
+    // Tests wants to load it from 'electron'...
+    ipcRenderer = require('electron').ipcRenderer
+  } catch (TypeError) {
+    // ...but development prefers mocking!
+    ipcRenderer = undefined
+  }
+}
 
 export const openChannel = (channel, params) => {
+  // Development uses this
   if (!ipcRenderer) {
     console.log('IPC client mock response', channel, params)
     return mockResponse(channel, params)
