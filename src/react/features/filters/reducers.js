2cd3d6383cc0a813c64054676557c207d60ae7aa
Olli-Pekka Pohjola <olli-pekka.pohjola@houston-inc.com>
2020-12-20 14:40:07 +0200

    DDA-83: Add filters to lenses bar

--- src/react/features/filters/reducers.js
+++ src/react/features/filters/reducers.js
@@ -1,15 +1,44 @@
-import { SET_TEXT_FILTER } from './actionsTypes'
+import { SET_FILTERS, ACTIVATE_FILTER, SET_FILTER, SET_TEXT_FILTER } from './actionsTypes'
 import { createReducer } from '../../store'
 
-export const initialState = {
-  textFilter: undefined,
+const DEFAULT_Filter = {
+  grid: 1,
 }
 
-export const setTextFilter = (state, { payload }) => ({
+export const initialState = {
+  active: {
+    grid: true,
+  },
+  filters: DEFAULT_Filter,
+  textFilter: undefined,
+  updated: false,
+}
+
+const setFilters = (state, { payload }) => ({
+  ...state,
+  filters: payload.filters,
+  updated: false,
+})
+
+const activateFilter = (state, { payload }) => ({
+  ...state,
+  active: {
+    ...state.active,
+    ...payload.filter,
+  },
+  updated: true,
+})
+
+const setFilter = (state, { payload }) => state
+
+const setTextFilter = (state, { payload }) => ({
   ...state,
   textFilter: payload.textFilter,
 })
 
 export default createReducer({
+  [SET_FILTERS]: setFilters,
+  [ACTIVATE_FILTER]: activateFilter,
+  [SET_FILTER]: setFilter,
   [SET_TEXT_FILTER]: setTextFilter,
 })
