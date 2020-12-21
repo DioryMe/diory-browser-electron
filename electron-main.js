8d8d7fecf29b21bcb8b5d911d00b035bbe0f69e5
Jouni Alanen <jvalanen@gmail.com>
2020-12-21 20:55:56 +0200

    New better way to load channels in electron-main.js

--- electron-main.js
+++ electron-main.js
@@ -50,7 +50,11 @@ app.on('activate', () => {
   }
 })
 
+
 function loadChannels() {
+  // Load channels from single file instead of iterating several
+  require('electron/channels/channels.js')
+
   const files = glob.sync(path.join(__dirname, 'electron/channels/**/*.js'))
   files.forEach((file) => require(file))
 }
