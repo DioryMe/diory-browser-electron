74a3c60cff9755a069b825facdfe62e2af1c1fc4
Jouni Alanen <jvalanen@gmail.com>
2020-12-21 14:10:39 +0200

    handleEvent created for common handling of frontend events in the backend

--- electron/channels/get-room-channel.spec.js
+++ electron/channels/get-room-channel.spec.js
@@ -1,10 +1,11 @@
-import { handleGetRoomEvent } from './room-util'
+import { handleEvent } from './handle-event'
+import { handleGetRoomEvent } from '../lib/room-util'
 
 const fs = require('fs')
 
-const mockEventSenderSend = jest.fn()
+const mockEventReply = jest.fn()
 
-const mockEvent = { sender: { send: mockEventSenderSend } }
+const mockEvent = { reply: mockEventReply }
 
 describe('handleGetRoomEvent', () => {
   it('works', async () => {
@@ -12,14 +13,13 @@ describe('handleGetRoomEvent', () => {
       '/Users/Jouni/Code/diory-browser-electron/public/development-content-room'
     const params = { address: exampleFolderPath }
 
-    await handleGetRoomEvent(mockEvent, params)
+    await handleEvent(handleGetRoomEvent)(mockEvent, params)
 
     const diographJSONRawContents = fs.readFileSync(`${exampleFolderPath}/diograph.json`)
     const diographObject = JSON.parse(diographJSONRawContents)
     const returnValue = { diograph: diographObject }
 
-    expect(mockEventSenderSend.mock.calls.length).toBe(1)
-    expect(mockEventSenderSend.mock.calls[0][0]).toEqual('GET_ROOM')
-    expect(mockEventSenderSend.mock.calls[0][1]).toEqual(returnValue)
+    expect(mockEventReply.mock.calls.length).toBe(1)
+    expect(mockEventReply.mock.calls[0][0]).toEqual(returnValue)
   })
 })
