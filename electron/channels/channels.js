aeae1f1c118b0132f2b59d17d4a0b0c57d749fdd
Jouni Alanen <jvalanen@gmail.com>
2020-12-21 22:18:43 +0200

    Initial spec for GET_HOME channel

--- electron/channels/channels.js
+++ electron/channels/channels.js
@@ -2,6 +2,10 @@ const { ipcMain } = require('electron')
 const { channels } = require('../../src/shared/constants')
 const { handleGetRoomEvent } = require('../lib/room-util')
 const { handleEvent } = require('./handle-event')
+const { handleGetHomeEvent } = require('./get-home-channel')
 
 /* GET_ROOM */
 ipcMain.on(channels.GET_ROOM, handleEvent(handleGetRoomEvent))
+
+/* GET_HOME */
+ipcMain.on(channels.GET_HOME, handleGetHomeEvent)
--- electron/channels/get-home-channel.js
+++ electron/channels/get-home-channel.js
@@ -1,36 +1,34 @@
-const { ipcMain } = require('electron')
 const HomeStore = require('electron-store')
 const path = require('path')
-const { channels } = require('../../src/shared/constants')
+// const { channels } = require('../../src/shared/constants')
 
-ipcMain.on(channels.GET_HOME, (event) => {
-  console.log('GET_HOME')
-
-  const defaultWelcomeRoomPath = process.env.BINARY_BUILD
-    ? path.join(__dirname, '../../default-welcome-room')
-    : './public/default-welcome-room'
-
-  const defaultHome = {
-    rooms: {
-      'welcome-room': {
-        id: 'welcome-room',
-        text: 'Welcome room!',
-      },
-    },
-    connections: {
-      [defaultWelcomeRoomPath]: {
-        room: 'welcome-room',
-        connector: 'file',
-      },
-    },
-    focus: {
-      roomId: 'welcome-room',
-      dioryId: 'welcome-room',
-    },
-  }
-
+export const handleGetHomeEvent = (event, params) => {
   const store = new HomeStore()
   const home = store.get('home') || defaultHome
 
-  event.reply(channels.GET_HOME, home)
-})
+  // event.reply(channels.GET_HOME, home)
+  event.reply(home)
+}
+
+export const defaultWelcomeRoomPath = process.env.BINARY_BUILD
+  ? path.join(__dirname, '../../default-welcome-room')
+  : './public/default-welcome-room'
+
+export const defaultHome = {
+  rooms: {
+    'welcome-room': {
+      id: 'welcome-room',
+      text: 'Welcome room!',
+    },
+  },
+  connections: {
+    [defaultWelcomeRoomPath]: {
+      room: 'welcome-room',
+      connector: 'file',
+    },
+  },
+  focus: {
+    roomId: 'welcome-room',
+    dioryId: 'welcome-room',
+  },
+}
