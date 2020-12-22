8a2ca3c1a26642d1d902f129ab46e3545420a6b9
Jouni Alanen <jvalanen@gmail.com>
2020-12-22 10:59:02 +0200

    Improve documentation + initial generateDiographEventHandler

--- electron/channels/channels.js
+++ electron/channels/channels.js
@@ -4,9 +4,13 @@ const { eventHandlerWrapper } = require('./channel-util')
 
 const { getHomeEventHandler } = require('./get-home-channel')
 const { getRoomEventHandler } = require('./get-room-channel')
+const { generateDiographEventHandler } = require('./generate-diograph-channel')
 
 /* GET_ROOM */
 ipcMain.on(channels.GET_ROOM, eventHandlerWrapper(getRoomEventHandler))
 
 /* GET_HOME */
 ipcMain.on(channels.GET_HOME, eventHandlerWrapper(getHomeEventHandler))
+
+/* GENERATE_DIOGRAPH */
+ipcMain.on(channels.GENERATE_DIOGRAPH, eventHandlerWrapper(generateDiographEventHandler))
