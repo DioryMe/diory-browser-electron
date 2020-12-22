96f81468a4d2b4b50f7d29b24eaa857b1908dbee
Jouni Alanen <jvalanen@gmail.com>
2020-12-22 23:43:30 +0200

    Spec for original implementation of generateDiographEventHandler

--- electron/channels/generate-diograph-channel.spec.js
+++ electron/channels/generate-diograph-channel.spec.js
@@ -1,17 +1,19 @@
 // import { eventHandlerWrapper } from './channel-util'
 import { generateDiographEventHandler } from './generate-diograph-channel'
 import { generateDiograph } from '../generators/diograph-generator'
+import { saveRoom } from '../lib/room-util'
 
 // Mock event.reply
 const mockEventReply = jest.fn()
 const mockEvent = { reply: mockEventReply }
+// Mock generateDiograph
 jest.mock('../generators/diograph-generator')
-generateDiograph.mockImplementation(
-  () =>
-    new Promise((resolve, reject) => {
-      resolve('this is generated diograph')
-    })
+generateDiograph.mockImplementation(() =>
+  Promise.resolve({ id: 'this', diograph: 'is generated diograph' })
 )
+// Mock saveRoom
+jest.mock('../lib/room-util')
+saveRoom.mockImplementation(() => Promise.resolve(undefined))
 
 describe('generateDiographEventHandler', () => {
   it('sends event with generateDiograph return value', async () => {
@@ -22,8 +24,17 @@ describe('generateDiographEventHandler', () => {
     expect(generateDiograph).toHaveBeenCalledTimes(1)
     expect(generateDiograph).toHaveBeenCalledWith(params)
 
+    // WTF is this `await expect` => with this it works!
+    await expect(saveRoom).toHaveBeenCalledTimes(1)
+    expect(saveRoom).toHaveBeenCalledWith(params, 'is generated diograph')
+
     expect(mockEventReply.mock.calls.length).toBe(1)
     expect(mockEventReply.mock.calls[0][0]).toEqual('GENERATE_DIOGRAPH')
-    expect(mockEventReply.mock.calls[0][1]).toEqual({ diograph: 'this is generated diograph' })
+    const responseObject = {
+      id: 'this',
+      diograph: 'is generated diograph',
+      path: 'this is a file path',
+    }
+    expect(mockEventReply.mock.calls[0][1]).toEqual(responseObject)
   })
 })
