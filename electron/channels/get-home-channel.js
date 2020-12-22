367db523f0659d2dec1234c90c3d6884ab64312a
Jouni Alanen <jvalanen@gmail.com>
2020-12-22 09:53:11 +0200

    GET_ROOM & GET_HOME works fine with handleEvent pattern

--- electron/channels/get-home-channel.js
+++ electron/channels/get-home-channel.js
@@ -1,19 +1,30 @@
 const HomeStore = require('electron-store')
 const path = require('path')
-// const { channels } = require('../../src/shared/constants')
+const { channels } = require('../../src/shared/constants')
 
+/**
+ * Event handler for GET_HOME channel
+ * @function
+ * @param event {Object} - Event from frontend via ipcMain
+ * @param params {Object} - No params
+ * @return {Promise} Resolves with home object (keys: rooms, connections, focus)
+ */
 export const handleGetHomeEvent = (event, params) => {
   const store = new HomeStore()
   const home = store.get('home') || defaultHome
 
-  // event.reply(channels.GET_HOME, home)
-  event.reply(home)
+  return new Promise((resolve, reject) => {
+    resolve({ channelName: channels.GET_HOME, responseObject: home })
+  })
 }
 
+// Path to Welcome room folder
+// - different in production build (.dmg/.exe) than in local build
 export const defaultWelcomeRoomPath = process.env.BINARY_BUILD
   ? path.join(__dirname, '../../default-welcome-room')
   : './public/default-welcome-room'
 
+// Welcome room home object
 export const defaultHome = {
   rooms: {
     'welcome-room': {
