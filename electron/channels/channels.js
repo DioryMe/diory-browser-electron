744f835d81b9c2b4fb0de328e8b0fa17ccb7d704
Jouni Alanen <jvalanen@gmail.com>
2020-12-23 22:22:53 +0200

    Extract saveRoomEventHandler from SAVE_ROOM event handler ipcMain.on(...) thingie

--- electron/channels/channels.js
+++ electron/channels/channels.js
@@ -5,6 +5,7 @@ const { eventHandlerWrapper } = require('./channel-util')
 const { getHomeEventHandler } = require('./get-home-channel')
 const { getRoomEventHandler } = require('./get-room-channel')
 const { generateDiographEventHandler } = require('./generate-diograph-channel')
+const { saveRoomEventHandler } = require('./save-room-channel')
 
 /* GET_ROOM */
 ipcMain.on(channels.GET_ROOM, eventHandlerWrapper(getRoomEventHandler))
@@ -15,3 +16,7 @@ ipcMain.on(channels.GET_HOME, eventHandlerWrapper(getHomeEventHandler))
 /* GENERATE_DIOGRAPH */
 ipcMain.on(channels.GENERATE_DIOGRAPH, generateDiographEventHandler)
 // ipcMain.on(channels.GENERATE_DIOGRAPH, eventHandlerWrapper(generateDiographEventHandler))
+
+/* SAVE_ROOM */
+ipcMain.on(channels.SAVE_ROOM, saveRoomEventHandler)
+// ipcMain.on(channels.SAVE_ROOM, eventHandlerWrapper(saveRoomEventHandler))
