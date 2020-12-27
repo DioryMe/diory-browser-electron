85a2e85df125685d7f4939431e675280f2ed184e
Jouni Alanen <jvalanen@gmail.com>
2020-12-27 18:12:00 +0200

    saveRoom channel to use eventWrapper

--- electron/channels/save-room-channel.js
+++ electron/channels/save-room-channel.js
@@ -1,14 +1,14 @@
 const { channels } = require('../../src/shared/constants')
 const { saveRoom } = require('../lib/room-util')
 
-export const saveRoomEventHandler = (event, { path, room: { id, diograph } }) => {
-  saveRoom(path, diograph)
-    .then(() => {
-      console.log(JSON.stringify(diograph, null, 2))
-      event.reply(channels.SAVE_ROOM, true)
-    })
-    .catch((err) => {
-      console.log(err.message)
-      return event.reply(channels.SAVE_ROOM, null, err)
-    })
-}
+export const saveRoomEventHandler = (event, { path, room: { id, diograph } }) =>
+  new Promise((resolve, reject) =>
+    saveRoom(path, diograph).then(
+      (responseObject) => {
+        resolve({ channelName: channels.SAVE_ROOM, responseObject: true })
+      },
+      (error) => {
+        reject({ channelName: channels.SAVE_ROOM, errorObject: error }) // eslint-disable-line prefer-promise-reject-errors
+      }
+    )
+  )
--- electron/channels/save-room-channel.spec.js
+++ electron/channels/save-room-channel.spec.js
@@ -1,4 +1,4 @@
-// import { eventHandlerWrapper } from './channel-util'
+import { eventHandlerWrapper } from './channel-util'
 import { saveRoomEventHandler } from './save-room-channel'
 import { saveRoom } from '../lib/room-util'
 
@@ -22,8 +22,8 @@ describe('saveRoomEventHandler sends event with payload ', () => {
   it("'true, undefined' if Promise resolves", async () => {
     saveRoom.mockImplementation(() => Promise.resolve(undefined))
 
-    await saveRoomEventHandler(mockEvent, params)
-    // await eventHandlerWrapper(saveRoomEventHandler)(mockEvent, params)
+    // await saveRoomEventHandler(mockEvent, params)
+    await eventHandlerWrapper(saveRoomEventHandler)(mockEvent, params)
 
     await expect(saveRoom).toHaveBeenCalledTimes(1)
     expect(saveRoom).toHaveBeenCalledWith(params.path, params.room.diograph)
@@ -37,8 +37,8 @@ describe('saveRoomEventHandler sends event with payload ', () => {
   it("'null, err' if Promise rejects", async () => {
     saveRoom.mockImplementation(() => Promise.reject(new Error('this is an error')))
 
-    await saveRoomEventHandler(mockEvent, params)
-    // await eventHandlerWrapper(saveRoomEventHandler)(mockEvent, params)
+    // await saveRoomEventHandler(mockEvent, params)
+    await eventHandlerWrapper(saveRoomEventHandler)(mockEvent, params)
 
     // WTF is this `await expect` => with this it works!
     await expect(saveRoom).toHaveBeenCalledTimes(1)
@@ -46,7 +46,6 @@ describe('saveRoomEventHandler sends event with payload ', () => {
 
     expect(mockEventReply.mock.calls.length).toBe(1)
     expect(mockEventReply.mock.calls[0][0]).toEqual('SAVE_ROOM')
-    expect(mockEventReply.mock.calls[0][1]).toEqual(null)
-    expect(mockEventReply.mock.calls[0][2].message).toEqual('this is an error')
+    expect(mockEventReply.mock.calls[0][1].message).toEqual('this is an error')
   })
 })
