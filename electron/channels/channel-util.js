da112a956abf98ebfe7f64c46c746e61a65d44a5
Jouni Alanen <jvalanen@gmail.com>
2020-12-27 19:31:51 +0200

    Add generic IPC backend logging + docs for eventHandlerWrapper

--- electron/channels/channel-util.js
+++ electron/channels/channel-util.js
@@ -1,39 +1,22 @@
 /**
- * Wrapper for specific event handler which resolves the Promise
- * returned by the specific event handler and then replies to the event.
- * On success replies with {Object} and on error with {Error}
+ * Wraps (1) generic IPC channel logging and (2) error handling
+ * around the eventHandler function and (3) resolves its promise
+ * and (4) replies to the IPC event from frontend
  *
- * @param {function(): Promise} specificEventHandler - Function which returns Promise which resolves with {Object} and rejects with {Error}
+ * @param {string} channelName - Function which returns Promise which resolves with {Object} and rejects with {Error}
+ * @param {function(): Promise} eventHandler - Promise which resolves with responseObject and rejects with errorObject
  * @return {function(): void} Specific eventHandler function to be given for ipcMain.on as a second argument
  */
 export const eventHandlerWrapper = (channelName, eventHandler) => {
-  /**
-   * Injects specific eventHandler function into the generic eventHandler
-   * @param event {Event}
-   * @param params {Object}
-   * @returns {undefined}
-   */
-
-  // TÄMÄ EI OLE MIKÄÄN PROMISE
-  // - tää on se, minkä ipcMain laukaisee
-  // - tämä solvaa sitten tuon eventHandler promisen
-  //   ja vastaa objektilla tai virheellä
-
-  // Yhteneväiset logitukset tämän yhteyteen olis kova kans
-  // - ei tarttis erikseen määrittää, vaan aina logittais kaikille samat
-  // - tyyliin: console.log('Backend IPC: GENERATE_DIOGRAPH', path)
-  //            console.log('Backend IPC: SAVE_ROOM', path, id)
-  //            console.log('SAVE_HOME', home)
-  // console.log(JSON.stringify(diograph, null, 2))
-  // console.log(err.message)
-
   const specificEventHandler = (event, params) => {
     const success = (responseObject) => {
+      console.log(`Backend IPC reply: ${channelName}, `, responseObject)
       event.reply(channelName, responseObject)
     }
 
-    const err = (errorObject) => {
-      event.reply(channelName, errorObject)
+    const err = (e) => {
+      console.log(`ERROR: Backend IPC reply: ${channelName}, `, e.message)
+      event.reply(channelName, e)
     }
 
     eventHandler(event, params).then(success, err)
