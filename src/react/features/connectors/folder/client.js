252997c871a286781ae016bc7525c62d03f8ec68
Jouni Alanen <jvalanen@gmail.com>
2020-12-30 23:23:09 +0200

    Convert openChannel to invokeChannel

--- src/react/features/connectors/folder/client.js
+++
@@ -1,6 +0,0 @@
-import { channels } from '../../../../shared/constants'
-import { openChannel } from '../../../client/client'
-
-export const getRoom = (address) => openChannel(channels.GET_ROOM, address)
-
-export const saveRoom = (path, room) => openChannel(channels.SAVE_ROOM, { path, room })
