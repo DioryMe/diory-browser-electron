313a018c97f1c3fd149cca27fc1d608402250cf1
Jouni Alanen <jvalanen@gmail.com>
2021-01-01 22:19:51 +0200

    Test logging with window.frontendLogger

--- src/react/client/client.spec.js
+++ src/react/client/client.spec.js
@@ -4,7 +4,9 @@ import { invokeChannel } from './client'
 jest.mock('electron', () => ({ ipcRenderer: { invoke: jest.fn() } }), { virtual: true })
 const ipcRendererInvokeMock = ipcRenderer.invoke.mockResolvedValue('response')
 
-// window.frontendLogger = jest.fn()
+const logInfoMock = jest.fn()
+const logInfoError = jest.fn()
+window.frontendLogger = { info: logInfoMock, error: logInfoError }
 
 describe('invokeChannel', () => {
   it('works', async () => {
@@ -16,5 +18,9 @@ describe('invokeChannel', () => {
     expect(ipcRendererInvokeMock).toHaveBeenCalledTimes(1)
     expect(ipcRendererInvokeMock).toHaveBeenCalledWith(channel, params)
     expect(response).toEqual('response')
+
+    expect(logInfoMock).toHaveBeenCalledTimes(2)
+    expect(logInfoMock).toHaveBeenCalledWith('Frontend IPC invoke:', channel, params)
+    expect(logInfoMock).toHaveBeenCalledWith('Frontend IPC response:', channel, 'response')
   })
 })
