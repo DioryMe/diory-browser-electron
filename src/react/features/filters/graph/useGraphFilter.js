3b436bdb6e4fa24b93dd20d989ea6fbdce5509c2
Olli-Pekka Pohjola <olli-pekka.pohjola@houston-inc.com>
2021-01-09 19:16:18 +0200

    DDA-83: Create graph filter

---
+++ src/react/features/filters/graph/useGraphFilter.js
@@ -0,0 +1,33 @@
+import { useStore } from '../../../store'
+import { useFocus } from '../../diograph/hooks'
+
+import { reduceIdsToKeys } from '../../../utils/reduceIdsToKeys'
+
+function getArray(length) {
+  return [...Array(length - 1).keys()]
+}
+
+function getLinkIds({ links } = {}) {
+  return links ? Object.values(links).reduce(reduceIdsToKeys, {}) : {}
+}
+
+function getDioryLinkIds(diograph) {
+  return (obj, { id }) => ({
+    ...obj,
+    ...getLinkIds(diograph[id]),
+  })
+}
+
+export const useGraphFilter = () => {
+  const [{ diograph }] = useStore((state) => state.diograph)
+  const [{ grid: isActive }] = useStore((state) => state.filters.active)
+  const [{ grid: zoom }] = useStore((state) => state.filters.filters)
+  const { diory } = useFocus()
+  return (
+    isActive &&
+    getArray(zoom || 1).reduce(
+      (dioryIds) => Object.values(dioryIds).reduce(getDioryLinkIds(diograph), dioryIds),
+      getLinkIds(diory)
+    )
+  )
+}
