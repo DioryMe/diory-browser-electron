1f362a01b4d67758d68cc37fbc70eea594e9c489
Olli-Pekka Pohjola <olli-pekka.pohjola@houston-inc.com>
2021-01-09 19:42:41 +0200

    DDA-83: Set map bounds to map filter

--- src/react/features/lenses/map/hooks/useMapBounds.js
+++ src/react/features/lenses/map/hooks/useMapBounds.js
@@ -1,13 +1,17 @@
-import { useEffect } from 'react'
-import { useInitial } from '../../../../utils/useCompare'
+import { useCallback, useEffect, useState } from 'react'
+import { useCompare, useInitial } from '../../../../utils/useCompare'
 
 const MAX_ZOOM = 14
 
-export const useMapBounds = (mapRef, { center, min, max }) => {
+const useFitMapToBounds = (mapRef, { center, min, max }, bounds) => {
   const isInitial = useInitial(center)
   useEffect(() => {
     if (mapRef.current) {
-      if (min && max) {
+      if (bounds) {
+        isInitial
+          ? mapRef.current.fitBounds(bounds, { maxZoom: MAX_ZOOM })
+          : mapRef.current.flyToBounds(bounds, { maxZoom: MAX_ZOOM })
+      } else if (min && max) {
         isInitial
           ? mapRef.current.fitBounds([min, max], { maxZoom: MAX_ZOOM })
           : mapRef.current.flyToBounds([min, max], { maxZoom: MAX_ZOOM })
@@ -19,5 +23,47 @@ export const useMapBounds = (mapRef, { center, min, max }) => {
         mapRef.current.fitWorld()
       }
     }
-  }, [mapRef, isInitial, center, min, max])
+  }, [mapRef, isInitial, bounds, center, min, max])
+}
+
+function getBoundsArrays(bounds) {
+  return [
+    [bounds.getNorth(), bounds.getEast()],
+    [bounds.getSouth(), bounds.getWest()],
+  ]
+}
+
+const useGetMapBounds = (mapRef, onBoundsChange) => {
+  useEffect(() => {
+    if (mapRef.current) {
+      const currentBounds = mapRef.current.getBounds()
+      mapRef.current.off('moveend')
+      mapRef.current.on('moveend', (event) => {
+        const { target } = event
+        const newBounds = target.getBounds()
+        if (!newBounds.equals(currentBounds)) {
+          onBoundsChange(getBoundsArrays(newBounds))
+        }
+      })
+    }
+  }, [mapRef, onBoundsChange])
+}
+
+export const useMapBounds = (mapRef, dioryLocationData, onBoundsChange) => {
+  const [bounds, setBounds] = useState()
+
+  const locationDataChanged = useCompare(dioryLocationData.id)
+  useEffect(() => {
+    if (locationDataChanged) {
+      setBounds(undefined)
+    }
+  }, [locationDataChanged])
+
+  useFitMapToBounds(mapRef, dioryLocationData, bounds)
+
+  const handleBounds = useCallback((bounds) => {
+    onBoundsChange(bounds)
+    setBounds(bounds)
+  })
+  useGetMapBounds(mapRef, handleBounds)
 }
