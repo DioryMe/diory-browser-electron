b9f08beeeea72c7d74a86453a1b9f3f65b523e73
Jouni Alanen <jvalanen@gmail.com>
2021-01-12 18:42:15 +0200

    DDA-80: Check that if room folder paths exist and invoke an alert in frontend if it doesn't

--- electron/channels/get-home-channel.spec.js
+++ electron/channels/get-home-channel.spec.js
@@ -1,6 +1,8 @@
 const HomeStore = require('electron-store')
 const { eventHandlerWrapper } = require('./channel-util')
-const { getHomeEventHandler, defaultHome } = require('./get-home-channel')
+const getHomeChannel = require('./get-home-channel')
+
+const { getHomeEventHandler, defaultHome } = getHomeChannel
 
 // Mock event.reply
 const mockEventReply = jest.fn()
@@ -8,7 +10,31 @@ const mockEvent = { reply: mockEventReply }
 // Mock electron-store
 jest.mock('electron-store')
 
+const homeJSON = {
+  rooms: {
+    'development-content-room': {
+      id: 'development-content-room',
+    },
+  },
+  connections: {
+    './public/development-content-room': {
+      room: 'development-content-room',
+      connector: 'file',
+    },
+  },
+  focus: {
+    roomId: 'development-content-room',
+    dioryId: 'development-content-room',
+  },
+  errors: [],
+}
+
 describe('getHomeEventHandler', () => {
+  beforeEach(() => {
+    // Mock roomConnectionOK
+    getHomeChannel.roomConnectionOK = jest.fn(() => true)
+  })
+
   it('empty store', async () => {
     HomeStore.mockImplementation(() => ({ get: () => undefined }))
 
@@ -19,11 +45,25 @@ describe('getHomeEventHandler', () => {
   })
 
   it('store with value', async () => {
-    HomeStore.mockImplementation(() => ({ get: () => 'value' }))
+    HomeStore.mockImplementation(() => ({ get: () => homeJSON }))
 
     const params = {}
     const response = await eventHandlerWrapper('GET_HOME', getHomeEventHandler)(mockEvent, params)
 
-    expect(response).toEqual('value')
+    expect(response).toEqual(homeJSON)
+  })
+
+  it('invalid file path', async () => {
+    HomeStore.mockImplementation(() => ({ get: () => homeJSON }))
+    // Mock roomConnectionOK to return false
+    getHomeChannel.roomConnectionOK = jest.fn(() => false)
+
+    const params = {}
+    const response = await eventHandlerWrapper('GET_HOME', getHomeEventHandler)(mockEvent, params)
+
+    // Expect 'errors' to be added to the response
+    const homeJSONWithErrors = homeJSON
+    homeJSONWithErrors.errors = ['./public/development-content-room']
+    expect(response).toEqual(homeJSONWithErrors)
   })
 })
--- src/react/features/rooms/effects/useGetHomeEffect.js
+++ src/react/features/rooms/effects/useGetHomeEffect.js
@@ -8,11 +8,21 @@ import { setConnections } from '../../connectors/actions'
 import { invokeChannel } from '../../../client/client'
 import { channels } from '../../../../shared/constants'
 
+/*
+ * Requests home & focus info via GET_HOME and sets the focus for room & diory
+ * - saves the response to the store
+ * - invoke an alert for each error
+ */
 export const useGetHomeEffect = () => {
   const dispatch = useDispatch()
 
   useEffect(() => {
-    invokeChannel(channels.GET_HOME).then(({ rooms, connections, focus }) => {
+    invokeChannel(channels.GET_HOME).then(({ rooms, connections, focus, errors }) => {
+      errors.forEach((error) => {
+        alert(
+          `Couldn't connect to room ${error.connectionURI}, path didn't exist. Please remove and re-add the room to reconnect.`
+        )
+      })
       dispatch(setRooms(rooms))
       dispatch(setConnections(connections))
       dispatch(enterRoom({ id: focus.roomId }))
