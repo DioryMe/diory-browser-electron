ee2a697bc18d0cd973a7c0101b920b9789b5f194
Jouni Alanen <jvalanen@gmail.com>
2021-01-23 08:59:16 +0200

    DDA-60: Evergreen v5 changed input fields id's and broke our integration tests

--- src/react/utils/cypress/step-definitions/dialog.js
+++ src/react/utils/cypress/step-definitions/dialog.js
@@ -1,17 +1,17 @@
 import { When, Then } from 'cypress-cucumber-preprocessor/steps'
 
 When('I add {string} to {word} field', (inputFieldValue, inputFieldName) => {
-  cy.get(`input#TextInputField-${inputFieldName}`).type(inputFieldValue, {
+  cy.get(`input#${inputFieldName}`).type(inputFieldValue, {
     parseSpecialCharSequences: false,
   })
 })
 
 When('I press {word} key', (keyName) => {
-  cy.get('input#TextInputField-text').type(`{${keyName}}`)
+  cy.get('input#text').type(`{${keyName}}`)
 })
 
 When('I close dialog from X button', () => {
-  cy.get('div[role=dialog] > div > button > svg[data-icon=cross]').click()
+  cy.get('div[role=dialog] > div > button > div > svg[data-icon=cross]').click()
 })
 
 When('I click outside the dialog', () => {
@@ -20,9 +20,9 @@ When('I click outside the dialog', () => {
 })
 
 Then('I see {string} in {word} field', (text, inputFieldName) => {
-  cy.get(`input#TextInputField-${inputFieldName}`).invoke('val').should('eq', text)
+  cy.get(`input#${inputFieldName}`).invoke('val').should('eq', text)
 })
 
 Then('I do not see {string} in {word} field', (text, inputFieldName) => {
-  cy.get(`input#TextInputField-${inputFieldName}`).invoke('val').should('not.eq', text)
+  cy.get(`input#${inputFieldName}`).invoke('val').should('not.eq', text)
 })
