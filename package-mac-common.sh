96eb210c30577d4ee4e06936ef1dd901c896f737
Jouni Alanen <jvalanen@gmail.com>
2021-01-24 08:47:24 +0200

    POC-testcafe: Run common stuff for notarized & release candidate scripts in own script package-mac-common.sh

--- package-mac-common.sh
+++ package-mac-common.sh
@@ -3,14 +3,25 @@
 echo "Installing dependencies and building the app..."
 yarn && yarn build && yarn build-electron
 
-echo "Add app-package-envs to electron-main.js"
+# TESTCAFE_TEST = 1
+
+echo "Add TESTCAFE_TEST_ENVS to electron-main.js"
 export TESTCAFE_TEST_ENVS="process.env.TESTCAFE_TEST=1";
 awk 'BEGIN{print ENVIRON["TESTCAFE_TEST_ENVS"]}{print}' electron-main.js > electron-main-tmp.js
 cp electron-main.js electron-main-original.js
 mv electron-main-tmp.js electron-main.js
 unset TESTCAFE_TEST_ENVS
 
+echo "Run Testcafe E2E tests"
 yarn exec testcafe "electron:."  electron/spec/testcafe-e2e.test.js
 
+echo "Remove TESTCAFE_TEST_ENVS to electron-main.js"
 mv electron-main-original.js electron-main.js
 
+# BINARY_BUILD = 1
+
+echo "Add APP_PACKAGE_ENVS from electron-main.js"
+export APP_PACKAGE_ENVS="process.env.BINARY_BUILD=1";
+awk 'BEGIN{print ENVIRON["APP_PACKAGE_ENVS"]}{print}' build/electron-main.js > build/electron-main-tmp.js
+mv build/electron-main-tmp.js build/electron-main.js
+unset APP_PACKAGE_ENVS
--- package-mac-release-candidate.sh
+++ package-mac-release-candidate.sh
@@ -1,14 +1,8 @@
 # !/bin/bash
 set -e
 
-echo "Installing dependencies and building the app..."
-yarn && yarn build && yarn build-electron
-
-echo "Add app-package-envs to electron-main.js"
-export APP_PACKAGE_ENVS="process.env.BINARY_BUILD=1";
-awk 'BEGIN{print ENVIRON["APP_PACKAGE_ENVS"]}{print}' build/electron-main.js > build/electron-main-tmp.js
-mv build/electron-main-tmp.js build/electron-main.js
-unset APP_PACKAGE_ENVS
+# Run common stuff (Testcafe tests & BINARY_BUILD = 1)
+bash ./package-mac-common.sh
 
 yarn package-mac-rc
 
