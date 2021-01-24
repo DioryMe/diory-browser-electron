5301cb9fea288b63a3bd10defb94cdbadb0abf2b
Jouni Alanen <jvalanen@gmail.com>
2021-01-24 10:52:36 +0200

    POC-testcafe: Take screenshots during Testcafe test

--- electron/channels/save-home-channel.js
+++ electron/channels/save-home-channel.js
@@ -2,7 +2,7 @@ const HomeStore = require('electron-store')
 
 exports.saveHomeEventHandler = (event, home) => {
   const store = new HomeStore({
-    cwd: process.env.TESTCAFE_TEST ? process.env.PWD : undefined,
+    cwd: process.env.TESTCAFE_TEST ? `${process.env.PWD}/tmp/${Date.now()}` : undefined,
   })
   store.set({ home })
 
