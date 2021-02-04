618a94b714238808d0990c061ee64e9465e1f835
Jouni Alanen <jvalanen@gmail.com>
2021-02-04 15:45:34 +0200

    DDA-85: Run FIRST and SECOND tests with different folders

--- package-mac-common.sh
+++ package-mac-common.sh
@@ -5,25 +5,40 @@
 
 # TESTCAFE_TEST = 1
 
-echo "Add TESTCAFE_TEST_ENVS to electron-main.js"
-export TESTCAFE_TEST_ENVS="process.env.TESTCAFE_TEST=1";
+echo "FIRST TEST: Add TESTCAFE_TEST_ENVS to electron-main.js"
+export TESTCAFE_TEST_ENVS="process.env.TESTCAFE_TEST=1; process.env.DIOGRAPH_FOLDER_PATH='public/development-content-room';";
 awk 'BEGIN{print ENVIRON["TESTCAFE_TEST_ENVS"]}{print}' electron-main.js > electron-main-tmp.js
 cp electron-main.js electron-main-original.js
 mv electron-main-tmp.js electron-main.js
 unset TESTCAFE_TEST_ENVS
 
-echo "Run Testcafe E2E tests"
+echo "FIRST TEST: Run Testcafe E2E tests"
 rm -rf tmp/screenshots
-yarn exec testcafe "electron:."  electron/spec/testcafe-e2e.test.js
+yarn exec testcafe "electron:."  electron/spec/testcafe-e2e-1.test.js
+
+echo "FIRST TEST: Remove TESTCAFE_TEST_ENVS to electron-main.js"
+mv electron-main-original.js electron-main.js
+
+
+echo "SECOND TEST: Add TESTCAFE_TEST_ENVS to electron-main.js"
+export TESTCAFE_TEST_ENVS="process.env.TESTCAFE_TEST=1; process.env.DIOGRAPH_FOLDER_PATH='electron/readers/example-folder';";
+awk 'BEGIN{print ENVIRON["TESTCAFE_TEST_ENVS"]}{print}' electron-main.js > electron-main-tmp.js
+cp electron-main.js electron-main-original.js
+mv electron-main-tmp.js electron-main.js
+unset TESTCAFE_TEST_ENVS
+
+echo "SECOND TEST: Run Testcafe E2E tests"
+yarn exec testcafe "electron:."  electron/spec/testcafe-e2e-2.test.js
+
+echo "SECOND TEST: Remove TESTCAFE_TEST_ENVS to electron-main.js"
+mv electron-main-original.js electron-main.js
 
 echo "Check screenshots taken by Testcafe:"
 echo "$(pwd)/tmp/screenshots/welcome-room.png"
 echo "$(pwd)/tmp/screenshots/added-room.png"
-echo "--- Press enter to continue ---"
+echo "$(pwd)/tmp/screenshots/kayaking-at-potomac.png"
+# echo "--- Press enter to continue ---"
 # read press_enter_to_continue
-
-echo "Remove TESTCAFE_TEST_ENVS to electron-main.js"
-mv electron-main-original.js electron-main.js
 
 # BINARY_BUILD = 1
 
