bb6703289879fac753dda61527b8b7bb106927fa
Jouni Alanen <jvalanen@gmail.com>
2021-02-04 15:31:15 +0200

    DDA-85: Split test files in two: one for each diograph folder

--- electron/spec/testcafe-e2e-2.test.js
+++ electron/spec/testcafe-e2e-2.test.js
@@ -1,20 +1,17 @@
 import { Selector } from 'testcafe'
 
 // eslint-disable-next-line no-undef
-fixture`Electron test`.page('../../build/index.html')
+fixture`GIVEN welcome room`.page('../../build/index.html')
 
-test('Test test', async (t) => {
-  const dioryCount = Selector('.ub-color_white').count
+test('select new diograph folder (without diograph.json)', async (t) => {
+  const dioryCount = Selector('.ub-flx_1-0-360px').count
 
   await t
-    .takeScreenshot({ path: 'welcome-room.png' })
     .expect(dioryCount)
     .eql(1)
     .click('[data-testid="home"]')
     .click('[data-testid="tools-button"]')
     .click('[data-testid="undefined-button"]')
-    .wait(500)
-    .takeScreenshot({ path: 'added-room.png' })
     .expect(dioryCount)
     .eql(5)
 })
