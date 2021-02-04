38ff582f453af09e627339030090b6b9a5aa0278
Jouni Alanen <jvalanen@gmail.com>
2021-02-04 14:21:42 +0200

    DDA-85: Test settings to run Testcafe easily

--- package-mac-common.sh
+++ package-mac-common.sh
@@ -1,7 +1,7 @@
 # !/bin/bash
 
-echo "Installing dependencies and building the app..."
-yarn && yarn build && yarn build-electron
+# echo "Installing dependencies and building the app..."
+# yarn && yarn build && yarn build-electron
 
 # TESTCAFE_TEST = 1
 
@@ -20,15 +20,15 @@ echo "Check screenshots taken by Testcafe:"
 echo "$(pwd)/tmp/screenshots/welcome-room.png"
 echo "$(pwd)/tmp/screenshots/added-room.png"
 echo "--- Press enter to continue ---"
-read press_enter_to_continue
+# read press_enter_to_continue
 
 echo "Remove TESTCAFE_TEST_ENVS to electron-main.js"
 mv electron-main-original.js electron-main.js
 
 # BINARY_BUILD = 1
 
-echo "Add APP_PACKAGE_ENVS from electron-main.js"
-export APP_PACKAGE_ENVS="process.env.BINARY_BUILD=1";
-awk 'BEGIN{print ENVIRON["APP_PACKAGE_ENVS"]}{print}' build/electron-main.js > build/electron-main-tmp.js
-mv build/electron-main-tmp.js build/electron-main.js
-unset APP_PACKAGE_ENVS
+# echo "Add APP_PACKAGE_ENVS from electron-main.js"
+# export APP_PACKAGE_ENVS="process.env.BINARY_BUILD=1";
+# awk 'BEGIN{print ENVIRON["APP_PACKAGE_ENVS"]}{print}' build/electron-main.js > build/electron-main-tmp.js
+# mv build/electron-main-tmp.js build/electron-main.js
+# unset APP_PACKAGE_ENVS
