f77e2920458593b716ba2dae9e4212f0ceb7785b
Jouni Alanen <jvalanen@gmail.com>
2021-02-05 14:22:43 +0200

    DDA-85: Disable logger + open dev tools

--- src/react/utils/middleware/logger.js
+++ src/react/utils/middleware/logger.js
@@ -1,12 +1,10 @@
-export const initial = (state, action) => {
-  console.log('-------------')
-  console.log('PreviousState:', state)
-  console.log('Action:', action)
+export const initial = (state, action) =>
+  // console.log('-------------')
+  // console.log('PreviousState:', state)
+  // console.log('Action:', action)
 
-  return state
-}
+  state
 
-export const final = (state) => {
-  console.log('NextState:', state)
-  return state
-}
+export const final = (state) =>
+  // console.log('NextState:', state)
+  state
