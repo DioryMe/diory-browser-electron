31ef26c03f032abdbae26860c2be9132f12c1e43
Olli-Pekka Pohjola <olli-pekka.pohjola@houston-inc.com>
2021-02-09 23:08:16 +0200

    DDA-83: Create timeline filter

--- src/react/features/filters/useFilters.js
+++ src/react/features/filters/useFilters.js
@@ -1,8 +1,9 @@
 import { useMemo } from 'react'
-
 import { useStore } from '../../store'
+
 import { useGraphFilteredDiorys } from './graph/useGraphFilteredDiorys'
 import { useMapFilteredDiorys } from './map/useMapFilteredDiorys'
+import { useTimelineFilteredDiorys } from './timeline/useTimelineFilteredDiorys'
 import { useTextFilteredDiorys } from './text/useTextFilteredDiorys'
 
 import { reduceIdsToKeys } from '../../utils/reduceIdsToKeys'
@@ -14,26 +15,27 @@ export const useFilters = () => {
   const graphDioryIds = useGraphFilteredDiorys()
   const mapDioryIds = useMapFilteredDiorys()
   const textDioryIds = useTextFilteredDiorys()
+  const timelineDioryIds = useTimelineFilteredDiorys()
 
   const dioryIds = useMemo(
     () =>
-      (mapDioryIds || graphDioryIds || textDioryIds) &&
+      (mapDioryIds || graphDioryIds || textDioryIds || timelineDioryIds) &&
       Object.keys(diograph)
         .filter((id) => !graphDioryIds || graphDioryIds[id])
         .filter((id) => !mapDioryIds || mapDioryIds[id])
+        .filter((id) => !timelineDioryIds || timelineDioryIds[id])
         .filter((id) => !textDioryIds || textDioryIds[id])
         .map((id) => ({ id }))
         .reduce(reduceIdsToKeys, {}),
-    [diograph, graphDioryIds, mapDioryIds, textDioryIds]
+    [diograph, graphDioryIds, mapDioryIds, textDioryIds, timelineDioryIds]
   )
 
   const diorys = useDiorys(dioryIds)
   return useMemo(
     () =>
       dioryIds && {
-        diory: { id: 'filter', text: 'Filter' },
         diorys,
       },
-    [dioryIds]
+    [dioryIds, diorys]
   )
 }
