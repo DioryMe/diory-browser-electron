209308e5a2d16fe7c8f757d8114cf912a6cdf8cb
Jouni Alanen <jvalanen@gmail.com>
2021-02-11 21:40:39 +0200

    DDA-000: Replace window.processEnv.DIOGRAPH_FOLDER_PATH with tmp/testcafe-diograph-folder

--- electron/preload.js
+++ electron/preload.js
@@ -12,5 +12,4 @@ window.frontendLogger = frontendLogger.functions
 
 window.processEnv = {}
 window.processEnv.TESTCAFE_TEST = process.env.TESTCAFE_TEST
-window.processEnv.DIOGRAPH_FOLDER_PATH = process.env.DIOGRAPH_FOLDER_PATH
 window.processEnv.PWD = process.env.PWD
