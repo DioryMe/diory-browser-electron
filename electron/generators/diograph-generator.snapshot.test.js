edf212b1286fb090d851270fe6c2afa0917905ed
Jouni Alanen <jvalanen@gmail.com>
2021-02-12 11:17:34 +0200

    DDA-85: TEST SETTINGS

--- electron/generators/diograph-generator.snapshot.test.js
+++ electron/generators/diograph-generator.snapshot.test.js
@@ -1,29 +1,29 @@
 const { join } = require('path')
-const glob = require('glob')
-const { v4: uuid } = require('uuid')
+// const glob = require('glob')
+// const { v4: uuid } = require('uuid')
 
 const { generateDiograph } = require('./diograph-generator')
 
-jest.mock('uuid')
+// jest.mock('uuid')
 
-function getNumberOfFilesAndFolders(path) {
-  return glob.sync(join(path, '/**/*')).length
-}
+// function getNumberOfFilesAndFolders(path) {
+//   return glob.sync(join(path, '/**/*')).length
+// }
 
-function getArray(length) {
-  return [...Array(length).keys()]
-}
+// function getArray(length) {
+//   return [...Array(length).keys()]
+// }
 
 describe('diograph-generator', () => {
   describe('generateDiograph', () => {
-    it.skip('generates diograph from example folder', async () => {
+    it('generates diograph from example folder', async () => {
       const exampleFolderPath = join(__dirname, '../readers/example-folder')
-      const amountOfCases = getNumberOfFilesAndFolders(exampleFolderPath) + 1
-      getArray(amountOfCases).forEach((id) => {
-        uuid.mockReturnValueOnce(`uuid-${id}`)
-      })
+      // const amountOfCases = getNumberOfFilesAndFolders(exampleFolderPath) + 1
+      // getArray(amountOfCases).forEach((id) => {
+      //   uuid.mockReturnValueOnce(`uuid-${id}`)
+      // })
 
-      const { diograph } = await generateDiograph(exampleFolderPath)
+      const diograph = await generateDiograph(exampleFolderPath)
 
       expect(diograph).toMatchSnapshot()
     })
