72d280943ca8dadf63751c0b6930d54f0395ae04
Jouni Alanen <jvalanen@gmail.com>
2021-02-12 10:04:56 +0200

    DDA-000: Add yarn build to e2e script

--- run-e2e-tests.sh
+++ run-e2e-tests.sh
@@ -3,17 +3,20 @@
 # Always cleanup in the end, ignore failures
 set +e
 
+# Build frontend changes before running tests (although not necessary on CI pipeline)
+yarn build
+
 echo "Add TESTCAFE_TEST_ENVS to electron-main.js"
 awk 'BEGIN{print "process.env.TESTCAFE_TEST=1;" ENVIRON["TESTCAFE_TEST_ENV"]}{print}' electron-main.js > electron-main-tmp.js
 cp electron-main.js electron-main-original.js
 mv electron-main-tmp.js electron-main.js
 
-echo "Run Testcafe E2E test 1 (with development-content-room)"
+echo "Run Testcafe E2E test 1 (with development-content-room, with diograph.json)"
 cp -r public/development-content-room/ tmp/testcafe-diograph-folder/
 npx testcafe "electron:." electron/spec/testcafe-e2e-1.test.js
 rm -rf tmp/testcafe-diograph-folder
 
-echo "Run Testcafe E2E test 2 (with example-folder)"
+echo "Run Testcafe E2E test 2 (with example-folder, no diograph.json)"
 cp -r electron/readers/example-folder/ tmp/testcafe-diograph-folder/
 npx testcafe "electron:." electron/spec/testcafe-e2e-2.test.js
 rm -rf tmp/testcafe-diograph-folder
