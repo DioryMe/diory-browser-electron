e40aca9ebde5b554c3b77747385ee8adbf241845
Jouni Alanen <jvalanen@gmail.com>
2021-02-12 06:28:03 +0200

    DDA-000: Use same script in local and on CI pipeline

--- run-e2e-tests.sh
+++ run-e2e-tests.sh
@@ -1,12 +1,12 @@
 # !/bin/bash
+
+# Always cleanup in the end, ignore failures
 set +e
 
 echo "Add TESTCAFE_TEST_ENVS to electron-main.js"
-export TESTCAFE_TEST_ENVS="process.env.TESTCAFE_TEST=1";
-awk 'BEGIN{print ENVIRON["TESTCAFE_TEST_ENVS"]}{print}' electron-main.js > electron-main-tmp.js
+awk 'BEGIN{print "process.env.TESTCAFE_TEST=1;" ENVIRON["TESTCAFE_TEST_ENV"]}{print}' electron-main.js > electron-main-tmp.js
 cp electron-main.js electron-main-original.js
 mv electron-main-tmp.js electron-main.js
-unset TESTCAFE_TEST_ENVS
 
 echo "Run Testcafe E2E test 1 (with development-content-room)"
 cp -r public/development-content-room/ tmp/testcafe-diograph-folder/
@@ -24,5 +24,5 @@ rm -rf tmp/testcafe-diograph-folder
 # npx testcafe "electron:." electron/spec/testcafe-e2e-3.test.js
 # rm -rf tmp/testcafe-diograph-folder
 
-echo "Remove TESTCAFE_TEST_ENVS to electron-main.js"
+echo "Remove TESTCAFE_TEST_ENV from electron-main.js"
 mv electron-main-original.js electron-main.js
