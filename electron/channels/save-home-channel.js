e3deaf8c4f8c56bf66e9e5840947fcea45cd1a45
Jouni Alanen <jvalanen@gmail.com>
2021-02-13 19:10:47 +0200

    DDA-85: Convert channels to await/async + enforce exports. & function() convention

--- electron/channels/save-home-channel.js
+++ electron/channels/save-home-channel.js
@@ -1,12 +1,10 @@
 const HomeStore = require('electron-store')
 
-exports.saveHomeEventHandler = (event, home) => {
+exports.saveHomeEventHandler = async function (event, home) {
   const store = new HomeStore({
     cwd: process.env.TESTCAFE_TEST ? `${process.env.PWD}/tmp/${Date.now()}` : undefined,
   })
   store.set({ home })
 
-  return new Promise((resolve, reject) => {
-    resolve(true)
-  })
+  return true
 }
