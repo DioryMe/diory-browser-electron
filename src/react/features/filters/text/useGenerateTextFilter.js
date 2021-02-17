ed32a81e93182ccca5af8677a8eab12c08a4f4ac
Olli-Pekka Pohjola <olli-pekka.pohjola@houston-inc.com>
2021-02-18 00:29:44 +0200

    DDA-83: Require diory text in text filter

--- src/react/features/filters/text/useGenerateTextFilter.js
+++ src/react/features/filters/text/useGenerateTextFilter.js
@@ -3,7 +3,5 @@ import { useFilter } from '../hooks/useFilter'
 export const useGenerateTextFilter = () => {
   const { active, query = '' } = useFilter('text')
   return ({ text }) =>
-    !active ||
-    !query ||
-    (text && text.toLowerCase().includes(query.toLowerCase()))
+    !active || !query || (!!text && text.toLowerCase().includes(query.toLowerCase()))
 }
