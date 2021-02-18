37535d0fa85e463f7331aa1c34af86aa9ac37638
Jouni Alanen <jvalanen@gmail.com>
2021-02-18 18:00:01 +0200

    DDA-85: generatePathList helper to generate { path: '/', diory: dioryObject } listing

--- electron/channels/compare-and-merge-diographs.js
+++ electron/channels/compare-and-merge-diographs.js
@@ -14,24 +14,26 @@ exports.compareAndMergeDiographs = function compareAndMergeDiographs(
 
 function compareDiographs(existingDiograph, folderStructureDiograph) {
   let { diograph, rootId } = folderStructureDiograph
-  const newRootDiory = diograph[rootId]
-  const newRootDioryPath = '/'
-  const newRootDioryLinkedDiories = Object.entries(newRootDiory.links).map(([key, { id }]) => ({
-    path: `${newRootDioryPath}${key}`,
-    diory: diograph[id],
-  }))
+  const folderStructureDiographPathList = generatePathList(rootId, '/', diograph)
 
   ;({ diograph, rootId } = existingDiograph)
-  const existingRootDiory = diograph[rootId]
-  const existingRootDioryPath = '/'
-  const existingRootDioryLinkedDiories = Object.entries(
-    existingRootDiory.links
-  ).map(([key, { id }]) => ({ path: `${existingRootDioryPath}${key}`, diory: diograph[id] }))
+  const existingDiographPathList = generatePathList(rootId, '/', diograph)
 
-  console.log(existingRootDioryLinkedDiories.map((jee) => jee.path))
-  console.log(newRootDioryLinkedDiories.map((jee) => jee.path))
+  const paths1 = folderStructureDiographPathList.map((p) => p.path)
+  const paths2 = existingDiographPathList.map((p) => p.path)
+  console.log(paths1)
+  console.log(paths2)
+  return 'jee' // paths1.filter(path => !paths2.includes(path))
+}
 
-  return 'jee'
+function generatePathList(rootId, path, diograph) {
+  const rootDiory = diograph[rootId]
+  const linkedDioryPaths = Object.entries(rootDiory.links).map(([key, { id }]) => ({
+    path: `${path}${key}`,
+    diory: diograph[id],
+  }))
+  linkedDioryPaths.push({ path: '/', diory: rootDiory })
+  return linkedDioryPaths
 }
 
 function addDioriesToDiograph(diories, diograph) {
