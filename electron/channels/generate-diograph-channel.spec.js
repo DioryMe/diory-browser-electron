61d6a0db1830bd2f76fe425482d1964c5ca21d48
Jouni Alanen <jvalanen@gmail.com>
2021-02-18 15:16:42 +0200

    DDA-85: Cleanup generate-diograph-channel.spec.js

--- electron/channels/generate-diograph-channel.spec.js
+++ electron/channels/generate-diograph-channel.spec.js
@@ -14,7 +14,9 @@ describe('generateDiographEventHandler', () => {
   let act
   let params
   let someDiograph
-  let responseObject
+  let mergedDiograph
+  let someDiographResponse
+  let mergedDiographResponse
 
   beforeEach(() => {
     params = 'some-path'
@@ -22,67 +24,69 @@ describe('generateDiographEventHandler', () => {
       rootId: 'some-root-diory-id',
       diograph: { 'some-root-diory-id': 'some-diory-object' },
     }
-    responseObject = {
+    mergedDiograph = {
+      rootId: 'some-root-diory-id2',
+      diograph: { 'some-root-diory-id2': 'some-diory-object2' },
+    }
+    someDiographResponse = {
       ...someDiograph,
+      path: 'some-path',
+    }
+    mergedDiographResponse = {
+      ...mergedDiograph,
       path: 'some-path',
     }
     act = async () =>
       eventHandlerWrapper('GENERATE_DIOGRAPH', generateDiographEventHandler)('some-event', params)
   })
 
+  afterEach(() => {
+    expect(readDiographJson).toHaveBeenCalledTimes(1)
+    expect(readDiographJson).toHaveBeenCalledWith(params)
+    expect(generateDiograph).toHaveBeenCalledTimes(1)
+    expect(generateDiograph).toHaveBeenCalledWith(params)
+    expect(saveDiographJson).toHaveBeenCalledTimes(1)
+  })
+
   describe('GIVEN diograph.json not found (readDiographJson returns undefined)', () => {
     beforeEach(() => {
-      saveDiographJson.mockResolvedValue(undefined)
       readDiographJson.mockReturnValue(undefined)
+      saveDiographJson.mockResolvedValue(undefined)
       generateDiograph.mockResolvedValue(someDiograph)
     })
 
     it('returns generateDiograph return value + path', async () => {
       const response = await act()
-      expect(response).toEqual(responseObject)
 
-      expect(generateDiograph).toHaveBeenCalledTimes(1)
-      expect(generateDiograph).toHaveBeenCalledWith(params)
-
-      expect(saveDiographJson).toHaveBeenCalledTimes(1)
+      expect(compareAndMergeDiographs).not.toHaveBeenCalled()
       expect(saveDiographJson).toHaveBeenCalledWith(
         params,
         someDiograph.diograph,
         someDiograph.rootId
       )
+      expect(response).toEqual(someDiographResponse)
     })
   })
 
   describe('GIVEN diograph.json found (readDiographJson returns diograph)', () => {
     beforeEach(() => {
-      saveDiographJson.mockResolvedValue(undefined)
+      generateDiograph.mockResolvedValue('generated-diograph')
+      compareAndMergeDiographs.mockReturnValue(mergedDiograph)
       readDiographJson.mockReturnValue(someDiograph)
+      saveDiographJson.mockResolvedValue(undefined)
     })
 
-    describe('WHEN matching diograph.json and generated diograph', () => {
-      beforeEach(() => {
-        generateDiograph.mockResolvedValue(someDiograph)
-        compareAndMergeDiographs.mockReturnValue(someDiograph)
-      })
+    it('returns compareAndMergeDiographs return value + path', async () => {
+      const response = await act()
 
-      it('returns readDiographJson return value + path', async () => {
-        const response = await act()
-
-        expect(response).toEqual(responseObject)
-      })
-    })
-
-    describe('WHEN non-matching diograph.json and generated diograph', () => {
-      beforeEach(() => {
-        generateDiograph.mockResolvedValue('some-diograph')
-        compareAndMergeDiographs.mockReturnValue(someDiograph)
-      })
-
-      it('returns compareAndMergeDiographs return value + path', async () => {
-        const response = await act()
-
-        expect(response).toEqual(responseObject)
-      })
+      expect(compareAndMergeDiographs).toHaveBeenCalledTimes(1)
+      expect(compareAndMergeDiographs).toHaveBeenCalledWith(someDiograph, 'generated-diograph')
+      expect(saveDiographJson).toHaveBeenCalledWith(
+        params,
+        mergedDiograph.diograph,
+        mergedDiograph.rootId
+      )
+      expect(response).toEqual(mergedDiographResponse)
     })
   })
 })
