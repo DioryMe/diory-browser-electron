5f4658fa427e5e101dd18982c57e2cce3ed5f0bd
Olli-Pekka Pohjola <olli-pekka.pohjola@houston-inc.com>
2021-02-18 23:28:10 +0200

    TECH: Refactor lenses bar

--- src/react/features/lenses/LensesBarView.js
+++ src/react/features/lenses/LensesBarView.js
@@ -1,50 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Button, Tab, Pane, Tablist, Icon } from 'evergreen-ui'
+import { Pane, Tablist } from 'evergreen-ui'
+import LensButton from '../../components/LensButton'
 
 const LensesBarView = ({ lenses }) => (
   <Pane display="flex">
     <Tablist alignSelf="center">
-      {lenses.map(({ diory, isSelected, isFiltered, onSelect, onRemove, ...lens }) =>
-        isFiltered ? (
-          <Button
-            {...lens}
-            onClick={onSelect}
-            appearance="primary"
-            borderRadius="16px"
-            alignSelf="center"
-            opacity={isSelected ? 1 : 0.7}
-            marginRight={8}
-            paddingLeft={14}
-            paddingRight={14}
-            cursor="pointer"
-            aria-controls={`panel-${lens.id}`}
-            data-testid={`${lens.id}-lens`}
-          >
-            <Icon icon={diory.image} marginRight={8} size={16} />
-            {diory.text}
-            <Icon icon="small-cross" marginLeft={8} size={16} onClick={onRemove} />
-          </Button>
-        ) : (
-          <Tab
-            {...lens}
-            onSelect={onSelect}
-            isSelected={isSelected}
-            style={{ verticalAlign: 'middle' }}
-            borderRadius="16px"
-            alignSelf="center"
-            padding={12}
-            marginRight={8}
-            cursor="pointer"
-            aria-controls={`panel-${lens.id}`}
-            data-testid={`${lens.id}-lens`}
-          >
-            <Icon icon={diory.image} marginRight={8} size={16} />
-            {diory.text}
-            {isSelected && <Icon icon="small-plus" marginLeft={8} size={16} />}
-          </Tab>
-        )
-      )}
+      {lenses.map((lens) => (
+        <LensButton {...lens} />
+      ))}
     </Tablist>
   </Pane>
 )
