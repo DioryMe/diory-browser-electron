adee1356c08aae0ae791b20db216142742300fdb
Jouni Alanen <jvalanen@gmail.com>
2021-02-18 15:28:50 +0200

    DDA-85: Another initial test for compareAndMergeDiographs which uses fixture jsons

--- electron/channels/compare-and-merge-diographs.spec.js
+++ electron/channels/compare-and-merge-diographs.spec.js
@@ -1,4 +1,7 @@
+const fs = require('fs')
 const { compareAndMergeDiographs } = require('./compare-and-merge-diographs')
+
+const currentPath = './electron/channels'
 
 describe('compareAndMergeDiographs', () => {
   let act
@@ -10,5 +13,12 @@ describe('compareAndMergeDiographs', () => {
   it('works', () => {
     const response = act()
     expect(response).toEqual('existing-diograph')
+  })
+
+  it('works with fixture jsons', () => {
+    const existingDiograph = JSON.parse(fs.readFileSync(`${currentPath}/diograph-new.json`))
+    const newDiograph = JSON.parse(fs.readFileSync(`${currentPath}/diograph-existing.json`))
+    const response = compareAndMergeDiographs(existingDiograph, newDiograph)
+    expect(response).toEqual(existingDiograph)
   })
 })
