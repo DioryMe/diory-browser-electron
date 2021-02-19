c1b7b2d32dbfc84f3b629468e2bfb201b4dab9e7
Jouni Alanen <jvalanen@gmail.com>
2021-02-19 11:44:27 +0200

    DDA-85: Fix compare-and-merge-diographs.spec.js => check with json's from disk instead of objects (that are mutated by compareAndMergeDiographs...)

--- electron/channels/compare-and-merge-diographs.spec.js
+++ electron/channels/compare-and-merge-diographs.spec.js
@@ -2,30 +2,37 @@ const fs = require('fs')
 const { compareAndMergeDiographs } = require('./compare-and-merge-diographs')
 
 // Load static json fixtures
-const currentPath = './electron/channels'
-const originalDiograph = JSON.parse(fs.readFileSync(`${currentPath}/diograph-existing.json`))
-const newDiographSame = JSON.parse(fs.readFileSync(`${currentPath}/diograph-new-same.json`))
-const newDiographAddedDiories = JSON.parse(
-  fs.readFileSync(`${currentPath}/diograph-new-added-diories.json`)
-)
+function jsonDiograph(filename) {
+  return JSON.parse(fs.readFileSync(`./electron/channels/diograph-${filename}.json`))
+}
 
 describe('compareAndMergeDiographs', () => {
   describe('GIVEN diograph generated from same folder structure', () => {
     it('returns original diograph', () => {
-      const response = compareAndMergeDiographs(originalDiograph, newDiographSame)
-      expect(response).toEqual(originalDiograph)
-      expect(response.diograph).toEqual(originalDiograph.diograph)
-      expect(response.rootId).toEqual(originalDiograph.rootId)
+      const response = compareAndMergeDiographs(jsonDiograph('existing'), jsonDiograph('new-same'))
+      expect(response).toEqual(jsonDiograph('existing'))
     })
   })
 
   describe('GIVEN diograph with added diories', () => {
-    it('works with fixture jsons generated from changed folder structure (Tampere folder added)', () => {
-      const response = compareAndMergeDiographs(originalDiograph, newDiographAddedDiories)
+    let response
+
+    beforeEach(() => {
+      response = compareAndMergeDiographs(
+        jsonDiograph('existing'),
+        jsonDiograph('new-added-diories')
+      )
+      expect(response.rootId).toEqual(jsonDiograph('existing').rootId)
+    })
+
+    // Tämän tekee snapshot testi:
+    // expect(response.diograph).toEqual({ ...originalDiograph.diograph, ...tampereDiory, ...mp4Diory })
+
+    it('adds Tampere diory and links it to the root diory', () => {
+      const newDiographAddedDiories = jsonDiograph('new-added-diories')
       const tampereId = '43563e8d-9599-46b7-b011-a90450fe4c81'
-      const tampereDiory = { [tampereId]: newDiographAddedDiories.diograph[tampereId] }
-      expect(response.diograph).toEqual({ ...originalDiograph.diograph, ...tampereDiory })
-      expect(response.rootId).toEqual(originalDiograph.rootId)
+      const tampereDiory = newDiographAddedDiories.diograph[tampereId]
+      expect(response.diograph[tampereId]).toEqual(tampereDiory)
       expect(response.diograph[response.rootId].links.Tampere).toEqual({ id: tampereId })
     })
   })
