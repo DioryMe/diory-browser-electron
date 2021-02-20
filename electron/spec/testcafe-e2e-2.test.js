65f9d1a7ad2ae971eb71929f96a3e7562892070f
Jouni Alanen <jvalanen@gmail.com>
2021-02-20 10:48:39 +0200

    DDA-83: Extend E2E test to test also SAVE_HOME

--- electron/spec/testcafe-e2e-2.test.js
+++ electron/spec/testcafe-e2e-2.test.js
@@ -3,8 +3,10 @@ import { Selector } from 'testcafe'
 // eslint-disable-next-line no-undef
 fixture`GIVEN welcome room`.page('../../build/index.html')
 
-test('select new diograph folder (without diograph.json)', async (t) => {
+test('select new diograph folder (without diograph.json) and delete it', async (t) => {
   const dioryCount = Selector('.ub-flx_1-0-360px').count
+  const exampleFolderRoomDiory = Selector('div').withExactText('testcafe-diograph-folder')
+  const welcomeRoomDiory = Selector('div').withExactText('Welcome room!')
 
   await t
     .expect(dioryCount)
@@ -14,4 +16,10 @@ test('select new diograph folder (without diograph.json)', async (t) => {
     .click('[data-testid="undefined-button"]')
     .expect(dioryCount)
     .eql(5)
+    .click('[data-testid="home"]')
+    .click('[data-testid="tools-button"]')
+    .click('[data-testid="home-remove-room-button"]')
+    .click(exampleFolderRoomDiory)
+    .wait(1000) // Wait for SAVE_HOME to complete and possibly raise error
+    .click(welcomeRoomDiory)
 })
