DioryMe/diory-browser-electron

commit 2aa216f8ccf8a23c94b492678dc2156f741b63dd

DDA-81: Rename drop callback props for clarity

src/react/features/lenses/grid/GridLens.js
@@ -24,8 +24,8 @@ const useTools = () => {
       deleteDiory(diory)
       updateDiory(diory)
     },
-    onDrop: ({ focus, link }) => {
-      dispatch(createLink(focus, link))
+    onDrop: ({ dropId, dragId }) => {
+      dispatch(createLink({ id: dropId }, {  id: dragId }))
     },
   }
 }

src/react/features/lenses/grid/GridView.js
@@ -11,7 +11,7 @@ const GridView = ({ diory, diorys, onDrop, onClick }) => (
   <Droppable
     type={types.DIORY}
     isOverStyle={{ backgroundColor: '#D4EEE2' }}
-    onDrop={({ id }) => onDrop({ focus: diory, link: { id } })}
+    onDrop={({ id }) => onDrop({ droppedId: diory.id, draggedId: id })}
   >
     <BackgroundDiory diory={diory} gradient onClick={onClick}>
       {diorys.map((linkDiory) => (
@@ -20,7 +20,7 @@ const GridView = ({ diory, diorys, onDrop, onClick }) => (
             type={types.DIORY}
             style={{ height: '100%' }}
             isOverStyle={{ opacity: 0.5 }}
-            onDrop={({ id }) => onDrop({ focus: linkDiory, link: { id } })}
+            onDrop={({ id }) => onDrop({ droppedId: linkDiory.id, draggedId: id })}
           >
             <Draggable id={linkDiory.id} type={types.DIORY}>
               <Diory

commit 4591b3e4e742075737cbec23c72d4499ffabb559

rc-2021-02: Fix hand tool bug caused by 2aa216f8ccf8

src/react/features/lenses/grid/GridLens.js
@@ -24,8 +24,8 @@ const useTools = () => {
       deleteDiory(diory)
       updateDiory(diory)
     },
-    onDrop: ({ dropId, dragId }) => {
-      dispatch(createLink({ id: dropId }, { id: dragId }))
+    onDrop: ({ droppedId, draggedId }) => {
+      dispatch(createLink({ id: droppedId }, { id: draggedId }))
     },
   }
 }