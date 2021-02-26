79a724abbfd930390adae501fc57dfff3ec2665e
Jouni Alanen <jvalanen@gmail.com>
2021-02-26 11:49:25 +0200

    DDA-83: BUG: Add getUntrackedDiograph() to clone the diograph from store and resolve Proxy objects deeply

--- src/react/features/connectors/effects/useSaveRoomEffect.js
+++ src/react/features/connectors/effects/useSaveRoomEffect.js
@@ -7,6 +7,20 @@ import { useConnections } from '../useConnections'
 import { invokeChannel } from '../../../client/client'
 import { channels } from '../../../../shared/constants'
 
+// Makes a copy of storeDiograph and deeply resolves the Proxy objects
+function getUntrackedDiograph(diograph) {
+  const untrackedDiograph = { ...getUntrackedObject(diograph) }
+  if (untrackedDiograph) {
+    Object.entries(untrackedDiograph).forEach(([key, value]) => {
+      const untrackedLinks = getUntrackedObject(untrackedDiograph[key].links)
+      if (untrackedLinks) {
+        untrackedDiograph[key].links = { ...untrackedLinks }
+      }
+    })
+  }
+  return untrackedDiograph
+}
+
 export const useSaveRoomEffect = (connectorId) => {
   const [{ rootId, diograph, updated }] = useStore((state) => state.diograph)
   const { connected } = useConnections(connectorId)
@@ -19,7 +33,7 @@ export const useSaveRoomEffect = (connectorId) => {
           () =>
             invokeChannel(channels.SAVE_ROOM, {
               path: address,
-              room: { rootId, diograph: getUntrackedObject(diograph) },
+              room: { rootId, diograph: getUntrackedDiograph(diograph) },
             }),
           saveRoom
         )
