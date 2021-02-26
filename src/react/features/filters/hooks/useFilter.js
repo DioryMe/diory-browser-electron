a67dcf5e68437abb7302cf43bf10055a91109295
Olli-Pekka Pohjola <olli-pekka.pohjola@houston-inc.com>
2021-02-26 22:03:11 +0200

    DDA-83: Inactivate all filters on focus change

--- src/react/features/filters/hooks/useFilter.js
+++ src/react/features/filters/hooks/useFilter.js
@@ -1,6 +1,20 @@
-import { useStore } from '../../../store'
+import { useEffect } from 'react'
+import { useDispatchActions, useStore } from '../../../store'
+import { activateFilter } from '../actions'
+
+const useInactivateFilterOnFocusChange = (filterId) => {
+  const [{ focus }] = useStore((state) => state.navigation)
+  const { dispatch } = useDispatchActions()
+  useEffect(() => {
+    if (focus) {
+      dispatch(activateFilter(filterId, false))
+    }
+  }, [focus, filterId])
+}
 
 export const useFilter = (filterId) => {
+  useInactivateFilterOnFocusChange(filterId)
+
   const [{ filters }] = useStore((state) => state.filters)
   return filters[filterId] || {}
 }
