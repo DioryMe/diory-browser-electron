7a3893e5154c0a5322f65a6c159499b028341b54
Olli-Pekka Pohjola <olli-pekka.pohjola@houston-inc.com>
2021-03-01 13:52:24 +0200

    TECH: Extract effect from filters hook to prevent multiple executions

--- src/react/features/filters/Filters.js
+++ src/react/features/filters/Filters.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import GraphFilter from './graph/GraphFilter'
+import MapFilter from './map/MapFilter'
+import TimelineFilter from './timeline/TimelineFilter'
 
 const Filters = () => (
   <div data-testid="filters">
     <GraphFilter />
+    <MapFilter />
+    <TimelineFilter />
   </div>
 )
 
--- src/react/features/filters/hooks/useFilter.js
+++ src/react/features/filters/hooks/useFilter.js
@@ -1,20 +1,6 @@
-import { useEffect } from 'react'
-import { useDispatchActions, useStore } from '../../../store'
-import { activateFilter } from '../actions'
-
-const useInactivateFilterOnFocusChange = (filterId) => {
-  const [{ focus }] = useStore((state) => state.navigation)
-  const { dispatch } = useDispatchActions()
-  useEffect(() => {
-    if (focus) {
-      dispatch(activateFilter(filterId, false))
-    }
-  }, [focus, filterId])
-}
+import { useStore } from '../../../store'
 
 export const useFilter = (filterId) => {
-  useInactivateFilterOnFocusChange(filterId)
-
   const [{ filters }] = useStore((state) => state.filters)
   return filters[filterId] || {}
 }
