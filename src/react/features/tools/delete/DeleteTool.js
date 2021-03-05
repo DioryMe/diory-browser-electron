de8a434ecc79b4fe9ebbfebedded5d60ef89c314
Jouni Alanen <jvalanen@gmail.com>
2021-03-05 15:22:33 +0200

    DDA-103: Old functionality implemented with confirmation dialog & DeleteView/DeleteTool etc.

--- src/react/features/tools/delete/DeleteTool.js
+++ src/react/features/tools/delete/DeleteTool.js
@@ -1,9 +1,6 @@
 import React from 'react'
 import { useStore } from '../../../store'
-// import { useDispatchActions, useStore } from '../../../store'
-
-// import { deleteDiory } from '../../diograph/actions'
-import { useLinkDiory } from '../../diograph/hooks'
+import { useLinkDiory, useFocusDiory } from '../../diograph/hooks'
 
 import DeleteView from './DeleteView'
 
@@ -12,24 +9,12 @@ import { DELETE_TOOL_BUTTON } from './buttons'
 const DeleteTool = () => {
   const [{ active }] = useStore((state) => state.buttons)
   const [{ link }] = useStore((state) => state.navigation)
-  const { diory } = useLinkDiory()
+  const linkDiory = useLinkDiory().diory
+  const { diory } = useFocusDiory()
 
-  // const { dispatchAction } = useDispatchActions()
   return DELETE_TOOL_BUTTON === active && !!link ? (
-    <DeleteView diory={diory} onDone={() => alert('jee')} /> // {dispatchAction(deleteDiory)} />
+    <DeleteView diory={linkDiory} focus={diory} />
   ) : null
 }
 
 export default DeleteTool
-
-// import { setInactive } from '../../buttons/actions'
-// import { goBackward } from '../../navigation/actions'
-// import { deleteDiory, deleteLink } from '../../diograph/actions'
-
-// if (diory.id !== clickedDiory.id) {
-//   dispatch(deleteLink(diory, clickedDiory))
-// } else {
-//   dispatch(goBackward())
-//   dispatch(deleteDiory(clickedDiory))
-// }
-// dispatch(setInactive())
