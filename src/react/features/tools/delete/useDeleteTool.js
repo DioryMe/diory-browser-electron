12e995b7a82e4783bd0dc336ae3ff62fe4d5bc35
Jouni Alanen <jvalanen@gmail.com>
2021-03-05 14:43:21 +0200

    DDA-103: Delete tool confirmation dialog appears in UI

--- src/react/features/tools/delete/useDeleteTool.js
+++ src/react/features/tools/delete/useDeleteTool.js
@@ -1,26 +1,20 @@
 import { useDispatch, useStore } from '../../../store'
 import { useButtons } from '../../buttons'
-import { inactivateButton } from '../../buttons/actions'
-import { goBackward } from '../../navigation/actions'
-import { deleteDiory, deleteLink } from '../../diograph/actions'
-import { useFocus } from '../../diograph/hooks'
+
+import { setSelectedLink } from '../../navigation/actions'
 import { buttons, DELETE_TOOL_BUTTON } from './buttons'
+// import { useFocus } from '../../diograph/hooks'
 
 export const useDeleteTool = () => {
   useButtons(buttons)
 
   const [{ active }] = useStore((state) => state.buttons)
-  const { diory } = useFocus()
+
+  // const { diory } = useFocus()
   const dispatch = useDispatch()
   return (clickedDiory) => {
     if (DELETE_TOOL_BUTTON === active) {
-      if (diory.id !== clickedDiory.id) {
-        dispatch(deleteLink(diory, clickedDiory))
-      } else {
-        dispatch(goBackward())
-        dispatch(deleteDiory(clickedDiory))
-      }
-      dispatch(inactivateButton())
+      dispatch(setSelectedLink(clickedDiory))
     }
   }
 }
