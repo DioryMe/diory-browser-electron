5fc873548234d33f77a910dd803274b73d702156
Jouni Alanen <jouni.alanen@checkout.fi>
2021-03-11 09:41:29 +0200

    DDA-103: DDA-103: Fix useFocusDiory -> useFocus after rebase

--- src/react/features/tools/delete/DeleteTool.js
+++ src/react/features/tools/delete/DeleteTool.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useStore } from '../../../store'
-import { useLinkDiory, useFocusDiory } from '../../diograph/hooks'
+import { useLinkDiory, useFocus } from '../../diograph/hooks'
 
 import DeleteView from './DeleteView'
 
@@ -10,7 +10,7 @@ const DeleteTool = () => {
   const [{ active }] = useStore((state) => state.buttons)
   const [{ link }] = useStore((state) => state.navigation)
   const linkDiory = useLinkDiory().diory
-  const { diory } = useFocusDiory()
+  const { diory } = useFocus()
 
   return DELETE_TOOL_BUTTON === active && !!link ? (
     <DeleteView diory={linkDiory} focus={diory} />
