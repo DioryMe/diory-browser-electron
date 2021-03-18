b2b07561ed960d521f3f029c212d4d78fc074c43
Jouni Alanen <jouni@diory.me>
2021-03-18 14:17:08 +0200

    DDA-103: Remove also links to reverseLinkedDiories when isFocusDeleted

--- src/react/features/tools/delete/useDeleteDioryAndLinks.js
+++ src/react/features/tools/delete/useDeleteDioryAndLinks.js
@@ -3,31 +3,45 @@ import { setInactive } from '../../buttons/actions'
 import { goBackward } from '../../navigation/actions'
 import { deleteDiory, deleteLink } from '../../diograph/actions'
 
-const generateDeletedLinks = (focus, links) => {
+const generateDeletedLinks = (focus, links, isFocusDeleted) => {
   const [{ diograph }] = useStore((state) => state.diograph)
-  return Object.values(links).map(({ id }) => ({
+
+  const linkedDiories = Object.values(links).map(({ id }) => ({
     fromDiory: diograph[focus.id],
     toDiory: diograph[id],
   }))
+
+  let reverseLinkedDiories = []
+  if (isFocusDeleted) {
+    reverseLinkedDiories = Object.values(diograph)
+      .map((diory) => {
+        const reverseLinkedDiory = diory.links
+          ? Object.entries(diory.links).filter(([, { id }]) => id === focus.id)[0]
+          : null
+        return reverseLinkedDiory
+          ? {
+              fromDiory: diograph[reverseLinkedDiory[1].id],
+              toDiory: diograph[focus.id],
+            }
+          : null
+      })
+      .filter(Boolean)
+  }
+
+  return linkedDiories.concat(reverseLinkedDiories)
 }
 
 export const useDeleteDioryAndLinks = (focus, clickedDiory) => {
-  const deleteDioryInFocus = focus.id === clickedDiory.id
-  const deletedDiory = deleteDioryInFocus ? focus : null
-  const links = deleteDioryInFocus
-    ? focus.links || {}
-    : { [clickedDiory.id]: { id: clickedDiory.id } }
-  const deletedLinks = generateDeletedLinks(focus, links)
+  const isFocusDeleted = focus.id === clickedDiory.id
+  const deletedDiory = isFocusDeleted ? focus : null
+  const links = isFocusDeleted ? focus.links || {} : { [clickedDiory.id]: { id: clickedDiory.id } }
+  const deletedLinks = generateDeletedLinks(focus, links, isFocusDeleted)
 
   const dispatch = useDispatch()
   return {
     deletedDiory,
     deletedLinks,
     deleteDioryAndLinks: () => {
-      if (deletedDiory) {
-        dispatch(deleteDiory(deletedDiory))
-      }
-
       deletedLinks.forEach((deletedLink) => {
         if (deletedLink.fromDiory.id === deletedLink.toDiory.id) {
           return
@@ -35,7 +49,8 @@ export const useDeleteDioryAndLinks = (focus, clickedDiory) => {
         dispatch(deleteLink(deletedLink.fromDiory, deletedLink.toDiory))
       })
 
-      if (deleteDioryInFocus) {
+      if (deletedDiory) {
+        dispatch(deleteDiory(deletedDiory))
         dispatch(goBackward())
       }
 
