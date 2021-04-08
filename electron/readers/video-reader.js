8a065d131884174d6009ad46e8e9519c0d9eddbf
Jouni Alanen <jvalanen@gmail.com>
2021-04-08 15:24:57 +0300

    DDA-118: Schema for VideoObjects

--- electron/readers/video-reader.js
+++ electron/readers/video-reader.js
@@ -1,6 +1,14 @@
 const { basename } = require('path')
 const { pathToFileURL } = require('url')
 
+function generateSchema(videoPath) {
+  return {
+    '@context': 'https://schema.org',
+    '@type': 'VideoObject',
+    contentUrl: pathToFileURL(videoPath).toString(),
+  }
+}
+
 exports.readVideo = function readVideo(videoPath) {
   if (!videoPath) {
     return
@@ -9,11 +17,11 @@ exports.readVideo = function readVideo(videoPath) {
   try {
     return {
       text: basename(videoPath),
-      video: pathToFileURL(videoPath),
-      // ...getDate(tags),
-      // ...getLatitude(tags),
-      // ...getLongitude(tags),
-      // ...getCreated(tags),
+      // TODO: Remove video attribute as now we have diory.data.contentUrl
+      video: pathToFileURL(videoPath).toString(),
+      data: {
+        ...generateSchema(videoPath),
+      },
     }
   } catch (error) {
     console.info(`Error reading video ${videoPath}: ${error}`)
