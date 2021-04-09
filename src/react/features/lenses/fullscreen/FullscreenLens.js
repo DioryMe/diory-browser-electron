9ef11722c691bd7b35e0572423138aa64abc05cb
Olli-Pekka Pohjola <olli-pekka.pohjola@houston-inc.com>
2021-04-10 00:36:09 +0300

    Revert "POC-113: Show data attributes in fullscreen lens"

--- src/react/features/lenses/fullscreen/FullscreenLens.js
+++ src/react/features/lenses/fullscreen/FullscreenLens.js
@@ -1,32 +1,21 @@
 import React from 'react'
-import DataAwareDiory from '../../../components/diories/DataAwareDiory'
+import Diory from '../../../components/diories/Diory'
 import { useFocus } from '../../diograph/hooks'
-
-const defaultStyle = {
-  image: {
-    backgroundSize: 'contain',
-    backgroundColor: 'black',
-  },
-}
-
-// TODO: To diory utils
-function addStyle({ style = {}, ...props }, addedStyle) {
-  return {
-    ...props,
-    style: {
-      ...style,
-      ...addedStyle,
-      image: {
-        ...(style && style.image),
-        ...addedStyle.image,
-      },
-    },
-  }
-}
 
 const FullscreenLens = () => {
   const { diory } = useFocus()
-  return <DataAwareDiory diory={addStyle(diory, defaultStyle)} height="100%" />
+  const fullScreenDiory = {
+    ...diory,
+    style: {
+      ...(diory && diory.style),
+      image: {
+        ...(diory.style && diory.style.image),
+        backgroundSize: 'contain',
+        backgroundColor: 'black',
+      },
+    },
+  }
+  return <Diory diory={fullScreenDiory} height="100%" />
 }
 
 export default FullscreenLens
