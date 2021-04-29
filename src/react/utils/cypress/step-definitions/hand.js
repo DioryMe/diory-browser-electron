558fab52301e7e3769c4923db0108de223faf819
Jouni Alanen <jouni@diory.me>
2021-04-29 16:32:01 +0300

    DDA-130: 'When I drag diory1 to diory2' helper

--- src/react/utils/cypress/step-definitions/hand.js
+++ src/react/utils/cypress/step-definitions/hand.js
@@ -3,3 +3,7 @@ import { When } from 'cypress-cucumber-preprocessor/steps'
 When('I drag {string} into hand', (dioryId) => {
   cy.get(`#${dioryId}`).drag('[data-testid="hand"]')
 })
+
+When('I drag {string} to {string}', (fromDioryId, toDioryId) => {
+  cy.get(`#${fromDioryId}`).drag(`#${toDioryId}`)
+})
