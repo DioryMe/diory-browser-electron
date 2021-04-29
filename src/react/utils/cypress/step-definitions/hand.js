113a86989e1ce0a5b00162c952674d99859f1edf
Jouni Alanen <jouni@diory.me>
2021-04-29 16:45:45 +0300

    DDA-130: Test for 'Clicking diory in hand'

--- src/react/utils/cypress/step-definitions/hand.js
+++ src/react/utils/cypress/step-definitions/hand.js
@@ -1,5 +1,9 @@
 import { When } from 'cypress-cucumber-preprocessor/steps'
 
+When('I click {string} diory in hand', (dioryId) => {
+  cy.get(`#hand-${dioryId}`).click()
+})
+
 When('I drag {string} into hand', (dioryId) => {
   cy.get(`#${dioryId}`).drag('[data-testid="hand"]')
 })
