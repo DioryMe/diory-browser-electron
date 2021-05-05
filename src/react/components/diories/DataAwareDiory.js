d34faf15e01e149a0b5eb92b36aa7d0c777ee245
Jouni Alanen <jvalanen@gmail.com>
2021-05-05 09:44:09 +0300

    DDA-126: Show DataAwareDiory in FullscreenLens

---
+++ src/react/components/diories/DataAwareDiory.js
@@ -0,0 +1,86 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { getUntrackedObject } from '../../store'
+
+import Diory from './Diory'
+import Image from './Image'
+import Video from './Video'
+import { invokeChannel } from '../../client/client'
+
+const showImage = (props, diory, data) => {
+  const imageStyle = {
+    ...(diory.style && diory.style.image),
+    backgroundSize: 'contain',
+    backgroundColor: 'black',
+  }
+  return (
+    <Image
+      image={diory.data && diory.data.contentUrl}
+      style={imageStyle}
+      gradient={Boolean(diory.text)}
+      gradientRgba="0, 0, 0, 0.2"
+    />
+  )
+}
+
+const showVideo = (props, diory, data) => (
+  <Video video={data && data.contentUrl} style={diory.style && diory.style.video} />
+)
+
+const showInBrowser = (props, diory, data) => (
+  <iframe title="content-in-browser" src={data && data.contentUrl} height="100%" width="100%" />
+)
+
+const showWebpage = (props, diory, data) => (
+  <iframe title="web-browser" src={data && data.url} height="100%" width="100%" />
+)
+
+const showExternalApplicationButton = (props, diory, data) => (
+  <button
+    type="submit"
+    onClick={() => invokeChannel('openInExternalApplication', data && data.contentUrl)}
+  >
+    Open in external application
+  </button>
+)
+
+const DataAwareDiory = (props) => {
+  const { diory } = props
+  const data = getUntrackedObject(diory.data)
+  const mimeType = data && data.encodingFormat
+  switch (mimeType) {
+    case 'image/jpeg':
+      return showImage(props, diory, data)
+    case 'video/mp4':
+    case 'video/x-m4v':
+    case 'video/quicktime':
+      return showVideo(props, diory, data)
+    case 'audio/mpeg':
+    case 'audio/x-m4a':
+    case 'audio/opus':
+      return showInBrowser(props, diory, data)
+    case 'application/pdf':
+      return showInBrowser(props, diory, data)
+    default:
+      if (data && data.url && /^http(s)?:\/\//.exec(data.url)) {
+        return showWebpage(props, diory, data)
+      }
+      if (data && data.contentUrl && /\.html?$/.exec(data.contentUrl)) {
+        return showInBrowser(props, diory, data)
+      }
+      if (data && data.contentUrl) {
+        return showExternalApplicationButton(props, diory, data)
+      }
+      return <Diory {...props} />
+  }
+}
+
+DataAwareDiory.propTypes = {
+  diory: PropTypes.shape({
+    image: PropTypes.string,
+    style: PropTypes.object,
+    data: PropTypes.object,
+  }),
+}
+
+export default DataAwareDiory
--- src/react/features/lenses/fullscreen/FullscreenLens.js
+++ src/react/features/lenses/fullscreen/FullscreenLens.js
@@ -1,21 +1,10 @@
 import React from 'react'
-import Diory from '../../../components/diories/Diory'
+import DataAwareDiory from '../../../components/diories/DataAwareDiory'
 import { useFocus } from '../../diograph/hooks'
 
 const FullscreenLens = () => {
   const { diory } = useFocus()
-  const fullScreenDiory = {
-    ...diory,
-    style: {
-      ...(diory && diory.style),
-      image: {
-        ...(diory.style && diory.style.image),
-        backgroundSize: 'contain',
-        backgroundColor: 'black',
-      },
-    },
-  }
-  return <Diory diory={fullScreenDiory} height="100%" />
+  return <DataAwareDiory diory={diory} height="100%" />
 }
 
 export default FullscreenLens
