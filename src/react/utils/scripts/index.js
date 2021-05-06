0a1c1020c3e9ef5d669493602da77c4946fedb7c
Jouni Alanen <jvalanen@gmail.com>
2021-05-06 15:41:44 +0300

    DDA-000: Remove unused react/utils/scripts folder

--- src/react/utils/scripts/index.js
+++
@@ -1,4 +0,0 @@
-// import flattenDiograph from '../scripts/flatten-diograph'
-// flattenDiograph(diograph)
-// import geoFromDataToDiory from '../scripts/geoFromDataToDiory'
-// geoFromDataToDiory(diograph)
