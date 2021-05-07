ed5294deb04ddc65e19d056f66161f9aff71b396
Jouni Alanen <jvalanen@gmail.com>
2021-05-07 13:59:56 +0300

    DDA-86: Revert Room.js

--- src/react/components/Room.js
+++ src/react/components/Room.js
@@ -1,40 +1,38 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Box from 'ui-box'
-import { useGetImageUrl } from '../utils'
 
-const Room = ({ diory: { id, image, text, ...diory }, onClick }) => {
-  const imageUrl = useGetImageUrl(image)
-  return (
+const Room = ({ diory: { id, image, text, ...diory }, onClick }) => (
+  <Box
+    key={id}
+    flex="1 0 240px"
+    margin={24}
+    borderTop="20px solid rgba(0,0,0,0.1)"
+    borderLeft="25px solid rgba(0,0,0,0.05)"
+    borderRight="25px solid rgba(0,0,0,0.05)"
+    borderBottom="30px solid rgba(0,0,0,0.1)"
+  >
     <Box
-      key={id}
-      flex="1 0 240px"
-      margin={24}
-      borderTop="20px solid rgba(0,0,0,0.1)"
-      borderLeft="25px solid rgba(0,0,0,0.05)"
-      borderRight="25px solid rgba(0,0,0,0.05)"
-      borderBottom="30px solid rgba(0,0,0,0.1)"
+      id={id}
+      {...diory}
+      height={160}
+      alignSelf="center"
+      onClick={onClick}
+      aria-controls={`panel-${id}`}
+      // backgroundImage={`linear-gradient(rgba(0, 0, 0, 0.1),rgba(0, 0, 0, 0.1)), url(${getImageUrl(
+      //   image
+      // )})`}
+      backgroundRepeat="no-repeat"
+      backgroundSize="cover"
+      backgroundColor="pink"
+      boxShadow="inset 0px -2px 50px -5px rgba(0,0,0,0.6), 0px 0px 30px 10px rgba(0,0,0,0.4)"
     >
-      <Box
-        id={id}
-        {...diory}
-        height={160}
-        alignSelf="center"
-        onClick={onClick}
-        aria-controls={`panel-${id}`}
-        backgroundImage={`linear-gradient(rgba(0, 0, 0, 0.1),rgba(0, 0, 0, 0.1)), url(${imageUrl})`}
-        backgroundRepeat="no-repeat"
-        backgroundSize="cover"
-        backgroundColor="pink"
-        boxShadow="inset 0px -2px 50px -5px rgba(0,0,0,0.6), 0px 0px 30px 10px rgba(0,0,0,0.4)"
-      >
-        <Box padding={16} color="white" fontWeight="bold">
-          {text}
-        </Box>
+      <Box padding={16} color="white" fontWeight="bold">
+        {text}
       </Box>
     </Box>
-  )
-}
+  </Box>
+)
 
 Room.propTypes = {
   diory: PropTypes.shape({
