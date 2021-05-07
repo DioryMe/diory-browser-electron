5fc156cf13b1de66f8891fb69287f51f7df257b7
Jouni Alanen <jvalanen@gmail.com>
2021-05-07 15:08:21 +0300

    DDA-86: Fix convertRelativePath to detect urls that are already absolute

--- src/react/utils/convertRelativePath.js
+++ src/react/utils/convertRelativePath.js
@@ -1,4 +1,8 @@
 export const convertRelativePath = (relativePath, connections) => {
   const diographFolderPath = Object.keys(connections).find((path) => connections[path].connected)
-  return `${diographFolderPath}/${relativePath}`
+  if (!RegExp(`^${diographFolderPath}`).exec(relativePath)) {
+    return `${diographFolderPath}/${relativePath}`
+  }
+
+  return relativePath
 }
