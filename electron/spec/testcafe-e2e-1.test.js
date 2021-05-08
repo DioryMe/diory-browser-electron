01b3957d3b551929918f2e36a22f7f148bdab875
Jouni Alanen <jvalanen@gmail.com>
2021-05-08 22:06:02 +0300

    DDA-000: E2E test to reproduce error when creating and relating new diory to diory with value in diory.data field

--- electron/spec/testcafe-e2e-1.test.js
+++ electron/spec/testcafe-e2e-1.test.js
@@ -6,6 +6,8 @@ fixture`GIVEN welcome room`.page('../../build/index.html')
 test('select diograph folder (with diograph.json)', async (t) => {
   const dioryCount = Selector('.ub-flx_1-0-360px').count
   const eventDiory = Selector('div').withExactText('Kayaking at Potomac')
+  const imageDiory = Selector('#c60c4384-6cb1-4569-b6f8-d6ece4dd75e0')
+  const doneButton = Selector('button').withText('Done')
 
   await t
     .expect(dioryCount)
@@ -18,4 +20,11 @@ test('select diograph folder (with diograph.json)', async (t) => {
     .click(eventDiory)
     .expect(dioryCount)
     .eql(14)
+    .click(imageDiory)
+    .click('[data-testid="tools-button"]')
+    .click('[data-testid="create-button"]')
+    .typeText('input#text', 'New diory')
+    .click(doneButton)
+    .wait(1000) // Wait for SAVE_ROOM to complete and possibly raise an error
+    .click('button[data-testid="home"]')
 })
