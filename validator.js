55fee20f8a8d985fabdb0ac73e5b55a12575c698
Jouni Alanen <jvalanen@gmail.com>
2021-05-09 19:46:51 +0300

    DDA-000: Initial diograph.json validator: node validator.js /Volumes/LaCie\ SSD/My\ Friends/

---
+++ validator.js
@@ -0,0 +1,22 @@
+const fs = require('fs')
+
+const diographFolderPath = process.argv[2]
+const diographPath = `${diographFolderPath}/diograph.json`
+if (!diographFolderPath || !fs.existsSync(diographPath)) {
+  console.log(diographFolderPath)
+  throw new Error('Give diographFolder which exists!')
+}
+
+const raw = fs.readFileSync(diographPath)
+const diograph = JSON.parse(raw).diograph
+
+Object.keys(diograph).forEach((dioryId) => {
+  const diory = diograph[dioryId]
+  diory.links && Object.entries(diory.links).forEach(([path, { id }]) => {
+    if (!diograph[id]) {
+      console.log('Diory', diory)
+      console.log('linkId', id)
+      throw new Error(`Linked diory doesn't exist in diograph.`)
+    }
+  })
+})
