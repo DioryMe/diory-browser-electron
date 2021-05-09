ad7f001561355ecfac11b4ea003f9fab1d63fcf5
Jouni Alanen <jvalanen@gmail.com>
2021-05-09 19:23:54 +0300

    DDA-86: Prevent adding diographFolder path if relativePath is undefined: contentUrl:/Volumes/LaCie SSD/My Bands/undefined

--- src/react/utils/convertRelativePath.js
+++ src/react/utils/convertRelativePath.js
@@ -1,6 +1,6 @@
 export const convertRelativePath = (relativePath, connections) => {
   const diographFolderPath = Object.keys(connections).find((path) => connections[path].connected)
-  if (!RegExp(`^${diographFolderPath}`).exec(relativePath)) {
+  if (relativePath && !RegExp(`^${diographFolderPath}`).exec(relativePath)) {
     return `${diographFolderPath}/${relativePath}`
   }
 
