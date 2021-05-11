bd09181b25252958aa0060bbeb9580e3db98378f
Jouni Alanen <jvalanen@gmail.com>
2021-05-11 10:42:36 +0300

    DDA-86: Fix convertRelativePath to detect development content & http-urls

--- src/react/utils/useGetImageUrl.js
+++ src/react/utils/useGetImageUrl.js
@@ -1,21 +1,11 @@
 import fileUrl from 'file-url'
-import { convertRelativePath } from './convertRelativePath'
-import { useStore } from '../store'
 
 export const useGetImageUrl = (imageUrl) => {
-  const [{ connections }] = useStore((state) => state.connectors)
-  const absoluteImageUrl = convertRelativePath(imageUrl.toString(), connections)
-
   // Images from internet
-  if (/^http(s)?:\/\//.exec(imageUrl)) {
+  if (RegExp('^http(s)?://').exec(imageUrl)) {
     return imageUrl
   }
 
-  // Development content
-  if (/^development-content-room/.exec(imageUrl)) {
-    return `http://localhost:3300/${imageUrl}`
-  }
-
   // Convert to file:// url
-  return absoluteImageUrl && fileUrl(absoluteImageUrl)
+  return imageUrl && fileUrl(imageUrl)
 }
