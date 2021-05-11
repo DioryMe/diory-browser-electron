fb93a2fd173d4d658a4c7eef64cfb24dbf8147c5
Jouni Alanen <jvalanen@gmail.com>
2021-05-11 09:14:09 +0300

    DDA-86: Extract getUntrackedDiory from getUntrackedDiograph + create getUntrackedDiories for future use in lens

--- src/react/utils/getUntrackedDiograph.js
+++ src/react/utils/getUntrackedDiograph.js
@@ -1,18 +1,25 @@
 import { getUntrackedObject } from '../store'
 
+const getUntrackedDiory = (diory) => {
+  const untrackedLinks = getUntrackedObject(diory.links)
+  if (untrackedLinks) {
+    diory.links = { ...untrackedLinks }
+  }
+  const untrackedData = getUntrackedObject(diory.data)
+  if (untrackedData) {
+    diory.data = { ...untrackedData }
+  }
+  return diory
+}
+
+export const getUntrackedDiories = (diories) => diories.map((diory) => getUntrackedDiory(diory))
+
 // Makes a copy of storeDiograph and deeply resolves the Proxy objects
 export const getUntrackedDiograph = (diograph) => {
   const untrackedDiograph = { ...getUntrackedObject(diograph) }
   if (untrackedDiograph) {
     Object.entries(untrackedDiograph).forEach(([key, value]) => {
-      const untrackedLinks = getUntrackedObject(untrackedDiograph[key].links)
-      if (untrackedLinks) {
-        untrackedDiograph[key].links = { ...untrackedLinks }
-      }
-      const untrackedData = getUntrackedObject(untrackedDiograph[key].data)
-      if (untrackedData) {
-        untrackedDiograph[key].data = { ...untrackedData }
-      }
+      untrackedDiograph[key] = getUntrackedDiory(untrackedDiograph[key])
     })
   }
   return untrackedDiograph
