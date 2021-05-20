2eec30a314875ee625ec915647f8cfc0cd56d167
Jouni Alanen <jvalanen@gmail.com>
2021-05-20 16:00:51 +0300

    DDA-144: Convert map filter to use latlng

--- src/react/features/filters/map/useGenerateMapFilter.spec.js
+++ src/react/features/filters/map/useGenerateMapFilter.spec.js
@@ -34,21 +34,21 @@ describe('useGenerateMapFilter', () => {
     describe('given diory is inside bounds', () => {
       it('returns true', () => {
         const mapFilter = useGenerateMapFilter()
-        expect(mapFilter({ latitude: 5, longitude: 5 })).toEqual(true)
+        expect(mapFilter({ latlng: '5, 5' })).toEqual(true)
       })
     })
 
     describe('given diory is on bounds', () => {
       it('returns true', () => {
         const mapFilter = useGenerateMapFilter()
-        expect(mapFilter({ latitude: 0, longitude: 10 })).toEqual(true)
+        expect(mapFilter({ latlng: '0, 10' })).toEqual(true)
       })
     })
 
     describe('given diory is outside bounds', () => {
       it('returns false', () => {
         const mapFilter = useGenerateMapFilter()
-        expect(mapFilter({ latitude: 15, longitude: 15 })).toEqual(false)
+        expect(mapFilter({ latlng: '15, 15' })).toEqual(false)
       })
 
       describe('given map filter is inactive', () => {
@@ -58,24 +58,26 @@ describe('useGenerateMapFilter', () => {
 
         it('returns true', () => {
           const mapFilter = useGenerateMapFilter()
-          expect(mapFilter({ latitude: 15, longitude: 15 })).toEqual(true)
+          expect(mapFilter({ latlng: '15, 15' })).toEqual(true)
         })
       })
     })
 
-    describe('given diory do not have latitude', () => {
-      it('returns true', () => {
-        const mapFilter = useGenerateMapFilter()
-        expect(mapFilter({ longitude: 15 })).toEqual(false)
-      })
-    })
+    // Not valid scenarios when we validate format / input data
 
-    describe('given diory do not have longitude', () => {
-      it('returns true', () => {
-        const mapFilter = useGenerateMapFilter()
-        expect(mapFilter({ latitude: 15 })).toEqual(false)
-      })
-    })
+    // describe('given diory do not have latitude', () => {
+    //   it('returns true', () => {
+    //     const mapFilter = useGenerateMapFilter()
+    //     expect(mapFilter({ latlng: '15, 15' })).toEqual(false)
+    //   })
+    // })
+
+    // describe('given diory do not have longitude', () => {
+    //   it('returns true', () => {
+    //     const mapFilter = useGenerateMapFilter()
+    //     expect(mapFilter({ latlng: '15, 15' })).toEqual(false)
+    //   })
+    // })
   })
 
   describe('given map filter is active and does not have bounds', () => {
@@ -91,7 +93,7 @@ describe('useGenerateMapFilter', () => {
 
     it('returns true', () => {
       const mapFilter = useGenerateMapFilter()
-      expect(mapFilter({ latitude: 5, longitude: 5 })).toEqual(true)
+      expect(mapFilter({ latlng: '5, 5' })).toEqual(true)
     })
   })
 })
