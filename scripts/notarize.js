704315db6166e43b2469d2e7f3f0e76220e959f6
Jouni Alanen <jvalanen@gmail.com>
2021-05-27 12:42:55 +0300

    DDA-000: Eslint scripts/ folder

--- scripts/notarize.js
+++ scripts/notarize.js
@@ -1,21 +1,21 @@
 // Copy-pasted from https://kilianvalkhof.com/2019/electron/notarizing-your-electron-application/
 // eslint-disable
 
-require('dotenv').config();
-const { notarize } = require('electron-notarize');
+require('dotenv').config()
+const { notarize } = require('electron-notarize')
 
 exports.default = async function notarizing(context) {
-  const { electronPlatformName, appOutDir } = context;
+  const { electronPlatformName, appOutDir } = context
   if (electronPlatformName !== 'darwin') {
-    return;
+    return
   }
 
-  const appName = context.packager.appInfo.productFilename;
+  const appName = context.packager.appInfo.productFilename
 
-  return await notarize({
+  return notarize({
     appBundleId: 'com.diory.browser-electron',
     appPath: `${appOutDir}/${appName}.app`,
     appleId: process.env.APPLEID,
     appleIdPassword: process.env.APPLEIDPASS,
-  });
-};
+  })
+}
