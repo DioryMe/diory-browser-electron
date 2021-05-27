5e5061f3705bcb835e09bd7adcbdc6b334a706f2
Jouni Alanen <jvalanen@gmail.com>
2021-05-27 19:52:56 +0300

    BUG: Verify diory.data field in E2E tests

--- electron/spec/testcafe-e2e-2.test.js
+++ electron/spec/testcafe-e2e-2.test.js
@@ -7,23 +7,36 @@ test('select new diograph folder (without diograph.json) and delete it', async (
   const dioryCount = Selector('.ub-flx_1-0-360px').count
   const exampleFolderRoomDiory = Selector('div').withExactText('testcafe-diograph-folder')
   const someFolderDiory = Selector('div').withExactText('some-folder')
+  const someMusicDiory = Selector('div').withExactText('some-music.mp3')
+  const dataFieldValue = Selector('input#data').value
   const welcomeRoomDiory = Selector('div').withExactText('Welcome room!')
   const deleteButton = Selector('button').withText('Delete')
 
   await t
     .expect(dioryCount)
     .eql(1)
+    // Load diograph folder
     .click('[data-testid="home"]')
     .click('[data-testid="tools-button"]')
     .click('[data-testid="undefined-button"]')
     .expect(dioryCount)
     .eql(9)
+    // Delete diory
     .click('[data-testid="tools-button"]')
     .click('[data-testid="delete-button"]')
     .click(someFolderDiory)
     .click(deleteButton)
     .click('[data-testid="tools-button"]') // Close tool bar
     .wait(1000) // Wait for SAVE_ROOM to complete and possibly raise an error
+    // Check diory.data
+    .click('[data-testid="tools-button"]')
+    .click('[data-testid="update-button"]')
+    .click(someMusicDiory)
+    .expect(dataFieldValue)
+    .eql(
+      '[{"@context":"https://schema.org","@type":"AudioObject","contentUrl":"some-music.mp3","encodingFormat":"audio/mpeg"}]'
+    )
+    // Delete room
     .click('[data-testid="home"]')
     .click('[data-testid="tools-button"]')
     .click('[data-testid="home-remove-room-button"]')
