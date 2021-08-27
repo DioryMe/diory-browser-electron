542bd2f78c05807288a5b0e2a71ccf86e96792b6
Jouni Alanen <jvalanen@gmail.com>
2021-08-27 11:53:09 +0300

    POC-159: CloseButton to FullscreenLens

--- src/react/features/lenses/fullscreen/FullscreenLens.js
+++ src/react/features/lenses/fullscreen/FullscreenLens.js
@@ -1,15 +1,29 @@
 import React from 'react'
 
 import { useFocus } from '../../diograph/hooks'
+import { useDispatchActions } from '../../../store'
 
-import DataAwareDiory from '../../../components/diories/DataAwareDiory'
+import { selectLens } from '../actions'
+
 import Fullscreen from '../../../components/Fullscreen'
+import DataAwareDiory from '../../../components/diories/DataAwareDiory'
+import CloseButton from '../../../components/CloseButton'
+
+const useFullscreenLens = () => {
+  const { dispatch } = useDispatchActions()
+
+  return {
+    onClick: () => dispatch(selectLens('grid')),
+  }
+}
 
 const FullscreenLens = () => {
   const { diory } = useFocus()
+  const { onClick } = useFullscreenLens()
   return (
     <Fullscreen zIndex={10000}>
       <DataAwareDiory diory={diory} />
+      <CloseButton onClick={onClick} />
     </Fullscreen>
   )
 }
