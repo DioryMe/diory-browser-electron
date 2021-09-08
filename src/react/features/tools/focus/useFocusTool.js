16c673a7602bddf806fc1d811ac5a240402f017b
Jouni Alanen <jvalanen@gmail.com>
2021-09-08 18:40:44 +0300

    POC-163: Click to reverseLinked in fullscreen: changes focus

--- src/react/features/tools/focus/useFocusTool.js
+++ src/react/features/tools/focus/useFocusTool.js
@@ -5,9 +5,12 @@ import { addDioryToHand } from '../actions'
 export const useFocusTool = () => {
   const [{ active }] = useStore((state) => state.buttons)
   const [{ selectedDioryId }] = useStore((state) => state.navigation)
+  const [{ selectedLensId }] = useStore((state) => state.lenses)
   const dispatch = useDispatch()
   return (clickedDiory) => {
-    if (!active) {
+    if (selectedLensId === 'fullscreen') {
+      dispatch(setFocus({ focusId: clickedDiory.id }))
+    } else if (!active) {
       if (selectedDioryId === clickedDiory.id) {
         dispatch(addDioryToHand(clickedDiory.id))
         dispatch(setFocus({ focusId: clickedDiory.id }))
