5437c85818c123bccd31b0d94fb111db3448593e
Jouni Alanen <jvalanen@gmail.com>
2021-09-15 17:43:27 +0300

    POC-163: Fullscreen top row click when blue border

--- src/react/features/tools/focus/useFocusTool.js
+++ src/react/features/tools/focus/useFocusTool.js
@@ -1,15 +1,26 @@
 import { useDispatch, useStore } from '../../../store'
 import { setFocus, setSelectedDiory } from '../../navigation/actions'
+import { useFocus } from '../../diograph/hooks'
 import { addDioryToHand } from '../actions'
 
 export const useFocusTool = () => {
   const [{ active }] = useStore((state) => state.buttons)
-  const [{ selectedDioryId }] = useStore((state) => state.navigation)
+  const [{ focusId, selectedDioryId }] = useStore((state) => state.navigation)
+  const { reverseDiorys } = useFocus()
   const [{ selectedLensId }] = useStore((state) => state.lenses)
   const dispatch = useDispatch()
   return (clickedDiory) => {
     if (selectedLensId === 'fullscreen') {
-      dispatch(setFocus(clickedDiory))
+      // Fullscreen top row click when blue border
+      if (focusId === clickedDiory.id) {
+        if (reverseDiorys.length > 0) {
+          dispatch(setFocus(reverseDiorys[0]))
+          dispatch(setSelectedDiory(clickedDiory))
+        }
+      } else {
+        // Fullscreen bottom row click & top row without blue border
+        dispatch(setFocus(clickedDiory))
+      }
     } else if (!active) {
       if (selectedDioryId === clickedDiory.id) {
         dispatch(addDioryToHand(clickedDiory.id))
