fc9b6c0775d48fcd93d5d0061f531f28b78ca6fc
Olli-Pekka Pohjola <olli-pekka.pohjola@houston-inc.com>
2021-09-24 00:23:02 +0300

    POC-162: Create tool to import diograph from folder

---
+++ src/react/features/tools/folder/buttons.js
@@ -0,0 +1,12 @@
+export const FOLDER_IMPORT = 'FOLDER_IMPORT'
+
+export const buttons = [
+  {
+    id: FOLDER_IMPORT,
+    text: 'Import folder',
+    data: {
+      icon: 'folder',
+      testid: 'import-folder',
+    },
+  },
+]
