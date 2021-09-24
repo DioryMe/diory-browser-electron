e9a4142a196caf04be02a2c9c7442f73298a1c5b
Jouni Alanen <jvalanen@gmail.com>
2021-09-24 15:07:42 +0300

    DDA-000: Fix e2e & feature tests after updated demo content

--- electron/spec/testcafe-e2e-3.test.js
+++ electron/spec/testcafe-e2e-3.test.js
@@ -5,7 +5,7 @@ fixture`GIVEN welcome room`.page('../../build/index.html')
 
 test('select diograph folder with new subfolder (diograph.json + newly added subfolder)', async (t) => {
   const dioryCount = Selector('.ub-flx_1-0-360px').count
-  const eventDiory = Selector('div').withExactText('Kayaking at Potomac')
+  const eventDiory = Selector('div').withExactText('Scouts BSA International (event)')
 
   await t
     .expect(dioryCount)
@@ -14,8 +14,8 @@ test('select diograph folder with new subfolder (diograph.json + newly added sub
     .click('[data-testid="tools-button"]')
     .click('[data-testid="undefined-button"]')
     .expect(dioryCount)
-    .eql(4)
+    .eql(10)
     .click(eventDiory)
     .expect(dioryCount)
-    .eql(14)
+    .eql(8)
 })
