e0b4b50ef01d4bcac4c88f6367d4060ed36aeec8
Jouni Alanen <jvalanen@gmail.com>
2021-09-27 10:20:22 +0300

    POC-168: Add onClick to searchResult (didn't know how to get parameter to it though...) #

--- src/react/features/search/SearchResult.js
+++ src/react/features/search/SearchResult.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Pane } from 'evergreen-ui'
 
-const SearchResult = ({ text }) => <div>2: {text}</div>
+const style = {
+  border: '1px solid black',
+  height: '50px',
+  cursor: 'pointer',
+}
+
+const SearchResult = ({ text, onClick }) => (
+  <Pane style={style} onClick={onClick}>
+    3: {text}
+  </Pane>
+)
 
 SearchResult.propTypes = {
+  onClick: PropTypes.func,
   text: PropTypes.string,
 }
 
