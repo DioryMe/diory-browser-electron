51a666d3b148db036d29f36fd66fc819ee12c96f
Jouni Alanen <jvalanen@gmail.com>
2021-10-03 16:47:20 +0300

    POC-168: Move all Search  stuff from Home component to under its own feature folder

--- src/react/features/home/Home.js
+++ src/react/features/home/Home.js
@@ -1,7 +1,5 @@
 import React from 'react'
 
-import { useStore } from '../../store'
-
 import Navigation from '../navigation/Navigation'
 import Filters from '../filters/Filters'
 import Lenses from '../lenses/Lenses'
@@ -10,29 +8,6 @@ import Tools from '../tools/Tools'
 import Buttons from '../buttons'
 import Connectors from '../connectors/Connectors'
 import Search from '../search/Search'
-import SearchResults from '../search/SearchResults'
-import SearchResultAutocomplete from '../search/SearchResultAutocomplete'
-
-const Search2 = () => {
-  const [{ query, active }] = useStore((state) => state.search)
-
-  return active ? (
-    <div
-      style={{
-        backgroundColor: 'white',
-        position: 'fixed',
-        top: '48px',
-        right: 0,
-        width: '300px',
-        height: '100%',
-      }}
-    >
-      <Search />
-      {query ? <SearchResultAutocomplete /> : null}
-      <SearchResults />
-    </div>
-  ) : null
-}
 
 const Home = () => (
   <div className="App">
@@ -43,7 +18,7 @@ const Home = () => (
     <Tools />
     <Buttons />
     <Connectors />
-    <Search2 />
+    <Search />
   </div>
 )
 
