57d0e1ab52dc8df823a32ffe03eae2abec98f5df
Jouni Alanen <jvalanen@gmail.com>
2021-10-03 21:18:10 +0300

    POC-168: Convert SearchResult to Hand-item (with image, text, drag & focus-click)

--- src/react/features/search/SearchResult.js
+++ src/react/features/search/SearchResult.js
@@ -1,23 +1,35 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Pane } from 'evergreen-ui'
 
-const style = {
-  border: '1px solid black',
-  height: '50px',
-  cursor: 'pointer',
-  padding: 'auto',
+import Diory from '../../components/diories/Diory'
+import Draggable, { types } from '../../components/Draggable'
+
+const itemStyle = {
+  height: '56px',
+  width: '200px',
+  padding: '0 8px',
+  flexShrink: 0,
 }
 
-const SearchResult = ({ id, text, onClick }) => (
-  <Pane style={style} onClick={() => onClick(id)}>
-    <center>{text}</center>
-  </Pane>
+const scaleStyle = {
+  transform: 'scale(0.5)',
+  width: '200%',
+  height: '200%',
+  transformOrigin: 'top left',
+}
+
+const SearchResult = ({ diory, onClick }) => (
+  <div key={diory.id} style={itemStyle} onClick={() => onClick(diory.id)}>
+    <div style={scaleStyle}>
+      <Draggable type={types.DIORY} id={diory.id}>
+        <Diory id={`search-${diory.id}`} diory={diory} height="50px" />
+      </Draggable>
+    </div>
+  </div>
 )
 
 SearchResult.propTypes = {
-  id: PropTypes.string,
-  text: PropTypes.string,
+  diory: PropTypes.object,
   onClick: PropTypes.func,
 }
 
