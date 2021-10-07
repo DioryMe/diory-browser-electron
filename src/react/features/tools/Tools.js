ac5cd4c6559cc1bf030b651067cd7dd0f8e46450
Jouni Alanen <jvalanen@gmail.com>
2021-10-07 16:03:57 +0300

    POC-168: Move hand from tool&bottom to search&sidebar + use SearchResult as HandItem

--- src/react/features/tools/Tools.js
+++ src/react/features/tools/Tools.js
@@ -3,14 +3,12 @@ import React from 'react'
 import CreateTool from './create/CreateTool'
 import UpdateTool from './update/UpdateTool'
 import DeleteTool from './delete/DeleteTool'
-import HandTool from './hand/HandTool'
 
 const Tools = () => (
   <>
     <CreateTool />
     <UpdateTool />
     <DeleteTool />
-    <HandTool />
   </>
 )
 
