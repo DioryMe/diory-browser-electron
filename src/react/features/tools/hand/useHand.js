39b4c09b8ed600a365c64352a213b3033aa4a62d
Jouni Alanen <jvalanen@gmail.com>
2021-10-07 16:09:12 +0300

    POC-168: onDropToItem for hand items => creates a link

--- src/react/features/tools/hand/useHand.js
+++ src/react/features/tools/hand/useHand.js
@@ -1,6 +1,7 @@
 import { useDispatchActions, useStore } from '../../../store'
 import { addDioryToHand } from '../actions'
 import { setFocus } from '../../navigation/actions'
+import { createLink } from '../../diograph/actions'
 
 export const useHand = () => {
   const [{ hand }] = useStore((state) => state.tools)
@@ -11,5 +12,7 @@ export const useHand = () => {
     diorys: diograph && hand.map((id) => diograph[id]).filter(Boolean),
     onDrop: ({ id }) => dispatch(addDioryToHand(id)),
     onClick: (id) => dispatch(setFocus({ id })),
+    onDropToItem: ({ droppedId, draggedId }) =>
+      dispatch(createLink({ id: droppedId }, { id: draggedId })),
   }
 }
