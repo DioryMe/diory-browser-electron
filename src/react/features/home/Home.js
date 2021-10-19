8897a89e4bb720eb406ff00ddd682d32f3964245
Jouni Alanen <jvalanen@gmail.com>
2021-10-19 21:02:08 +0300

    Add Welcome component

--- src/react/features/home/Home.js
+++ src/react/features/home/Home.js
@@ -3,20 +3,20 @@ import React from 'react'
 import Navigation from '../navigation/Navigation'
 import Filters from '../filters/Filters'
 import Lenses from '../lenses/Lenses'
-import Rooms from '../rooms/Rooms'
+// import Rooms from '../rooms/Rooms'
+import Welcome from '../welcome/Welcome'
 import Tools from '../tools/Tools'
 import Buttons from '../buttons'
-import Connectors from '../connectors/Connectors'
 
 const Home = () => (
   <div className="App">
     <Navigation />
-    <Rooms />
+    <Welcome />
+    <div /* Rooms */ />
     <Lenses />
     <Filters />
     <Tools />
     <Buttons />
-    <Connectors />
   </div>
 )
 
---
+++ src/react/features/welcome/Welcome.js
@@ -0,0 +1,104 @@
+import React, { useEffect } from 'react'
+import { Pane } from 'evergreen-ui'
+import { v4 as uuid } from 'uuid'
+
+import { useFocus } from '../diograph/hooks'
+import { useDispatch, useStore, useDispatchActions } from '../../store'
+import { getRoom } from '../diograph/actions'
+import { invokeChannel } from '../../client/client'
+import { channels } from '../../../shared/constants'
+import { useGetHomeEffect } from './effects/useGetHomeEffect'
+
+import { enterRoom, setFocus } from '../navigation/actions'
+import { activateButton, inactivateButton } from '../buttons/actions'
+
+const ADD_CONNECTION_BUTTON = 'ADD_CONNECTION_BUTTON'
+
+export const useAddConnectionButton = () => {
+  const [{ active }] = useStore((state) => state.buttons)
+  const { dispatch, dispatchPromiseAction } = useDispatchActions()
+
+  useEffect(() => {
+    const getHome = (result) => {
+      const diographFolderPath = result.filePaths[0]
+      invokeChannel(channels.GENERATE_DIOGRAPH, diographFolderPath).then(({ rootId, diograph }) => {
+        const roomId = uuid()
+        dispatchPromiseAction(
+          () => invokeChannel(channels.GENERATE_DIOGRAPH, diographFolderPath),
+          () => getRoom({ rootId, diograph, address: diographFolderPath })
+        ).then(() => {
+          dispatch(enterRoom({ id: roomId }))
+          dispatch(setFocus({ id: rootId }))
+        })
+      })
+    }
+
+    if (ADD_CONNECTION_BUTTON === active) {
+      dispatch(inactivateButton())
+      if (window.processEnv.TESTCAFE_TEST) {
+        const path = `${window.processEnv.PWD}/tmp/testcafe-diograph-folder`
+        const result = { filePaths: [path] }
+        getHome(result)
+      } else {
+        window.channelsApi.showOpenDialog().then((result) => {
+          getHome(result)
+        })
+      }
+    }
+  }, [active, dispatch])
+}
+
+const Welcome = () => {
+  const [{ active }] = useStore((state) => state.buttons)
+  const dispatch = useDispatch()
+
+  useGetHomeEffect()
+  useAddConnectionButton()
+
+  const onClick = () => {
+    dispatch(
+      ADD_CONNECTION_BUTTON === active ? inactivateButton() : activateButton(ADD_CONNECTION_BUTTON)
+    )
+  }
+
+  const { diory } = useFocus()
+  return (
+    !diory && (
+      <Pane
+        style={{
+          position: 'absolute',
+          top: 50,
+          width: '100%',
+          textAlign: 'center',
+        }}
+      >
+        <Pane
+          style={{
+            fontSize: 40,
+            lineHeight: 3,
+          }}
+        >
+          Welcome to Diory!
+        </Pane>
+        <Pane
+          onClick={onClick}
+          style={{
+            cursor: 'pointer',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            margin: '0 auto',
+            width: '350px',
+            height: '60px',
+            border: '3px solid',
+            color: ADD_CONNECTION_BUTTON === active ? 'blue' : 'black',
+          }}
+        >
+          + Choose folder for your diograph
+        </Pane>
+      </Pane>
+    )
+  )
+}
+
+export default Welcome
