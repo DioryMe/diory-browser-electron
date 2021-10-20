78bf57bf4dc2ab8397511e3512d16477cb305be0
Jouni Alanen <jvalanen@gmail.com>
2021-10-20 13:10:13 +0300

    Add client.mock for GET_DIOGRAPH

--- src/react/client/client.mock.js
+++ src/react/client/client.mock.js
@@ -1,10 +1,12 @@
 import GET_ROOM from './mockResponses/GET_ROOM.json'
 import GET_HOME from './mockResponses/GET_HOME.json'
+import GET_DIOGRAPH from './mockResponses/GET_DIOGRAPH.json'
 
 export const mockResponse = (channel) => {
   const data = {
     GET_HOME,
     GET_ROOM,
+    GET_DIOGRAPH,
   }[channel]
 
   if (!data) {
