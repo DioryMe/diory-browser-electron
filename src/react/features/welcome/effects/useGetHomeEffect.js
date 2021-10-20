e4d57a8372cbd37b9c8906ad4e1820d6d849d6f3
Jouni Alanen <jvalanen@gmail.com>
2021-10-20 13:08:47 +0300

    Cleanup & simplify welcome/effects/useGetHomeEffect.js

--- src/react/features/welcome/effects/useGetHomeEffect.js
+++ src/react/features/welcome/effects/useGetHomeEffect.js
@@ -2,32 +2,24 @@ import { useEffect } from 'react'
 import { useDispatchActions } from '../../../store'
 
 import { enterRoom, setFocus } from '../../navigation/actions'
-import { getRoom } from '../../diograph/actions'
 
 import { invokeChannel } from '../../../client/client'
 import { channels } from '../../../../shared/constants'
-// import { invokeAlertDialog } from '../../../client/alertDialog'
-/*
- * Requests home & focus info via GET_HOME and sets the focus for room & diory
- * - saves the response to the store
- * - invoke an alert for each error
- */
+import { invokeAlertDialog } from '../../../client/alertDialog'
+
 export const useGetHomeEffect = () => {
-  const { dispatch, dispatchPromiseAction } = useDispatchActions()
+  const { dispatch } = useDispatchActions()
 
   useEffect(() => {
-    invokeChannel(channels.GET_HOME).then(({ diographFolderPath, focus }) => {
-      invokeChannel(channels.GET_ROOM, { address: diographFolderPath }).then(
-        ({ rootId, diograph }) => {
-          dispatchPromiseAction(
-            () => invokeChannel(channels.GET_ROOM, { address: diographFolderPath }),
-            () => getRoom({ rootId, diograph, address: diographFolderPath })
-          ).then((yx, kax) => {
-            dispatch(enterRoom({ id: focus.roomId }))
-            dispatch(setFocus({ id: focus.dioryId }))
-          })
-        }
-      )
+    invokeChannel(channels.GET_DIOGRAPH).then(({ rootId, diograph }) => {
+      if (!rootId) {
+        invokeAlertDialog('Error getting diograph')
+      }
+
+      // TODO: PUT DIOGRAPH IN STORE => needs own store action (GET_ROOM used to do this)
+
+      dispatch(enterRoom({ id: rootId }))
+      dispatch(setFocus({ id: rootId }))
     })
   }, [dispatch])
 }
