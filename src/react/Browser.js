7bd8ba4ed822ff8890e2b92e7928b8986a18f96f
Jouni Alanen <jvalanen@gmail.com>
2021-10-24 20:56:16 +0300

    Remove Welcome from Browser.js as it is already moved to Root component

--- src/react/Browser.js
+++ src/react/Browser.js
@@ -4,14 +4,12 @@ import Navigation from './features/navigation/Navigation'
 import Filters from './features/filters/Filters'
 import Lenses from './features/lenses/Lenses'
 // import Rooms from './features/rooms/Rooms'
-import Welcome from './features/welcome/Welcome'
 import Tools from './features/tools/Tools'
 import Buttons from './features/buttons'
 
 const Browser = () => (
   <div className="App">
     <Navigation />
-    <Welcome />
     <div /* Rooms */ />
     <Lenses />
     <Filters />
