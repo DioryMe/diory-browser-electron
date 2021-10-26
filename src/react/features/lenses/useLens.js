3f85524add6b3b35eff3caeeb9875f0c255da850
Olli-Pekka Pohjola <op@diory.me>
2021-10-26 23:35:00 +0300

    TECH: Move relative to absolute url conversion to utilizing components

--- src/react/features/lenses/useLens.js
+++ src/react/features/lenses/useLens.js
@@ -1,38 +1,11 @@
-import { useStore } from '../../store'
-import { getUntrackedDiory, convertRelativePath } from '../../utils'
-
-import { useStory } from '../diograph/hooks'
+import { useDiograph } from '../diograph/useDiograph'
 import { useFilterIsActive } from '../filters/hooks/useFilterIsActive'
 import { useFilteredDiorys } from '../filters/useFilteredDiorys'
 
-const prepareDiory = (diory, connections) => {
-  const preparedDiory = getUntrackedDiory(diory)
-  if (preparedDiory) {
-    preparedDiory.image = convertRelativePath(preparedDiory.image, connections)
-    if (preparedDiory.data) {
-      preparedDiory.data[0].contentUrl = convertRelativePath(
-        preparedDiory.data[0].contentUrl,
-        connections
-      )
-    }
-  }
-  return preparedDiory
-}
-
 export const useLens = () => {
   const { filterIsActive } = useFilterIsActive()
-  const focusDiorys = useStory()
+  const focusDiorys = useDiograph()
   const filteredDiorys = useFilteredDiorys()
-  const { story, memories } = filterIsActive ? filteredDiorys : focusDiorys
-  const [{ selectedLensId }] = useStore((state) => state.lenses)
 
-  const [{ connections }] = useStore((state) => state.connectors)
-  const preparedDiory = story && prepareDiory(story, connections)
-  const preparedDiorys = memories.map((diory) => prepareDiory(diory, connections))
-
-  return {
-    selectedLensId,
-    story: preparedDiory,
-    memories: preparedDiorys,
-  }
+  return filterIsActive ? filteredDiorys : focusDiorys
 }
