df38c28c427708a6d678923fe5d3608971562afd
Jouni Alanen <jvalanen@gmail.com>
2021-10-28 15:09:23 +0300

    Add copyFolderRecursiveSync to IMPORT_FOLDER channel before generating the diograph

--- electron/channels/import-folder-channel.js
+++ electron/channels/import-folder-channel.js
@@ -1,16 +1,45 @@
+const fs = require('fs')
+const path = require('path')
 const { generateDiograph } = require('../generators/diograph-generator')
-const { saveDiographJson } = require('../lib/save-diograph-json')
 const { readDiographJson } = require('../lib/read-diograph-json')
 
-exports.importFolderEventHandler = async function importFolderEventHandler(path) {
+function copyFolderRecursiveSync(source, target) {
+  if (!fs.lstatSync(source).isDirectory()) {
+    throw new Error(`copyFolderRecursiveSync: source folder (${source}) is not a folder!`)
+  }
+
+  fs.readdirSync(source).forEach((itemName) => {
+    const sourcePath = path.join(source, itemName)
+    const targetPath = path.join(target, itemName)
+
+    if (fs.lstatSync(sourcePath).isDirectory()) {
+      fs.mkdirSync(targetPath)
+      copyFolderRecursiveSync(sourcePath, targetPath)
+    } else {
+      fs.copyFileSync(sourcePath, targetPath)
+    }
+  })
+}
+
+exports.importFolderEventHandler = async function importFolderEventHandler(importFolderPath) {
+  // TODO: Replace path with { importFolderPath, folderLocation }
+  const folderLocation = '/Users/Jouni/My Diory'
+
+  // Create new folder to My Diory folder
+  const importedFolderPathInDioryFolder = path.join(folderLocation, path.basename(importFolderPath))
+  console.log(importedFolderPathInDioryFolder)
+  fs.mkdirSync(importedFolderPathInDioryFolder)
+  // Copy everything recursively from importFolderPath to the new destination
+  copyFolderRecursiveSync(importFolderPath, importedFolderPathInDioryFolder, { recursive: true })
+
   // Read existing diograph.json
-  const existingDiograph = readDiographJson(path)
+  const existingDiograph = readDiographJson(importedFolderPathInDioryFolder)
   if (existingDiograph) {
     return existingDiograph
   }
 
   // Generate diograph if no diograph.json
-  const diograph = await generateDiograph(path)
-  await saveDiographJson(path, diograph.diograph, diograph.rootId)
+  const diograph = await generateDiograph(importedFolderPathInDioryFolder)
+  // await saveDiographJson(path, diograph.diograph, diograph.rootId)
   return diograph
 }
