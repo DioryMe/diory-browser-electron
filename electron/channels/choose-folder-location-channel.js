edc9275c211b3cadb5a811212da387b4e71dd6ec
Jouni Alanen <jvalanen@gmail.com>
2021-10-28 09:29:52 +0300

    Rename CHOOSE_DIOGRAPH_FOLDER -> CHOOSE_FOLDER_LOCATION

--- electron/channels/choose-folder-location-channel.js
+++ electron/channels/choose-folder-location-channel.js
@@ -2,7 +2,7 @@ const { generateDiograph } = require('../generators/diograph-generator')
 const { saveDiographJson } = require('../lib/save-diograph-json')
 const { readDiographJson } = require('../lib/read-diograph-json')
 
-exports.chooseDiographFolderEventHandler = async function chooseDiographFolderEventHandler(path) {
+exports.chooseFolderLocationEventHandler = async function chooseFolderLocationEventHandler(path) {
   const existingDiograph = readDiographJson(path)
   const folderStructureDiograph = await generateDiograph(path)
   const diograph = existingDiograph || folderStructureDiograph
