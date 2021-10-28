0d7adbcdb34d5927c7dfc487c0a9343ff29c01eb
Jouni Alanen <jvalanen@gmail.com>
2021-10-28 20:55:27 +0300

    POC-2021: Revert changes to HandTool + feature tests + remove from Search

--- src/react/features/tools/hand/HandTool.js
+++ src/react/features/tools/hand/HandTool.js
@@ -1,12 +1,15 @@
 import React from 'react'
 
+import { useStore } from '../../../store'
 import { useHand } from './useHand'
 
 import HandView from './HandView'
 
 const HandTool = () => {
+  const [{ open }] = useStore((state) => state.buttons)
+  const [{ selectedLensId }] = useStore((state) => state.lenses)
   const props = useHand()
-  return <HandView {...props} />
+  return open && selectedLensId === 'grid' ? <HandView {...props} /> : null
 }
 
 export default HandTool
--- src/react/features/tools/hand/useHand.js
+++ src/react/features/tools/hand/useHand.js
@@ -1,7 +1,6 @@
 import { useDispatchActions, useStore } from '../../../store'
 import { addDioryToHand } from '../actions'
 import { setFocus } from '../../navigation/actions'
-import { createLink } from '../../diograph/actions'
 
 export const useHand = () => {
   const [{ hand }] = useStore((state) => state.tools)
@@ -11,8 +10,6 @@ export const useHand = () => {
   return {
     diorys: diograph && hand.map((id) => diograph[id]).filter(Boolean),
     onDrop: ({ id }) => dispatch(addDioryToHand(id)),
-    onClick: (id) => dispatch(setFocus({ id })),
-    onDropToItem: ({ droppedId, draggedId }) =>
-      dispatch(createLink({ id: droppedId }, { id: draggedId })),
+    onClick: ({ diory: { id } }) => dispatch(setFocus({ id })),
   }
 }
