03bbff2b8b36fa99a1cd198a4e835dd919ad5b5a
Jouni Alanen <jvalanen@gmail.com>
2021-10-28 12:26:37 +0300

    Use folderLocation from store instead of retrievedFolderLocation

--- src/react/Root.js
+++ src/react/Root.js
@@ -1,27 +1,20 @@
 import React, { useEffect } from 'react'
-import { useDispatchActions } from './store'
+import { useDispatchActions, useStore } from './store'
 
 import { setFocus } from './features/navigation/actions'
 import { addDiograph } from './features/diograph/actions'
 
 import { invokeChannel } from './client/client'
-import { invokeAlertDialog } from './client/alertDialog'
 import { channels } from '../shared/constants'
 
 import Browser from './Browser'
 import Welcome from './features/welcome/Welcome'
-
-let retrievedFolderLocation
 
 export const useGetDiograph = () => {
   const { dispatch } = useDispatchActions()
 
   useEffect(() => {
     invokeChannel(channels.GET_DIOGRAPH).then(({ diograph, rootId, folderLocation }) => {
-      if (!rootId) {
-        invokeAlertDialog('Error getting diograph')
-      }
-      retrievedFolderLocation = folderLocation
       dispatch(addDiograph(diograph, rootId, folderLocation))
       dispatch(setFocus({ id: rootId }))
     })
@@ -29,13 +22,15 @@ export const useGetDiograph = () => {
 }
 
 const Root = () => {
+  const [{ folderLocation }] = useStore((state) => state.diograph)
+
   useGetDiograph()
 
-  if (retrievedFolderLocation === undefined) {
+  if (folderLocation === undefined) {
     return <div>Loading...</div>
   }
 
-  return retrievedFolderLocation === null ? <Welcome /> : <Browser />
+  return folderLocation === null ? <Welcome /> : <Browser />
 }
 
 export default Root
