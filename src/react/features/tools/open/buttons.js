e32ccf8a0ecafd5a392e152c30d3415bff657ede
Olli-Pekka Pohjola <op@diory.me>
2021-10-29 22:31:45 +0300

    DDA-176: Add open in folder tool

---
+++ src/react/features/tools/open/buttons.js
@@ -0,0 +1,13 @@
+export const OPEN_TOOL_BUTTON = 'OPEN_TOOL_BUTTON'
+
+export const buttons = [
+  {
+    id: OPEN_TOOL_BUTTON,
+    text: 'Open',
+    data: {
+      icon: 'folder',
+      testid: 'open',
+      visible: true,
+    },
+  },
+]
