30d5842fc283914743089e9a7cd95c3e20be9d76
Olli-Pekka Pohjola <op@diory.me>
2021-11-02 23:54:12 +0200

    DDA-176: Remove contexts from grid scroll

--- src/react/components/DragDropBackground.js
+++ src/react/components/DragDropBackground.js
@@ -4,15 +4,17 @@ import Box from 'ui-box'
 import Draggable, { types } from './Draggable'
 import Droppable from './Droppable'
 
-const DragDropBackground = forwardRef(({ diory, onDrop, ...props }, ref) => (
-  <Box ref={ref} {...props}>
+const DragDropBackground = forwardRef(({ diory, onDrop, children, ...props }, ref) => (
+  <Box ref={ref} height="100%" {...props}>
     <Droppable
       type={types.DIORY}
       style={{ height: '100%' }}
       isOverStyle={{ backgroundColor: 'white', opacity: 0.5 }}
       onDrop={({ id }) => onDrop({ droppedId: diory.id, draggedId: id })}
     >
-      <Draggable id={diory.id} type={types.DIORY} />
+      <Draggable id={diory.id} type={types.DIORY}>
+        {children}
+      </Draggable>
     </Droppable>
   </Box>
 ))
