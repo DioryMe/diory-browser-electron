1032827217294046fcb5cb2e45302cd9874e0165
Jouni Alanen <jouni@diory.me>
2021-11-05 15:55:36 +0200

    Fix testcafe-e2e-3.test.js

--- electron/spec/testcafe-e2e-3.test.js
+++ electron/spec/testcafe-e2e-3.test.js
@@ -3,24 +3,24 @@ import { Selector } from 'testcafe'
 // eslint-disable-next-line no-undef
 fixture`GIVEN welcome room`.page('../../build/index.html')
 
-test('Import example-folder and choose it again', async (t) => {
+test('Import diory-demo-content with diograph.json and choose it again', async (t) => {
   const chooseFolderButton = Selector('div').withExactText(
     '+ Choose where your Diory folder is located on this Mac'
   )
   const dioryCount = Selector('.ub-flx_1-0-360px').count
-  const exampleFolderDiory = Selector('div').withExactText('testcafe-diograph-folder')
+  const exampleFolderDiory = Selector('div').withExactText('example-folder')
   const settingsButton = Selector('[data-icon=cog]')
 
   await t
     .click(chooseFolderButton)
     .expect(dioryCount)
-    .eql(0) // Welcome screen doesn't have any diories
+    .eql(8)
     // Import example-folder folder
     .click('[data-testid="tools-button"]')
     .click('[data-testid="import-button"]')
     .click('#FOLDER_IMPORT')
     .expect(dioryCount)
-    .eql(1)
+    .eql(9)
     .click(exampleFolderDiory)
     .expect(dioryCount)
     .eql(9)
@@ -28,12 +28,13 @@ test('Import example-folder and choose it again', async (t) => {
     .click(settingsButton)
     .click(chooseFolderButton)
     .expect(dioryCount)
-    .eql(0) // Welcome screen doesn't have any diories
+    .eql(9)
     // Import example-folder folder again
     .click('[data-testid="tools-button"]')
     .click('[data-testid="import-button"]')
     .click('#FOLDER_IMPORT')
     .expect(dioryCount)
-    .eql(1)
+    .eql(10)
   // TODO: Check that the diory name is not "example-folder" but "example-folder-2021-01-01T123456"
+  // TODO2: Try searching something to verify that the store has been cleared (requires creating new diory in first phase)
 })
