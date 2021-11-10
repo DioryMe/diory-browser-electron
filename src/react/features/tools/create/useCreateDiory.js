19d9c5ff4ad5547a8ae28e65c9fa700ad3421fdb
Jouni Alanen <jouni@diory.me>
2021-11-10 21:51:53 +0200

    POC-2021-11: Revert disabling adding diory to hand when it is created

--- src/react/features/tools/create/useCreateDiory.js
+++ src/react/features/tools/create/useCreateDiory.js
@@ -3,6 +3,7 @@ import { v4 as uuid } from 'uuid'
 import { useDispatchActions, useStore } from '../../../store'
 import { inactivateButton } from '../../buttons/actions'
 import { useDiograph } from '../../diograph/useDiograph'
+import { addDioryToHand } from '../actions'
 
 import { createDiory, createLink } from '../../diograph/actions'
 import { getDefaultImage } from '../../../../shared/getDefaultImage'
@@ -23,6 +24,7 @@ export const useCreateDiory = () => {
       dispatch(createDiory({ ...newDiory, id }))
       dispatch(createLink(story, { id }))
       dispatch(inactivateButton())
+      dispatch(addDioryToHand(id))
     }
   }
 }
