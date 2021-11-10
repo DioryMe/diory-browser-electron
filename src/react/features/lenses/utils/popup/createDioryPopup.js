62651a01cc1f2abcbb4cb312792ce8b2630bcd40
Jouni Alanen <jouni@diory.me>
2021-11-10 20:52:47 +0200

    POC-2021-11: Fix showing images in map & timeline popups

--- src/react/features/lenses/utils/popup/createDioryPopup.js
+++ src/react/features/lenses/utils/popup/createDioryPopup.js
@@ -1,19 +1,19 @@
 import L from 'leaflet'
 import { convertToFileUrl } from '../../../../utils'
 
-const getPopupStyle = ({ image }) =>
+const getPopupStyle = ({ image }, folderLocation) =>
   [
     'overflow: hidden',
     'min-width: 400px',
     'min-height: 200px',
     `background-color: #fa7921`,
-    `background-image: url('${convertToFileUrl(image)}')`,
+    `background-image: url('${convertToFileUrl(image, folderLocation)}')`,
     'background-size: cover',
     'background-position: center',
     'background-repeat: no-repeat',
   ].join(';')
 
-export const createDioryPopup = ({ diory = {} }) => {
+export const createDioryPopup = ({ diory = {} }, folderLocation) => {
   const elements = [
     diory.text &&
       `<div style="margin: 16px; font-size: 16px; font-weight: bold; color: white">${diory.text}</div>`,
@@ -21,7 +21,7 @@ export const createDioryPopup = ({ diory = {} }) => {
     .filter(Boolean)
     .join('')
 
-  const content = `<div style="${getPopupStyle(diory)}">${elements}</div>`
+  const content = `<div style="${getPopupStyle(diory, folderLocation)}">${elements}</div>`
   const popup = L.popup({
     closeButton: false,
   }).setContent(content)
--- src/react/features/lenses/utils/popup/useLinkPopups.js
+++ src/react/features/lenses/utils/popup/useLinkPopups.js
@@ -1,19 +1,22 @@
 import { useEffect } from 'react'
+import { useStore } from '../../../../store'
 import { createDioryPopup } from './createDioryPopup'
 
 export const useLinkPopups = (markerRefs, memories) => {
+  const [{ folderLocation }] = useStore((state) => state.diograph)
+
   useEffect(() => {
     if (markerRefs.current) {
       markerRefs.current
         .filter((marker) => !marker.getPopup())
         .forEach((marker) => {
           const diory = memories.find(({ id }) => id === marker.dioryId)
-          const popup = createDioryPopup({ diory })
+          const popup = createDioryPopup({ diory }, folderLocation)
           marker.bindPopup(popup, {
             maxWidth: 600,
             autoPan: false,
           })
         })
     }
-  }, [markerRefs, memories])
+  }, [markerRefs, memories, folderLocation])
 }
