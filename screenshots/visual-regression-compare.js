3b1dd72e44f6ac5540f47b10d39bb3f6520ec3b8
Jouni Alanen <jouni@diory.me>
2021-11-15 21:57:58 +0200

    DDA-180: Visual regression compare tool v0.1: Dockerized compare script, baseline images and shell script for iterating baseline images

---
+++ screenshots/visual-regression-compare.js
@@ -0,0 +1,58 @@
+/*
+## Run E2E / Visual Regression tests
+
+```
+brew install pkg-config cairo pango libpng jpeg giflib librsvg
+
+node visual-regression-compare.js \
+  baseline/testcafe-e2e-1/home-with-tools.png \
+  test/testcafe-e2e-1/home-with-tools.png
+```
+*/
+
+const Rembrandt = require('rembrandt')
+const fs = require('fs')
+const { argv } = require('process')
+
+const baselineImage = argv[2]
+const newlyTakenImage = argv[3]
+// const testFolder = argv[2]
+// const screenshotName = argv[3]
+// const screenshotRelativePath = `${testFolder}/${screenshotName}`
+// const baselineImage = `./screenshots/${screenshotRelativePath}`
+// const newlyTakenImage = `./tmp/screenshots/${screenshotRelativePath}`
+
+if (!fs.existsSync(baselineImage)) {
+  console.log("ERROR: Path didn't exist:", baselineImage)
+  process.exit(1)
+}
+
+if (!fs.existsSync(newlyTakenImage)) {
+  console.log("ERROR: Path didn't exist:", newlyTakenImage)
+  process.exit(1)
+}
+
+const rembrandt = new Rembrandt({
+  imageA: baselineImage,
+  imageB: newlyTakenImage,
+  thresholdType: Rembrandt.THRESHOLD_PIXELS,
+  // Number of different pixels
+  // - must be at least 2000 (or maybe 4000) because of id in window row
+  maxThreshold: 4000,
+  renderComposition: true,
+})
+
+// Run the comparison
+rembrandt.compare().then((result) => {
+  const passOrFail = result.passed ? 'PASS' : 'FAIL'
+  const difference = result.differences
+  const fileName = `./diffs/${passOrFail}-${difference}-difference.png`
+  // const fileName = `./tmp/screenshots/${passOrFail}-${difference}-${screenshotName}-difference.png`
+  fs.writeFileSync(fileName, result.compositionImage)
+
+  console.log(`${passOrFail}, Pixel Difference: ${difference}, ${fileName}`)
+
+  if (!result.passed) {
+    process.exit('1')
+  }
+})
