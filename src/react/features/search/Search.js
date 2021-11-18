f3e967266e928074b712d5c2f6e761b09975a8ba
Olli-Pekka Pohjola <op@diory.me>
2021-11-18 23:32:34 +0200

    DDA-187: Extract search bar

--- src/react/features/search/Search.js
+++ src/react/features/search/Search.js
@@ -1,28 +1,18 @@
 import React from 'react'
-import { Pane } from 'evergreen-ui'
 
 import { useStore } from '../../store'
 
 import SearchCreateDioryButton from './SearchCreateDioryButton'
 import SearchResults from './SearchResults'
+import SearchBar from './SearchBar'
 
 const Search = (props) => {
   const [{ showSearchBar }] = useStore((state) => state.search)
-
   return showSearchBar ? (
-    <Pane
-      background="tint2"
-      position="fixed"
-      top={48}
-      right={0}
-      bottom={0}
-      display="flex"
-      flexDirection="column"
-      {...props}
-    >
+    <SearchBar {...props}>
       <SearchCreateDioryButton />
       <SearchResults />
-    </Pane>
+    </SearchBar>
   ) : null
 }
 
