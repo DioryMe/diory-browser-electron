d315faad67b20ab06b626688d3ad663631ba190e
Olli-Pekka Pohjola <op@diory.me>
2021-11-18 23:25:31 +0200

    DDA-187: Search bar next to lenses

--- src/react/features/lenses/Lenses.js
+++ src/react/features/lenses/Lenses.js
@@ -1,13 +1,12 @@
 import React from 'react'
 
 import Fullscreen from '../../components/Fullscreen'
-
 import GridLens from './grid/GridLens'
 import TimelineLens from './timeline/TimelineLens'
 import MapLens from './map/MapLens'
 
-const Lenses = () => (
-  <Fullscreen marginTop={48} zIndex={-1}>
+const Lenses = (props) => (
+  <Fullscreen marginTop={48} {...props}>
     <GridLens />
     <TimelineLens />
     <MapLens />
