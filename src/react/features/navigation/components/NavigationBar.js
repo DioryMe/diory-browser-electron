8f5c078ee81b99e69a2bba13c6bad0abf3899a6a
Olli-Pekka Pohjola <op@diory.me>
2021-11-21 22:04:47 +0200

    DDA-191: Add settings to navigation

--- src/react/features/navigation/components/NavigationBar.js
+++ src/react/features/navigation/components/NavigationBar.js
@@ -1,44 +1,24 @@
 import React from 'react'
 import { Pane } from 'evergreen-ui'
 
-import { useDispatchActions } from '../../../store'
-
-import { resetStore } from '../../../store/reducer'
-import { getDiograph } from '../../diograph/actions'
-
 import NavigationButtons from './NavigationButtons'
 import LensesNavigation from '../../lenses/Navigation'
 import SearchNavigation from '../../search/Navigation'
-import Icon from '../../../components/Icon'
 
-const NavigationBar = () => {
-  const { dispatch } = useDispatchActions()
-  return (
-    <Pane
-      display="flex"
-      justifyContent="space-between"
-      padding={8}
-      background="tint2"
-      zIndex={15}
-      position="fixed"
-      width="100%"
-    >
-      <NavigationButtons display="flex" alignSelf="center" />
-      <LensesNavigation display="flex" />
-      <SearchNavigation display="flex" alignSelf="center" />
-      <Icon
-        size={20}
-        icon="cog"
-        marginRight="24px"
-        style={{ cursor: 'hand' }}
-        onClick={() => {
-          dispatch(resetStore())
-          // Default folderLocation needs to be changed to null
-          dispatch(getDiograph({}, undefined, null))
-        }}
-      />
-    </Pane>
-  )
-}
+const NavigationBar = () => (
+  <Pane
+    display="flex"
+    justifyContent="space-between"
+    padding={8}
+    background="tint2"
+    zIndex={15}
+    position="absolute"
+    width="100%"
+  >
+    <NavigationButtons display="flex" alignSelf="center" />
+    <LensesNavigation display="flex" />
+    <SearchNavigation display="flex" alignSelf="center" />
+  </Pane>
+)
 
 export default NavigationBar
