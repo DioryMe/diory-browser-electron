c010416ddab2b819a40c99f1a1df5ab97e52c809
Jouni Alanen <jouni@diory.me>
2021-11-25 22:52:40 +0200

    DDA-197: Combine e2e-test-2 with test-1

--- electron/spec/testcafe-e2e-1.test.js
+++ electron/spec/testcafe-e2e-1.test.js
@@ -11,29 +11,50 @@ test('Import diory-demo-content', async (t) => {
   const importedFolderDiory = Selector('div').withExactText('testcafe-diograph-folder')
   const doneButton = Selector('button').withText('Done')
 
+  const someFolderDiory = Selector('div').withExactText('some-folder')
+  const someMusicDiory = Selector('div').withExactText('some-music.mp3')
+  const dataFieldValue = Selector('input#data').value
+  const deleteButton = Selector('button').withText('Delete')
+
   await t
     .click(chooseFolderButton)
-    .wait(3000) // Wait for welcome screen to disappear
+    .wait(2200) // Wait for welcome screen to disappear
     .expect(dioryCount)
-    .eql(0) // Welcome screen doesn't have any diories
-    // Import diory-demo-content folder
-    .click('[data-testid="tools-button"]')
-    .click('[data-testid="import-button"]')
-    .click('#FOLDER_IMPORT')
-    .expect(dioryCount)
-    .eql(1)
-    .click(importedFolderDiory)
-    .expect(dioryCount)
-    .eql(8)
+    .eql(0) // Default diograph doesn't have any diories
+    // ----- Create diory
     .click('[data-testid="tools-button"]')
     .click('[data-testid="import-button"]')
     .click('#CREATE_TOOL_BUTTON')
     .typeText('input#text', 'New diory')
     .click(doneButton)
-    .wait(1000) // Wait for SAVE_ROOM to complete and possibly raise an error
-    .expect(dioryCount)
-    .eql(9)
-    .click('button[data-testid="home"]')
+    .wait(1000) // Wait for SAVE_DIOGRAPH to complete and possibly raise an error
     .expect(dioryCount)
     .eql(1)
+    // ------ Import diory-demo-content folder
+    .click('[data-testid="tools-button"]')
+    .click('[data-testid="import-button"]')
+    .click('#FOLDER_IMPORT')
+    .expect(dioryCount)
+    .eql(2)
+    .click(importedFolderDiory)
+    .expect(dioryCount)
+    .eql(9)
+    // ------ Delete diory
+    .click('[data-testid="tools-button"]')
+    .click('[data-testid="delete-button"]')
+    .click(someFolderDiory)
+    .click(deleteButton)
+    .click('[data-testid="tools-button"]') // Close tool bar
+    .wait(1000) // Wait for SAVE_ROOM to complete and possibly raise an error
+    .expect(dioryCount)
+    .eql(7)
+    // Check diory.data
+    .click('[data-testid="tools-button"]')
+    .click('[data-testid="update-button"]')
+    .click(someMusicDiory)
+    .expect(dataFieldValue)
+    .eql(
+      '[{"@context":"https://schema.org","@type":"AudioObject","contentUrl":"testcafe-diograph-folder/some-music.mp3","encodingFormat":"audio/mpeg"}]'
+    )
+    .click(doneButton)
 })
