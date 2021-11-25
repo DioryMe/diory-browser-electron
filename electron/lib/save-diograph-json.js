4f66cdcd8ab3d5fb066dbbafbf400758d2ae20c7
Jouni Alanen <jouni@diory.me>
2021-11-25 22:17:32 +0200

    DDA-197: Convert SAVE_ROOM to SAVE_DIOGRAPH (and use saveDiographJson to it)

--- electron/lib/save-diograph-json.js
+++ electron/lib/save-diograph-json.js
@@ -1,6 +1,8 @@
 const fsPromise = require('fs').promises
+const path = require('path').posix
 
-exports.saveDiographJson = function saveDiographJson({ diographJsonPath, diograph, rootId }) {
+exports.saveDiographJson = function saveDiographJson({ dioryFolderLocation, diograph, rootId }) {
+  const diographJsonPath = path.join(dioryFolderLocation, 'diograph.json')
   const fileContent = { rootId, diograph }
   return fsPromise.writeFile(diographJsonPath, JSON.stringify(fileContent, null, 2))
 }
