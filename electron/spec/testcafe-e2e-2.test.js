56c19c1bbfcd84e6bd9756d85e718c8b3b206f1a
Jouni Alanen <jouni@diory.me>
2021-11-25 22:55:20 +0200

    DDA-197: Remove testcafe-e2e-2.test.js + rename 3 -> 2

--- electron/spec/testcafe-e2e-2.test.js
+++ electron/spec/testcafe-e2e-2.test.js
@@ -3,49 +3,39 @@ import { Selector } from 'testcafe'
 // eslint-disable-next-line no-undef
 fixture`GIVEN welcome room`.page('../../build/index.html')
 
-test('Delete diory and check diory.data relative path', async (t) => {
+test('Import diory-demo-content with diograph.json and choose it again', async (t) => {
   const chooseFolderButton = Selector('div').withExactText(
     '+ Choose where your Diory is located on this Computer'
   )
   const dioryCount = Selector('.ub-flx_1-0-360px').count
-  const importedFolderDiory = Selector('div').withExactText('testcafe-diograph-folder')
-
-  const someFolderDiory = Selector('div').withExactText('some-folder')
-  const someMusicDiory = Selector('div').withExactText('some-music.mp3')
-  const dataFieldValue = Selector('input#data').value
-  const deleteButton = Selector('button').withText('Delete')
-  const doneButton = Selector('button').withText('Done')
+  const exampleFolderDiory = Selector('div').withExactText('example-folder')
 
   await t
     .click(chooseFolderButton)
     .wait(3000) // Wait for welcome screen to disappear
     .expect(dioryCount)
-    .eql(0) // Welcome screen doesn't have any diories
+    .eql(8)
     // Import example-folder folder
     .click('[data-testid="tools-button"]')
     .click('[data-testid="import-button"]')
     .click('#FOLDER_IMPORT')
     .expect(dioryCount)
-    .eql(1)
-    .click(importedFolderDiory)
+    .eql(9)
+    .click(exampleFolderDiory)
     .expect(dioryCount)
     .eql(9)
-    // Delete diory
-    .click('[data-testid="tools-button"]')
-    .click('[data-testid="delete-button"]')
-    .click(someFolderDiory)
-    .click(deleteButton)
-    .click('[data-testid="tools-button"]') // Close tool bar
-    .wait(1000) // Wait for SAVE_ROOM to complete and possibly raise an error
-    .expect(dioryCount)
-    .eql(8)
-    // Check diory.data
-    .click('[data-testid="tools-button"]')
-    .click('[data-testid="update-button"]')
-    .click(someMusicDiory)
-    .expect(dataFieldValue)
-    .eql(
-      '[{"@context":"https://schema.org","@type":"AudioObject","contentUrl":"testcafe-diograph-folder/some-music.mp3","encodingFormat":"audio/mpeg"}]'
-    )
-    .click(doneButton)
+  // FUNCTIONALITY CURRENTLY NOT AVAILABLE
+  // Re-choose example-folder
+  // .click('[data-testid="settings-navigation"]')
+  // .click('[data-testid="settings-dioryFolderLocation"]')
+  // .expect(dioryCount)
+  // .eql(9)
+  // // Import example-folder folder again
+  // .click('[data-testid="tools-button"]')
+  // .click('[data-testid="import-button"]')
+  // .click('#FOLDER_IMPORT')
+  // .expect(dioryCount)
+  // .eql(10)
+  // TODO: Check that the diory name is not "example-folder" but "example-folder-2021-01-01T123456"
+  // TODO2: Try searching something to verify that the store has been cleared (requires creating new diory in first phase)
 })
