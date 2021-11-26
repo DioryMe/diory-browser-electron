0aeeb77e0f5a5137bc53de2c24ba4f15603eef5a
Jouni Alanen <jouni@diory.me>
2021-11-26 16:41:45 +0200

    DDA-197: Spec for get-diograph.js + necessary changes to get it pass

--- electron/lib/get-diograph.js
+++ electron/lib/get-diograph.js
@@ -2,13 +2,18 @@ const fs = require('fs')
 const path = require('path').posix
 const { readDiographJson } = require('./read-diograph-json')
 
-exports.getDiograph = async function getDiograph(dioryFolderLocation) {
-  const diographJsonPath = path.join(dioryFolderLocation, 'diograph.json')
-
+exports.getDiograph = function getDiograph(dioryFolderLocation) {
   if (!fs.existsSync(dioryFolderLocation)) {
-    const errorMessage = `GET_DIOGRAPH: Diory folder location didn't exist anymore: ${dioryFolderLocation}`
+    const errorMessage = `GET_DIOGRAPH: Provided dioryFolderLocation didn't exist anymore: ${dioryFolderLocation}`
     throw new Error(errorMessage)
   }
 
-  return readDiographJson(diographJsonPath)
+  const diographJsonPath = path.join(dioryFolderLocation, 'diograph.json')
+
+  if (!fs.existsSync(diographJsonPath)) {
+    const errorMessage = `GET_DIOGRAPH: Provided dioryFolderLocation didn't contain diograph.json: ${dioryFolderLocation}`
+    throw new Error(errorMessage)
+  }
+
+  return readDiographJson({ diographJsonPath })
 }
