096fa67e8bec1b744bbf33c3f28ac05edf73cb50
Jouni Alanen <jouni@diory.me>
2021-11-26 10:00:36 +0200

    DDA-196: Delete all obsolete channels, their spec & fixtures and mentions from preload.js

--- electron/channels/get-room-channel.js
+++
@@ -1,5 +0,0 @@
-const { readDiographJson } = require('../lib/read-diograph-json')
-
-exports.getRoomEventHandler = async function getRoomEventHandler({ address }) {
-  return readDiographJson(address)
-}
