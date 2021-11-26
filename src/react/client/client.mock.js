af78a6ad24d715fed4e786ad4e0b5d26022e0986
Jouni Alanen <jouni@diory.me>
2021-11-26 07:51:04 +0200

    DDA-197: Fix test content with client.mock.js => remove GET_HOME & GET_ROOM and add GET_DIORY_FOLDER_LOCATION.json

--- src/react/client/client.mock.js
+++ src/react/client/client.mock.js
@@ -1,12 +1,10 @@
-import GET_ROOM from './mockResponses/GET_ROOM.json'
-import GET_HOME from './mockResponses/GET_HOME.json'
+import GET_DIORY_FOLDER_LOCATION from './mockResponses/GET_DIORY_FOLDER_LOCATION.json'
 import GET_DIOGRAPH from './mockResponses/GET_DIOGRAPH.json'
 
 export const mockResponse = (channel) => {
   const data = {
-    GET_HOME,
-    GET_ROOM,
     GET_DIOGRAPH,
+    GET_DIORY_FOLDER_LOCATION,
   }[channel]
 
   if (!data) {
