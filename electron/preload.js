64339b622b3a7f6190057d546bb1801419cb9f8c
Jouni Alanen <jouni@diory.me>
2021-11-26 10:00:36 +0200

    DDA-196: Delete all obsolete channels, their spec & fixtures and mentions from preload.js

--- electron/preload.js
+++ electron/preload.js
@@ -3,11 +3,6 @@ const frontendLogger = require('electron-log')
 
 const { channels } = require('../src/shared/constants')
 const { eventHandlerWrapper } = require('./channels/channel-util')
-const { getHomeEventHandler } = require('./channels/get-home-channel')
-const { getRoomEventHandler } = require('./channels/get-room-channel')
-const { generateDiographEventHandler } = require('./channels/generate-diograph-channel')
-// const { saveRoomEventHandler } = require('./channels/save-room-channel')
-const { saveHomeEventHandler } = require('./channels/save-home-channel')
 
 const { importFolderEventHandler } = require('./channels/import-folder-channel')
 const { getDiograph } = require('./channels/get-diograph-channel')
@@ -16,15 +11,6 @@ const { setDioryFolderLocation } = require('./channels/set-diory-folder-location
 const { saveDiographJson } = require('./lib/save-diograph-json')
 
 contextBridge.exposeInMainWorld('channelsApi', {
-  [channels.GET_ROOM]: eventHandlerWrapper(channels.GET_ROOM, getRoomEventHandler),
-  [channels.GET_HOME]: eventHandlerWrapper(channels.GET_HOME, getHomeEventHandler),
-  [channels.GENERATE_DIOGRAPH]: eventHandlerWrapper(
-    channels.GENERATE_DIOGRAPH,
-    generateDiographEventHandler
-  ),
-  [channels.SAVE_ROOM]: eventHandlerWrapper(channels.SAVE_ROOM, saveDiographJson),
-  [channels.SAVE_HOME]: eventHandlerWrapper(channels.SAVE_HOME, saveHomeEventHandler),
-
   [channels.IMPORT_FOLDER]: eventHandlerWrapper(channels.IMPORT_FOLDER, importFolderEventHandler),
   [channels.GET_DIOGRAPH]: eventHandlerWrapper(channels.GET_DIOGRAPH, getDiograph),
   [channels.SAVE_DIOGRAPH]: eventHandlerWrapper(channels.SAVE_DIOGRAPH, saveDiographJson),
