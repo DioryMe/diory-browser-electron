4cc1d5f3c7fd7aacd4531125f49dc96efe71dc64
Jouni Alanen <jouni@diory.me>
2021-11-26 11:50:53 +0200

    DDA-196: Implement channelLogger as a simplier replacement for eventHandlerWrapper

--- electron/preload.js
+++ electron/preload.js
@@ -8,12 +8,22 @@ const { getDioryFolderLocation } = require('./lib/get-diory-folder-location')
 const { setDioryFolderLocation } = require('./lib/set-diory-folder-location')
 const { saveDiographJson } = require('./lib/save-diograph-json')
 
+function channelLogger(handler, params) {
+  console.log(`BACK-REQ: ${handler.name} called with`, params)
+  const response = handler(params)
+  response.then((solvedResponse) => {
+    console.log(`BACK-RES: ${handler.name} responded with`, solvedResponse)
+  })
+  return response
+}
+
+// NOTE: Currently channels can receive only one parameter!
 contextBridge.exposeInMainWorld('channelsApi', {
-  [channels.IMPORT_FOLDER]: importFolder,
-  [channels.GET_DIOGRAPH]: getDiograph,
-  [channels.SAVE_DIOGRAPH]: saveDiographJson,
-  [channels.GET_DIORY_FOLDER_LOCATION]: getDioryFolderLocation,
-  [channels.SET_DIORY_FOLDER_LOCATION]: setDioryFolderLocation,
+  [channels.IMPORT_FOLDER]: (params) => channelLogger(importFolder, params),
+  [channels.GET_DIOGRAPH]: (params) => channelLogger(getDiograph, params),
+  [channels.SAVE_DIOGRAPH]: (params) => channelLogger(saveDiographJson, params),
+  [channels.GET_DIORY_FOLDER_LOCATION]: (params) => channelLogger(getDioryFolderLocation, params),
+  [channels.SET_DIORY_FOLDER_LOCATION]: (params) => channelLogger(setDioryFolderLocation, params),
   showItemInFolder: async (fullPath) => shell.showItemInFolder(fullPath),
   openItemInDesktopManner: (path) => shell.openPath(path),
   openWebsiteInBrowser: (url) => shell.openExternal(url),
