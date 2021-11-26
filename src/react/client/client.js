994f433a5e383fae3a0e390a18a2cb134b8c6125
Jouni Alanen <jouni@diory.me>
2021-11-26 11:17:17 +0200

    DDA-196: Remove all electron-log logging => see if we can manage without it

--- src/react/client/client.js
+++ src/react/client/client.js
@@ -1,50 +1,22 @@
 import { mockResponse } from './client.mock'
 import { invokeAlertDialog } from './alertDialog'
 
-let frontendLogger
-
-if (window.channelsApi) {
-  // eslint-disable-next-line prefer-destructuring
-  frontendLogger = window.channelsApi.frontendLogger
-}
-
-if (process.env.NODE_ENV === 'test') {
-  frontendLogger = { info: () => {}, error: () => {} }
-}
-
-if (process.env.NODE_ENV === 'development') {
-  frontendLogger = { info: console.log, error: console.error }
-}
-
 export const invokeChannel = (channel, params) => {
   // Development uses mock responses
   if (process.env.NODE_ENV === 'development') {
-    frontendLogger.info('MOCK: Frontend IPC invoke:', channel, params)
     return mockResponse(channel, params)
   }
-
-  frontendLogger.info(
-    'Frontend IPC invoke:',
-    channel,
-    channel === 'SAVE_ROOM' ? params.path : params
-  )
 
   // Called when backend successfully responds (response object or Error object)
   const success = (responseObject) => {
     if (responseObject instanceof Error) {
       invokeAlertDialog(responseObject)
     }
-    frontendLogger.info(
-      'Frontend IPC response:',
-      channel,
-      channel === 'GET_ROOM' && responseObject ? responseObject.rootId : responseObject
-    )
     return responseObject
   }
 
   // Called when backend throws error
   const error = (errorObject) => {
-    frontendLogger.error('ERROR: Frontend IPC response:', channel, errorObject)
     invokeAlertDialog(errorObject)
     return {}
   }
