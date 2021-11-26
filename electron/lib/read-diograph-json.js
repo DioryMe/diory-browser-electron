b15c253fad2e397554ab912f1009f1eb2c7e1f33
Jouni Alanen <jouni@diory.me>
2021-11-26 15:07:26 +0200

    DDA-197: Spec for read-diograph-json.js + necessary changes to get them pass

--- electron/lib/read-diograph-json.js
+++ electron/lib/read-diograph-json.js
@@ -1,11 +1,17 @@
 const fs = require('fs')
 
-exports.readDiographJson = function readDiographJson(diographJsonPath) {
+exports.readDiographJson = function readDiographJson({ diographJsonPath }) {
   if (!fs.existsSync(diographJsonPath)) {
     const errorMessage = `readDiographJson: Provided diograph folder path doesn't exist (${diographJsonPath}). Did you use FileDialog to select it?`
     throw new Error(errorMessage)
   }
 
-  const raw = fs.readFileSync(diographJsonPath)
-  return JSON.parse(raw)
+  try {
+    const raw = fs.readFileSync(diographJsonPath)
+    const json = JSON.parse(raw)
+    return json
+  } catch (e) {
+    console.log('readDiographJson failed when reading', diographJsonPath, e.message)
+    return undefined
+  }
 }
