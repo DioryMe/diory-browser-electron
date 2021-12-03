accad47ea3bf7bbd89e0d7c821c472572e423de7
Jouni Alanen <jouni@diory.me>
2021-12-03 16:06:17 +0200

    DDA-197: Enforce mocking conventions

--- electron/lib/save-diograph.spec.js
+++ electron/lib/save-diograph.spec.js
@@ -1,25 +1,27 @@
-jest.mock('fs', () => ({
-  promises: {
-    writeFile: jest.fn().mockResolvedValue(),
-  },
-}))
-
 const fs = require('fs')
 const { saveDiograph } = require('./save-diograph')
 
-const mockedDiograph = {
+const someDiograph = {
   rootId: 'some-diory',
   diograph: {
     'some-diory': { id: 'some-diory' },
   },
 }
 
+// For some reason this maybe should be placed on top of the file
+// - https://stackoverflow.com/questions/64947786/how-to-mock-fs-promises-writefile-with-jest#comment114826489_64948126
+jest.mock('fs', () => ({
+  promises: {
+    writeFile: jest.fn().mockResolvedValue(),
+  },
+}))
+
 describe('saveDiograph', () => {
   it('calls writeFile with correct params', async () => {
     const dioryFolderLocation = '/some/path'
-    const prettyPrintedJson = JSON.stringify(mockedDiograph, null, 2)
+    const prettyPrintedJson = JSON.stringify(someDiograph, null, 2)
 
-    await saveDiograph({ dioryFolderLocation, ...mockedDiograph })
+    await saveDiograph({ dioryFolderLocation, ...someDiograph })
 
     expect(fs.promises.writeFile).toHaveBeenCalledTimes(1)
     expect(fs.promises.writeFile).toHaveBeenCalledWith(
