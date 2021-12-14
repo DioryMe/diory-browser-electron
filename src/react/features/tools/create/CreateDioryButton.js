463fd5b73d948c00190be59477e21ef797becc87
OP <op@diory.me>
2021-12-14 21:08:41 +0200

    DDA-208: Refactor create diory button

--- src/react/features/tools/create/CreateDioryButton.js
+++ src/react/features/tools/create/CreateDioryButton.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { v4 as uuid } from 'uuid'
 
 import { Button } from 'evergreen-ui'
 import { useDispatch } from '../../../store'
@@ -8,22 +7,15 @@ import { createDiory } from '../../diograph/diographActions'
 
 import Icon from '../../../components/Icon'
 
-const useCreateDiory = () => {
-  const dispatch = useDispatch()
-  return (props) => {
-    dispatch(createDiory({ ...props, id: uuid() }))
-  }
-}
-
 const CreateDioryButton = ({ text }) => {
-  const createDiory = useCreateDiory()
+  const dispatch = useDispatch()
   return (
     <Button
       appearance="primary"
       intent="success"
       margin={8}
       iconBefore={<Icon icon="plus" />}
-      onClick={() => createDiory({ text })}
+      onClick={() => dispatch(createDiory({ text }))}
       data-testid="add-button"
     >
       {text}
