850bcc65407a578532b53f05292de25569c57cd1
OP <op@diory.me>
2021-12-14 21:25:48 +0200

    DDA-208: Refactor search for story book

---
+++ src/react/features/search/SearchView.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Pane } from 'evergreen-ui'
+
+import SearchBar from './SearchBar'
+import CreateDioryButton from '../tools/create/CreateDioryButton'
+import SearchResult from './SearchResult'
+
+const SearchView = ({ query, results, onClick, onDrop, ...props }) => (
+  <SearchBar {...props}>
+    {query && <CreateDioryButton text={query} />}
+    <Pane {...props} paddingX={8} overflow="auto">
+      {results.map((diory) => (
+        <SearchResult key={diory.id} diory={diory} onClick={onClick} onDrop={onDrop} />
+      ))}
+    </Pane>
+  </SearchBar>
+)
+
+SearchView.propTypes = {
+  query: PropTypes.string.isRequired,
+  results: PropTypes.array.isRequired,
+  onClick: PropTypes.func.isRequired,
+  onDrop: PropTypes.func.isRequired,
+}
+
+export default SearchView
