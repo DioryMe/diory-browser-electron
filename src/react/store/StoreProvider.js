e0b548a8c8c4307250df5f3797d1e0ec562e2f0b
OP <op@diory.me>
2021-12-30 21:20:55 +0200

    DDA-214: Add redux developer tools

--- src/react/store/StoreProvider.js
+++ src/react/store/StoreProvider.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { createStore } from 'redux'
+import { createStore, compose } from 'redux'
 import { Provider } from 'react-redux'
 
 import { reducer } from './reducer'
 
-const store = createStore(reducer)
+const enhancers = compose(
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+const store = createStore(reducer, {}, enhancers)
 
 const StoreProvider = ({ children }) => <Provider store={store}>{children}</Provider>
 
