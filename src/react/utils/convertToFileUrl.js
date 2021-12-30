f5df73a02b4697af810441063c228ed922f6e9ac
Jouni Alanen <jouni@diory.me>
2021-12-30 09:41:38 +0200

    DDA-212: Replace importing fileUrl by copy-pasting the function from the Github repo and modified it to remove unnecessary use of 'path' (as webpack 5 doesn't include polyfills for node.js core modules anymore)

--- src/react/utils/convertToFileUrl.js
+++ src/react/utils/convertToFileUrl.js
@@ -1,4 +1,21 @@
-import fileUrl from 'file-url'
+function fileUrl(filePath) {
+  if (typeof filePath !== 'string') {
+    throw new TypeError(`Expected a string, got ${typeof filePath}`)
+  }
+
+  let pathName = filePath
+
+  pathName = pathName.replace(/\\/g, '/')
+
+  // Windows drive letter must be prefixed with a slash.
+  if (pathName[0] !== '/') {
+    pathName = `/${pathName}`
+  }
+
+  // Escape required characters for path components.
+  // See: https://tools.ietf.org/html/rfc3986#section-3.3
+  return encodeURI(`file://${pathName}`).replace(/[?#]/g, encodeURIComponent)
+}
 
 export const convertToFileUrl = (relativePath, folderLocation) => {
   // Base64 images
