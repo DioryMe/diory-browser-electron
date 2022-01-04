193b4cd7db5facd8bc7cfcfdad8330bbfd6e0199
Jouni Alanen <jouni@diory.me>
2022-01-04 09:38:13 +0200

    DDA-217: Replace window title with static Diory instead of browsing history breadcrumb (remove useDocumentTitle)

--- src/react/features/navigation/Navigation.js
+++ src/react/features/navigation/Navigation.js
@@ -1,20 +1,15 @@
 import React from 'react'
 
-import { useDocumentTitle } from './hooks'
-
 import NavigationBar from './components/NavigationBar'
 import NavigationToLeft from './components/NavigationToLeft'
 import NavigationToRight from './components/NavigationToRight'
 
-const Navigation = () => {
-  useDocumentTitle()
-  return (
-    <>
-      <NavigationBar />
-      <NavigationToLeft />
-      <NavigationToRight />
-    </>
-  )
-}
+const Navigation = () => (
+  <>
+    <NavigationBar />
+    <NavigationToLeft />
+    <NavigationToRight />
+  </>
+)
 
 export default Navigation
