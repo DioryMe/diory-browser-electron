41853bbd30b39e4badaa7f7be5f0cfe5ef533281
Jouni Alanen <jouni@diory.me>
2022-01-11 10:32:28 +0200

    DDA-000: package-mac-notarized script to use aws vault + fix S3 upload command for multiple files

--- package-mac-notarized-publish.sh
+++ package-mac-notarized-publish.sh
@@ -1,6 +1,10 @@
 # !/bin/bash
 set -e
 
+AWS_USER=jvalanen
+# echo "Give aws user:"
+# read AWS_USER
+
 echo "Choose git-repo-branch:"
 read GIT_BRANCH
 
@@ -9,15 +13,6 @@ echo "AppleID email:"
 read apple_id
 echo "App-specific password for diory-browser-electron:"
 read -s apple_id_pass
-
-echo "Login to AWS..."
-echo "Input MFA from your Authenticator (jvalanen-private):"
-read mfa_code
-export AWS_TOKEN_CODE=$mfa_code
-export AWS_LOGIN_TMP=$(aws sts get-session-token --serial-number arn:aws:iam::037977746924:mfa/jvalanen-private --token-code $AWS_TOKEN_CODE)
-export AWS_ACCESS_KEY_ID=$(echo $AWS_LOGIN_TMP | jq -r .Credentials.AccessKeyId)
-export AWS_SECRET_ACCESS_KEY=$(echo $AWS_LOGIN_TMP | jq -r .Credentials.SecretAccessKey)
-export AWS_SESSION_TOKEN=$(echo $AWS_LOGIN_TMP | jq -r .Credentials.SessionToken)
 
 echo "Create a temp directory to ~/temp-package-mac..."
 # Remove directory if it exists
@@ -33,14 +28,15 @@ git checkout $GIT_BRANCH
 bash ./package-common.sh
 
 echo "Creating MacOS distribution packages..."
+# Intel macs
 APPLEID=$apple_id APPLEIDPASS=$apple_id_pass yarn package-mac --x64
+# M1 macs
 APPLEID=$apple_id APPLEIDPASS=$apple_id_pass yarn package-mac --arm64
 
-echo "Uploading the .dmg binary to AWS..."
-aws s3 cp dist/*.dmg s3://dda-downloads --acl public-read
+echo "Uploading the .dmg binaries to AWS..."
+aws-vault exec $AWS_USER -- aws s3 cp dist/ s3://dda-downloads --recursive --exclude "*" --include "*.dmg" --acl public-read
 echo "Triggering the pipeline..."
-aws codepipeline start-pipeline-execution --name DioryDemoApp-build-pipeline
-unset AWS_TOKEN_CODE AWS_LOGIN_TMP AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN
+aws-vault exec $AWS_USER -- aws codepipeline start-pipeline-execution --name DioryDemoApp-build-pipeline
 
 echo "Removing temp folder..."
 cd ~
