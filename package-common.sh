14837aebb012682e31eb1f185282ced28dbf1a61
Jouni Alanen <jouni@diory.me>
2022-01-11 10:04:08 +0200

    DDA-000: Cleanup / improve packaging process + add --arm64/M1 build to package-mac-notarized-publish.sh script

--- package-common.sh
+++ package-common.sh
@@ -3,8 +3,6 @@
 echo "Installing dependencies and building the app..."
 yarn && yarn build && yarn build-electron
 
-# BINARY_BUILD = 1
-
 echo "Add APP_PACKAGE_ENVS from electron-main.js"
 export APP_PACKAGE_ENVS="process.env.BINARY_BUILD=1";
 awk 'BEGIN{print ENVIRON["APP_PACKAGE_ENVS"]}{print}' build/electron-main.js > build/electron-main-tmp.js
