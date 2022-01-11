1a782bb211396740c17d8154ad05d18496096aeb
Jouni Alanen <jouni@diory.me>
2022-01-11 10:31:22 +0200

    DDA-000: Fix package-mac-release-candidate script

--- package-mac-release-candidate.sh
+++ package-mac-release-candidate.sh
@@ -1,14 +1,14 @@
 # !/bin/bash
 set -e
 
-# Run common stuff (Testcafe tests & BINARY_BUILD = 1)
-bash ./package-mac-common.sh
+# Run common stuff (build & add BINARY_BUILD=1 to electron-main.js)
+bash ./package-common.sh
 
 # Intel macs
-yarn package-mac-rc-x64
+yarn package-mac-rc --x64
 
 # M1 macs
-yarn package-mac-rc-arm64
+yarn package-mac-rc --arm64
 
 echo "Done."
 echo "All this was made with: yarn: $(yarn --version), node: $(node --version)"
