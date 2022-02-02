63b977bf5663815b2adb775075fcd7234c470583
Jouni Alanen <jouni@diory.me>
2022-02-02 13:59:19 +0200

    POC-000: Frontend image generator with a preview

--- src/react/features/tools/update/UpdateView.js
+++ src/react/features/tools/update/UpdateView.js
@@ -28,6 +28,59 @@ const useUpdateView = (diory = {}) => {
   }
 }
 
+const resizeImage = ({ file, maxWidth, maxHeight }) => {
+  const reader = new FileReader()
+  const image = new Image()
+  const canvas = document.createElement('canvas')
+
+  const resize = () => {
+    // Define the size of the canvas
+    let { width, height } = image
+    if (width > height) {
+      if (width > maxWidth) {
+        height *= maxWidth / width
+        width = maxWidth
+      }
+    } else if (height > maxHeight) {
+      width *= maxHeight / height
+      height = maxHeight
+    }
+    canvas.width = width
+    canvas.height = height
+
+    // Draw image to canvas and get data (in a form of dataUrl)
+    canvas.getContext('2d').drawImage(image, 0, 0, width, height)
+    const dataUrl = canvas.toDataURL('image/jpeg')
+
+    // Append preview image
+    const img = document.createElement('img')
+    img.src = dataUrl
+    document.getElementById('preview').appendChild(img)
+
+    // Convert data url to blob
+    return new Promise((resolve) => {
+      canvas.toBlob(resolve, 'image/jpeg')
+    })
+  }
+
+  return new Promise((resolve) => {
+    reader.onload = (readerEvent) => {
+      image.onload = () => resolve(resize())
+      image.src = readerEvent.target.result
+    }
+    reader.readAsDataURL(file)
+  })
+}
+
+const resizeAndSaveImage = (event) => {
+  const file = event.target.files[0]
+  resizeImage({ file, maxHeight: 360, maxWidth: 480 })
+    .then((blob) => {
+      console.log('Send blob to backend?', blob)
+    })
+    .catch((err) => console.log(err))
+}
+
 const UpdateView = ({ diory, title, isShown, onDone }) => {
   const { fields, setValue, updatedDiory, resetView } = useUpdateView(diory)
 
@@ -41,6 +94,8 @@ const UpdateView = ({ diory, title, isShown, onDone }) => {
       }}
       onCancel={resetView}
     >
+      <div id="preview" />
+      <input type="file" accept="image/*" onChange={resizeAndSaveImage} />
       {fields.map(({ key, label, format, value, autoFocus }) => (
         <TextInput
           id={key}
