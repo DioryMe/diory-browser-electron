7a108bdebe4c183fed391ed7d823081cdd8728b7
Jouni Alanen <jouni@diory.me>
2022-02-02 15:07:08 +0200

    POC-000: resizeAndSaveImage: Promisify properly and throw error

--- scripts/image-generator.js
+++ scripts/image-generator.js
@@ -1,7 +1,20 @@
 // Usage: node scripts/image-generator.js public/logo192.png test.jpg
 
+const { join } = require('path')
+const { DiographJson } = require('diograph-js')
 const { createCanvas, loadImage } = require('node-canvas')
 const fs = require('fs')
+
+const diographFolderPath = process.argv[2] || './public/diory-demo-content'
+const diographJson = new DiographJson({
+  path: join(diographFolderPath, 'diograph.json'),
+})
+
+const relativeThumbnailPath = 'images'
+const thumbnailPath = join(diographFolderPath, relativeThumbnailPath)
+if (!fs.existsSync(thumbnailPath)) {
+  fs.mkdirSync(thumbnailPath)
+}
 
 const resizeImage = async ({ sourceFilePath, maxWidth, maxHeight }) => {
   const canvas = createCanvas()
@@ -31,21 +44,49 @@ const resizeImage = async ({ sourceFilePath, maxWidth, maxHeight }) => {
   return resize()
 }
 
-const resizeAndSaveImage = ({ sourceFilePath, destinationFilePath }) => {
-  resizeImage({ sourceFilePath, maxHeight: 360, maxWidth: 480 })
-    .then((image) => {
+const resizeAndSaveImage = ({ sourceFilePath, destinationFilePath }) =>
+  new Promise((resolve) => {
+    resizeImage({ sourceFilePath, maxHeight: 360, maxWidth: 480 }).then((image) => {
       fs.writeFile(destinationFilePath, image, () => {
         console.log('Image saved!')
+        resolve(true)
       })
     })
-    .catch((err) => console.log(err))
-}
+  })
 
-if (!process.argv[2] || !process.argv[3]) {
-  throw new Error('Source or destination path not given!')
-}
+diographJson
+  .load()
+  .then(() =>
+    Promise.all(
+      diographJson.search('ImageObject', 'data').map((diory) => {
+        // contentUrl must exist in order to make a thumbnail
+        if (!(diory.data && diory.data[0] && diory.data[0].contentUrl)) return null
 
-resizeAndSaveImage({
-  sourceFilePath: process.argv[2] || 'public/test-image.png',
-  destinationFilePath: process.argv[3] || 'test.jpg',
-})
+        // // image must be null or start with data to verify that thumbnail doesn't yet exist
+        // if (diory.image && !/^data\:image\/png\;base64/.exec(diory.image)) return null
+
+        // TODO: Sanitize file url to absolute path (REMOVE THIS HORRORNESS!)
+        let sanitisedContentUrl
+        const { contentUrl } = diory.data[0]
+        sanitisedContentUrl = contentUrl.replace(/%20/g, ' ')
+        sanitisedContentUrl = sanitisedContentUrl.replace(/a%CC%88/g, 'ä')
+        sanitisedContentUrl = sanitisedContentUrl.replace(/o%CC%88/g, 'ö')
+
+        const dioryThumbnailPath = join(relativeThumbnailPath, `${diory.id}.jpg`)
+        const params = {
+          sourceFilePath: join(diographFolderPath, sanitisedContentUrl),
+          destinationFilePath: join(diographFolderPath, dioryThumbnailPath),
+        }
+
+        return resizeAndSaveImage(params).then(() => {
+          const updatedDiory = diographJson.update(diory.id, {
+            image: dioryThumbnailPath,
+          })
+          console.log(updatedDiory)
+        })
+      })
+    )
+  )
+  .then(() => {
+    diographJson.save()
+  })
