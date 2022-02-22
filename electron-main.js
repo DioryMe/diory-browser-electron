0a17e7fbffa3614eb2d82a3ca740816ff1c0e9e1
Jouni Alanen <jouni@diory.me>
2022-02-22 09:03:03 +0200

    DDA-228: Small code to verify diograph-js installation

--- electron-main.js
+++ electron-main.js
@@ -10,6 +10,14 @@ require('electron-reload')
 const log = require('electron-log')
 console.log = log.info
 
+// Testing diograph-js integration
+// eslint-disable-next-line import/newline-after-import
+const { DiographJson } = require('diograph-js')
+const dio = new DiographJson({ baseUrl: './public/diory-demo-content' })
+dio.loadDiograph().then(() => {
+  console.log(dio.getDiory('generic-content'))
+})
+
 const ffmpegDir = path
   .dirname(require.resolve('ffmpeg-static'))
   .replace('app.asar', 'app.asar.unpacked')
