8536729af5519cb7ded683add7827733b546c0fe
OP <op@diory.me>
2022-02-22 22:35:30 +0200

    DDA-226: Return correct channel name from client mock

--- src/react/client/client.mock.js
+++ src/react/client/client.mock.js
@@ -1,8 +1,10 @@
 import GET_DIORY_FOLDER_LOCATION from './mockResponses/GET_DIORY_FOLDER_LOCATION.json'
 import GET_DIOGRAPH from './mockResponses/GET_DIOGRAPH.json'
+import { channels } from '../../shared/constants'
 
 export const mockResponse = (channel) => {
   const data = {
+    ...channels,
     GET_DIOGRAPH,
     GET_DIORY_FOLDER_LOCATION,
   }[channel]
