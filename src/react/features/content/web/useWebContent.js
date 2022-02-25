9ae1da56e8c72093520e084db2ebca78cd27e2f2
OP <op@diory.me>
2022-02-25 18:44:15 +0200

    TECH: Prettier

--- src/react/features/content/web/useWebContent.js
+++ src/react/features/content/web/useWebContent.js
@@ -6,17 +6,19 @@ import { channels } from '../../../../shared/constants'
 
 export const BROWSER_BUTTON = 'BROWSER_BUTTON'
 
-const buttons = [{
-  id: BROWSER_BUTTON,
-  text: 'Browser',
-  data: {
-    order: 0,
-    type: 'content',
-    icon: 'globe-network',
-    testid: 'browser',
-    visible: true,
+const buttons = [
+  {
+    id: BROWSER_BUTTON,
+    text: 'Browser',
+    data: {
+      order: 0,
+      type: 'content',
+      icon: 'globe-network',
+      testid: 'browser',
+      visible: true,
+    },
   },
-}]
+]
 
 const openInBrowser = (url) => () => invokeChannel(channels.OPEN_IN_BROWSER, url)
 
