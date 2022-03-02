98257c640466f79b37133466fd0098c38f7cf001
Jouni Alanen <jouni@diory.me>
2022-03-02 09:10:48 +0200

    DDA-228: Move testing-diograph-js-integration.js file to electron/ folder so it will be included to Electron binary

--- electron/testing-diograph-js-integration.js
+++ electron/testing-diograph-js-integration.js
@@ -1,6 +1,6 @@
 // Testing diograph-js integration
 // eslint-disable-next-line import/newline-after-import
-const { DiographJson } = require('diograph-js')
+const { DiographJson } = require('../diograph-js')
 const dio = new DiographJson({ baseUrl: './public/diory-demo-content' })
 dio.loadDiograph().then(() => {
   console.log(dio.getDiory('generic-content'))
