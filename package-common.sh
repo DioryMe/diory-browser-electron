1397d1ed6bd4e5f5c0e0a0742a67d38d75a41a06
Jouni Alanen <jouni@diory.me>
2022-03-02 08:55:06 +0200

    DDA-228: Install missing sharp-package binary for x64 / Intel platform

--- package-common.sh
+++ package-common.sh
@@ -1,7 +1,12 @@
 # !/bin/bash
 
 echo "Installing dependencies and building the app..."
-yarn && yarn build && yarn build-electron
+yarn
+# Build missing sharp binaries
+# - NOTE: This assumes that release is done on arm64 / M1 Mac (on Intel replace x64 with arm64)
+# - TODO: Make condition which detects used architecture and installs missing sharp binaries accordingly
+SHARP_IGNORE_GLOBAL_LIBVIPS=1 npm_config_arch=x64 npm_config_platform=darwin yarn add sharp
+yarn build && yarn build-electron
 
 echo "Add APP_PACKAGE_ENVS from electron-main.js"
 export APP_PACKAGE_ENVS="process.env.BINARY_BUILD=1";
