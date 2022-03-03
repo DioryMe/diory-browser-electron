271daa5656229d0d5b4124db4e355f4b6a8e3b47
Jouni Alanen <jouni@diory.me>
2022-03-03 09:39:23 +0200

    DDA-228: Copy diory-demo-content diograph.json to /electron - clean up testing-diograph-js-integration.js

--- electron/testing-diograph-js-integration.js
+++ electron/testing-diograph-js-integration.js
@@ -1,7 +1,11 @@
-// Testing diograph-js integration
-// eslint-disable-next-line import/newline-after-import
 const { DiographJson } = require('../diograph-js')
-const dio = new DiographJson({ baseUrl: './public/diory-demo-content' })
-dio.loadDiograph().then(() => {
-  console.log(dio.getDiory('generic-content'))
-})
+
+const dio = new DiographJson({ baseUrl: __dirname })
+dio.loadDiograph().then(
+  () => {
+    console.log('Generic content diory from test content:', dio.getDiory('generic-content'))
+  },
+  (e) => {
+    console.log(e)
+  }
+)
