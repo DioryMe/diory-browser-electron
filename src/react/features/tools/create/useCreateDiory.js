375c45debf0c6548d6ca14276d07eb48aaae6d7e
OP <op@diory.me>
2022-05-03 14:25:27 +0300

    DDA-246: Refactor import tools

--- src/react/features/tools/create/useCreateDiory.js
+++ src/react/features/tools/create/useCreateDiory.js
@@ -7,16 +7,20 @@ import { inactivateButton } from '../../buttons/buttonsActions'
 import { addDioryToHand, deselectTool } from '../toolsActions'
 import { createDiory, createLink } from '../../diograph/diographActions'
 
+import { CREATE_TOOL } from './buttons'
+
 export const useCreateDiory = () => {
   const { story } = useDiograph()
   const { dispatch } = useDispatchActions()
 
   return (newDiory) => {
-    const id = uuid()
-    dispatch(createDiory({ ...newDiory, id }))
-    dispatch(createLink(story, { id }))
-    dispatch(inactivateButton())
-    dispatch(deselectTool())
-    dispatch(addDioryToHand(id))
+    if (CREATE_TOOL === active) {
+      const id = uuid()
+      dispatch(createDiory({ ...newDiory, id }))
+      dispatch(createLink(story, { id }))
+      dispatch(inactivateButton())
+      dispatch(deselectTool())
+      dispatch(addDioryToHand(id))
+    }
   }
 }
