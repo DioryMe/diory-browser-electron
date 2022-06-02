4935b15bfc469d908df32bd51fff7fdf09cf18a3
Jouni Alanen <jouni@diory.me>
2022-06-02 12:59:38 +0300

    POC-000: Ripped off all the diographReducers, Actions and ActionTypes * Only setRoom & setStory

--- src/react/features/diograph/diographActionTypes.js
+++ src/react/features/diograph/diographActionTypes.js
@@ -1,11 +1,2 @@
-export const GET_DIOGRAPH = 'GET_DIOGRAPH'
-export const SAVE_DIOGRAPH = 'SAVE_DIOGRAPH'
-export const ADD_DIOGRAPH = 'ADD_DIOGRAPH'
-
-export const CREATE_DIORY = 'CREATE_DIORY'
-export const UPDATE_DIORY = 'UPDATE_DIORY'
-export const DELETE_DIORY = 'DELETE_DIORY'
-
-export const CREATE_LINK = 'CREATE_LINK'
-export const DELETE_LINK = 'DELETE_LINK'
-export const DELETE_LINKS = 'DELETE_LINKS'
+export const SET_ROOM = 'SET_ROOM'
+export const SET_STORY = 'SET_STORY'
--- src/react/features/diograph/diographReducer.js
+++ src/react/features/diograph/diographReducer.js
@@ -1,126 +1,37 @@
-import {
-  GET_DIOGRAPH,
-  SAVE_DIOGRAPH,
-  ADD_DIOGRAPH,
-  CREATE_DIORY,
-  CREATE_LINK,
-  DELETE_DIORY,
-  DELETE_LINK,
-  DELETE_LINKS,
-  UPDATE_DIORY,
-} from './diographActionTypes'
-import { promiseReducers, createReducer } from '../../store'
+import { SET_ROOM, SET_STORY } from './diographActionTypes'
+import { /* promiseReducers , */ createReducer } from '../../store'
 
 const initialState = {
-  rootId: undefined,
-  diograph: {},
-  loaded: false,
-  updated: false,
+  room: null,
+  story: null,
+  memories: [],
 }
 
-export const getDiograph = (state, { payload: { diograph, rootId } }) => ({
+export const setRoom = (state, { payload: { room } }) => ({
   ...state,
-  rootId,
-  diograph,
-  loaded: true,
-  updated: false,
+  room,
 })
 
-export const addDiograph = (state, { payload: { diograph } }) => ({
-  ...state,
-  diograph: {
-    ...state.diograph,
-    ...diograph,
-  },
-  updated: true,
-})
-
-export const createDiory = (state, { payload: { diory } }) => ({
-  ...state,
-  diograph: {
-    ...state.diograph,
-    [diory.id]: diory,
-  },
-  updated: true,
-})
-
-export const deleteDiory = (state, { payload: { diory } }) => {
-  // eslint-disable-next-line no-unused-vars
-  const { [diory.id]: omit, ...diograph } = state.diograph
+export const setStory = (state, { payload: { storyId } }) => {
+  const story = state.room.diograph.getDiory(storyId)
+  const memories = state.room.diograph.getDioryWithLinks()
   return {
     ...state,
-    diograph,
-    updated: true,
+    story,
+    memories,
   }
 }
-
-const updateDiory = (state, { payload }) => ({
-  ...state,
-  diograph: {
-    ...state.diograph,
-    [payload.diory.id]: {
-      ...state.diograph[payload.diory.id],
-      ...payload.diory,
-    },
-  },
-  updated: true,
-})
-
-const createLink = (state, { payload }) => {
-  const diory = state.diograph[payload.diory.id]
-  return {
-    ...state,
-    diograph: {
-      ...state.diograph,
-      [payload.diory.id]: {
-        ...diory,
-        ...payload.diory,
-        links: {
-          ...diory.links,
-          [payload.link.id]: { id: payload.link.id },
-        },
-      },
-    },
-    updated: true,
-  }
-}
-
-/**
- * payload.diory.id = diory where link should be removed
- * payload.linkedDiory.id =  diory where link is directed
- */
-export const deleteLink = (state, { payload: { fromDiory, toDiory } }) => {
-  const dioryLinks = state.diograph[fromDiory.id].links
-  const linkKey = Object.entries(dioryLinks).filter(([, { id }]) => id === toDiory.id)[0][0]
-  // eslint-disable-next-line no-unused-vars
-  const { [linkKey]: omit, ...links } = dioryLinks
-  return {
-    ...state,
-    diograph: {
-      ...state.diograph,
-      [fromDiory.id]: {
-        ...state.diograph[fromDiory.id],
-        links,
-      },
-    },
-    updated: true,
-  }
-}
-
-export const deleteLinks = (state, { payload: deletedLinks }) =>
-  deletedLinks.reduce(
-    (tempState, deletedLink) => deleteLink(tempState, { payload: deletedLink }),
-    state
-  )
 
 export default createReducer(initialState, {
-  [ADD_DIOGRAPH]: addDiograph,
-  [CREATE_DIORY]: createDiory,
-  [DELETE_DIORY]: deleteDiory,
-  [UPDATE_DIORY]: updateDiory,
-  [CREATE_LINK]: createLink,
-  [DELETE_LINK]: deleteLink,
-  [DELETE_LINKS]: deleteLinks,
-  [GET_DIOGRAPH]: getDiograph,
-  ...promiseReducers(SAVE_DIOGRAPH, 'updated', 'saving', 'saved', 'error'),
+  [SET_ROOM]: setRoom,
+  [SET_STORY]: setStory,
+  // [ADD_DIOGRAPH]: addDiograph,
+  // [CREATE_DIORY]: createDiory,
+  // [DELETE_DIORY]: deleteDiory,
+  // [UPDATE_DIORY]: updateDiory,
+  // [CREATE_LINK]: createLink,
+  // [DELETE_LINK]: deleteLink,
+  // [DELETE_LINKS]: deleteLinks,
+  // [GET_DIOGRAPH]: getDiograph,
+  // ...promiseReducers(SAVE_DIOGRAPH, 'updated', 'saving', 'saved', 'error'),
 })
