e0859c89bf55542558ca596aae6248364b1245d7
OP <op@diory.me>
2022-06-03 12:22:45 +0300

    DDA-247: Move get diograph logic to diograph client

--- src/react/clients/diograph/diographClient.mock.js
+++ src/react/clients/diograph/diographClient.mock.js
@@ -1,4 +1,14 @@
+import GET_DIOGRAPH from '../../client/mockResponses/GET_DIOGRAPH.json'
+
+export function getDiograph() {
+  const mockResponse = GET_DIOGRAPH
+  console.log('mock: getDiograph', mockResponse)
+  return mockResponse
+}
+
 export function saveDiograph() {
-  console.log('saveDiograph: mock response: true')
-  return true
+  const mockResponse = true
+
+  console.log('mock: saveDiograph', mockResponse)
+  return mockResponse
 }
