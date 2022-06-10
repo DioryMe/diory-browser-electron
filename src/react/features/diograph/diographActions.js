deaa46f2edb0724fabb349aadb8a58d05096924e
OP <op@diory.me>
2022-06-11 00:13:23 +0300

    DDA-247: Use diograph in diograph actions

--- src/react/features/diograph/diographActions.js
+++ src/react/features/diograph/diographActions.js
@@ -5,30 +5,10 @@ import {
   SAVE_DIOGRAPH_BEGIN,
   SAVE_DIOGRAPH_SUCCESS,
   SAVE_DIOGRAPH_FAILURE,
-  ADD_DIOGRAPH,
-  CREATE_DIORY,
-  UPDATE_DIORY,
-  DELETE_DIORY,
+  UPDATE_DIOGRAPH,
 } from './diographActionTypes'
 
 import { selectStory } from '../navigation/navigationActions'
-
-export const getDiograph =
-  () =>
-  async (dispatch, getState, { client }) => {
-    const { saving } = getState().diograph
-    if (!saving) {
-      dispatch(getDiographBegin())
-      try {
-        const { dioryFolderLocation } = getState().settings
-        const { diograph, rootId } = await client.getDiograph(dioryFolderLocation)
-        dispatch(getDiographSuccess(diograph, rootId))
-        dispatch(selectStory({ id: rootId }))
-      } catch (error) {
-        dispatch(getDiographFailure(error))
-      }
-    }
-  }
 
 export const getDiographBegin = () => ({
   type: GET_DIOGRAPH_BEGIN,
@@ -44,23 +24,19 @@ export const getDiographFailure = (error) => ({
   payload: { error },
 })
 
-export const saveDiograph =
+export const getDiograph =
   () =>
   async (dispatch, getState, { client }) => {
     const { saving } = getState().diograph
     if (!saving) {
-      dispatch(saveDiographBegin())
+      dispatch(getDiographBegin())
       try {
         const { dioryFolderLocation } = getState().settings
-        const { rootId, diograph } = getState().diograph
-        await client.saveDiograph({
-          dioryFolderLocation,
-          rootId,
-          diograph,
-        })
-        dispatch(saveDiographSuccess())
+        const { diograph, rootId } = await client.getDiograph({ dioryFolderLocation })
+        dispatch(getDiographSuccess(diograph, rootId))
+        dispatch(selectStory({ id: rootId }))
       } catch (error) {
-        dispatch(saveDiographFailure(error))
+        dispatch(getDiographFailure(error))
       }
     }
   }
@@ -78,73 +54,74 @@ export const saveDiographFailure = (error) => ({
   payload: { error },
 })
 
-export const addDiograph = (diograph) => ({
-  type: ADD_DIOGRAPH,
+export const saveDiograph =
+  () =>
+  async (dispatch, getState, { client }) => {
+    const { saving } = getState().diograph
+    if (!saving) {
+      dispatch(saveDiographBegin())
+      try {
+        const { dioryFolderLocation } = getState().settings
+        await client.saveDiograph({ dioryFolderLocation })
+        dispatch(saveDiographSuccess())
+      } catch (error) {
+        dispatch(saveDiographFailure(error))
+      }
+    }
+  }
+
+export const updateDiograph = (diograph) => ({
+  type: UPDATE_DIOGRAPH,
   payload: { diograph },
 })
+
+export const addDiograph =
+  (diographData) =>
+  (dispatch, _, { client }) => {
+    const diograph = client.addDiograph(diographData)
+    dispatch(updateDiograph(diograph))
+  }
 
 export const createDiory =
   (dioryData) =>
   (dispatch, _, { client }) => {
-    const { diory } = client.createDiory(dioryData)
-    dispatch({
-      type: CREATE_DIORY,
-      payload: { diory },
-    })
+    const diograph = client.createDiory(dioryData)
+    dispatch(updateDiograph(diograph))
   }
 
 export const updateDiory =
   (dioryData) =>
   (dispatch, getState, { client }) => {
-    const { diograph } = getState().diograph
-    const { diory } = client.updateDiory(dioryData, diograph)
-    dispatch({
-      type: UPDATE_DIORY,
-      payload: { diory },
-    })
+    const diograph = client.updateDiory(dioryData)
+    dispatch(updateDiograph(diograph))
   }
 
 export const deleteDiory =
   (dioryData) =>
   (dispatch, _, { client }) => {
-    const { diory } = client.deleteDiory(dioryData)
-    dispatch({
-      type: DELETE_DIORY,
-      payload: { diory },
-    })
+    const diograph = client.deleteDiory(dioryData)
+    dispatch(updateDiograph(diograph))
   }
 
 export const createLink =
   (dioryData, linkData) =>
   (dispatch, getState, { client }) => {
-    const { diograph } = getState().diograph
-    const { diory } = client.createLink(dioryData.id, linkData.id, diograph)
-    dispatch({
-      type: UPDATE_DIORY,
-      payload: { diory },
-    })
+    const diograph = client.createLink(dioryData.id, linkData.id)
+    dispatch(updateDiograph(diograph))
   }
 
 export const deleteLink =
   (from, to) =>
   (dispatch, getState, { client }) => {
-    const { diograph } = getState().diograph
-    const { diory } = client.deleteLink(from.id, to.id, diograph)
-    dispatch({
-      type: UPDATE_DIORY,
-      payload: { diory },
-    })
+    const diograph = client.deleteLink(from.id, to.id)
+    dispatch(updateDiograph(diograph))
   }
 
 export const deleteLinks =
   (deletedLinks) =>
   (dispatch, getState, { client }) => {
-    const { diograph } = getState().diograph
     deletedLinks.forEach(({ fromDiory, toDiory }) => {
-      const { diory } = client.deleteLink(fromDiory.id, toDiory.id, diograph)
-      dispatch({
-        type: UPDATE_DIORY,
-        payload: { diory },
-      })
+      const diograph = client.deleteLink(fromDiory.id, toDiory.id)
+      dispatch(updateDiograph(diograph))
     })
   }
