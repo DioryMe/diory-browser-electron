1171287d07585d057931aace755ce4824c17c76a
OP <op@diory.me>
2022-06-11 00:13:23 +0300

    DDA-247: Use diograph in diograph actions

--- src/react/features/diograph/diographReducer.js
+++ src/react/features/diograph/diographReducer.js
@@ -1,11 +1,4 @@
-import {
-  GET_DIOGRAPH_SUCCESS,
-  SAVE_DIOGRAPH,
-  ADD_DIOGRAPH,
-  CREATE_DIORY,
-  DELETE_DIORY,
-  UPDATE_DIORY,
-} from './diographActionTypes'
+import { GET_DIOGRAPH_SUCCESS, SAVE_DIOGRAPH, UPDATE_DIOGRAPH } from './diographActionTypes'
 import { promiseReducers, createReducer } from '../../store'
 
 const initialState = {
@@ -23,48 +16,14 @@ export const getDiographSuccess = (state, { payload: { diograph, rootId } }) => 
   updated: false,
 })
 
-export const addDiograph = (state, { payload: { diograph } }) => ({
+export const updateDiograph = (state, { payload: { diograph } }) => ({
   ...state,
-  diograph: {
-    ...state.diograph,
-    ...diograph,
-  },
-  updated: true,
-})
-
-export const createDiory = (state, { payload }) => ({
-  ...state,
-  diograph: {
-    ...state.diograph,
-    [payload.diory.id]: payload.diory,
-  },
-  updated: true,
-})
-
-export const deleteDiory = (state, { payload: { diory } }) => {
-  // eslint-disable-next-line no-unused-vars
-  const { [diory.id]: omit, ...diograph } = state.diograph
-  return {
-    ...state,
-    diograph,
-    updated: true,
-  }
-}
-
-const updateDiory = (state, { payload }) => ({
-  ...state,
-  diograph: {
-    ...state.diograph,
-    [payload.diory.id]: payload.diory,
-  },
+  diograph,
   updated: true,
 })
 
 export default createReducer(initialState, {
-  [ADD_DIOGRAPH]: addDiograph,
+  [UPDATE_DIOGRAPH]: updateDiograph,
   [GET_DIOGRAPH_SUCCESS]: getDiographSuccess,
-  [CREATE_DIORY]: createDiory,
-  [DELETE_DIORY]: deleteDiory,
-  [UPDATE_DIORY]: updateDiory,
   ...promiseReducers(SAVE_DIOGRAPH, 'updated', 'saving', 'saved', 'error'),
 })
