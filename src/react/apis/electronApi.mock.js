33259d321a81210a2c7ed9b6cf540500b9d8bbb8
OP <op@diory.me>
2022-06-11 00:14:26 +0300

    DDA-247: Store diograph to client

--- src/react/apis/electronApi.mock.js
+++ src/react/apis/electronApi.mock.js
@@ -1,14 +1,15 @@
-import GET_DIOGRAPH from '../../client/mockResponses/GET_DIOGRAPH.json'
+import GET_DIOGRAPH from '../client/mockResponses/GET_DIOGRAPH.json'
 
 export function getDiograph() {
   const mockResponse = GET_DIOGRAPH
   console.log('mock: getDiograph', mockResponse)
+
   return mockResponse
 }
 
 export function saveDiograph() {
   const mockResponse = true
-
   console.log('mock: saveDiograph', mockResponse)
+
   return mockResponse
 }
