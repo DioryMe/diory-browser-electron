7fbb2c0ba13bffd58a833937a7ff9178efacf983
Jouni Alanen <jouni@diory.me>
2022-06-23 12:21:21 +0300

    Revert "DDA-246: Replace deprecated ReactDOM.render with root.render"
    This reverts commit 72ce1ee1383a9333e84ec9408fa0d39496363c00.

--- src/index.js
+++ src/index.js
@@ -3,5 +3,4 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './react/App'
 
-const root = ReactDOM.createRoot(document.querySelector('#root'))
-root.render(<App />)
+ReactDOM.render(<App />, document.getElementById('root'))
