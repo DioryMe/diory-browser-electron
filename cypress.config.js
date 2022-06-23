a0c7181616a2cfe1217af76f056f299a3798aa97
Jouni Alanen <jouni@diory.me>
2022-06-23 12:27:37 +0300

    DDA-246: Fix lint

--- cypress.config.js
+++ cypress.config.js
@@ -16,7 +16,7 @@ module.exports = defineConfig({
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./src/react/utils/cypress/plugins/index.js')(on, config)
+      return require('./src/react/utils/cypress/plugins')(on, config)
     },
     baseUrl: 'http://localhost:3300',
     specPattern: 'src/react/**/*.feature',
