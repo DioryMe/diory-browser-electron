10914ce859078ffde33fa9b95f48439339cfecf9
Jouni Alanen <jouni@diory.me>
2022-06-23 11:56:01 +0300

    DDA-246: Replace deprecated ReactDOM.render with root.render

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './react/App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const root = ReactDOM.createRoot(document.querySelector('#root'))
+root.render(<App />)
