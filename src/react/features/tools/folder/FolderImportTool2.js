eb8c9b9f2049b3a54a73927ebe9234a2dafe515e
Jouni Alanen <jouni@diory.me>
2022-06-24 08:51:49 +0300

    DDA-254: onDiographChange: add diograph & rootId to tool-store => adding new diories to diograph works

--- src/react/features/tools/folder/FolderImportTool2.js
+++ src/react/features/tools/folder/FolderImportTool2.js
@@ -5,10 +5,11 @@ import { useDispatchActions, useSelector } from '../../../store'
 
 import { inactivateButton } from '../../buttons/buttonsActions'
 import { addDiograph, createLink } from '../../diograph/diographActions'
-import { deselectTool, selectFolderPath, generateDiograph } from '../toolsActions'
+import { deselectTool, addDiograph as toolAddDiograph, selectStory } from '../toolsActions'
 
 import ImportView from '../import/ImportView'
 import GridLens2 from '../../lenses/grid/GridLens2'
+import Fullscreen from '../../../components/Fullscreen'
 
 const selectContentSourceAddress = async () => {
   return window.channelsApi.showOpenDialog().then(({ filePaths }) => filePaths[0])
@@ -41,9 +42,12 @@ export const useAddSelectedDiorys = () => {
 
 const FolderImportTool2 = () => {
   const [connection, setConnection] = useState(null)
+  const { dispatch } = useDispatchActions()
 
   const onDiographChange = () => {
-    console.log('Connection diograph changed')
+    console.log('Connection diograph changed', connection.diograph.toObject())
+    dispatch(toolAddDiograph(connection.diograph.toObject()))
+    dispatch(selectStory('/'))
   }
 
   useEffect(() => {
@@ -70,7 +74,9 @@ const FolderImportTool2 = () => {
   return (
     connection && (
       <ImportView onDone={addSelectedDiorys} onCancel={resetView}>
-        <GridLens2 connection={connection} onDiographChange={onDiographChange} />
+        <Fullscreen>
+          <GridLens2 connection={connection} onDiographChange={onDiographChange} />
+        </Fullscreen>
       </ImportView>
     )
   )
