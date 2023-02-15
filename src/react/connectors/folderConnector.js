267f0f9cd9c563f2ebaf58a6367d6a6dd7bbf70f
OP <op@diory.me>
2023-02-15 22:22:14 +0200

    DDA-257: Rename settings to room

--- src/react/connectors/folderConnector.js
+++ src/react/connectors/folderConnector.js
@@ -1,1 +1,5 @@
-export default window.folderConnector
+import * as mockConnector from './mockConnector'
+
+export function createFolderConnector() {
+  return process.env.NODE_ENV === 'development' ? mockConnector : window.folderConnector
+}
