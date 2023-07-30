da0e6adedfae6e6d01b059f1a636d87a6e21b571
Olli-Pekka Pohjola <olli-pekka.pohjola.ext@konecranes.com>
2023-07-30 21:55:07 +0300

    DDA-259: Add lenses to sidebar

--- src/react/features/sideBar/SideBarNavigation.js
+++ src/react/features/sideBar/SideBarNavigation.js
@@ -1,31 +1,12 @@
 import React from 'react'
-import { IconButton, Pane } from 'evergreen-ui'
 
-import { useSideBar } from './useSideBar'
+import NavigationTopBar from '../../components/NavigationTopBar'
+import LensesNavigation from '../lenses/LensesNavigation'
 
-import Icon from '../../components/Icon'
-
-const SideBarNavigation = () => {
-  const { showSideBar, toggleSideBar } = useSideBar()
-  return (
-    <Pane position="absolute" top={0} right={0} zIndex={100} padding={4}>
-      {showSideBar ? (
-        <IconButton
-          appearance="minimal"
-          icon={<Icon icon="chevron-left" size={24} />}
-          onClick={toggleSideBar}
-          data-testid="toggleSideBar"
-        />
-      ) : (
-        <IconButton
-          appearance="minimal"
-          icon={<Icon icon="chevron-right" size={24} color="disabled" />}
-          onClick={toggleSideBar}
-          data-testid="toggleSideBar"
-        />
-      )}
-    </Pane>
-  )
-}
+const SideBarNavigation = () => (
+  <NavigationTopBar>
+    <LensesNavigation />
+  </NavigationTopBar>
+)
 
 export default SideBarNavigation
