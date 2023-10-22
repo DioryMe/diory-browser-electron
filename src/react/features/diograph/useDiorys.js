2c815a0d0540f6725a306ffc87e1130022cb7c31
Olli-Pekka Pohjola <olli-pekka.pohjola.ext@konecranes.com>
2023-10-22 19:30:06 +0300

    DDA-259: Use room connections in diograph

--- src/react/features/diograph/useDiorys.js
+++
@@ -1,8 +0,0 @@
-import { useSelector } from '../../store'
-
-export const useDiorys = (ids = {}) => {
-  const { diograph = {} } = useSelector((state) => state.diograph)
-  return Object.entries(ids)
-    .map(([key, { id }]) => ({ key, ...diograph[id] }))
-    .filter(({ id }) => id)
-}
