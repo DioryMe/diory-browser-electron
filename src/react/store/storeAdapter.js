297f6a0b72d39f4e8f71d2e56f140de92630eb0c
Jouni Alanen <jouni@diory.me>
2024-05-01 21:52:42 +0300

    All this kind of works
    - S3Client can be used and loads
    - everything works as before if comment out S3stuff from #initialise
    - validation
    - demo-content-room diograph.json

--- src/react/store/storeAdapter.js
+++ src/react/store/storeAdapter.js
@@ -1,3 +1,7 @@
+import { S3Client } from '@diograph/s3-client'
+import { constructAndLoadRoom } from '@diograph/utils'
+import { validateDiograph } from './validateDiograph'
+
 class StoreAdapter {
   constructor(client) {
     this.dioryClient = client
@@ -17,6 +21,36 @@ class StoreAdapter {
     this.diograph = this.dioryClient.diograph
     this.room = this.dioryClient.room
     this.diory = this.dioryClient.diory
+
+    // Validate
+    validateDiograph(this.dioryClient.diograph.toObject())
+
+    // --------
+
+    // const roomClientType = 'S3Client'
+    // const address = 's3://jvalanen-diory-test3/room/'
+    // const credentials = {
+    //   region: 'eu-west-1',
+    //   credentials: {
+    //     accessKeyId: '',
+    //     secretAccessKey: '',
+    //   },
+    // }
+    // const room = await constructAndLoadRoom(address, roomClientType, {
+    //   S3Client: {
+    //     clientConstructor: S3Client,
+    //     credentials,
+    //   },
+    // })
+
+    // room.diograph.initialise = () => {}
+    // this.dioryClient.diograph = room.diograph
+    // this.diograph = room.diograph
+
+    // // Validate
+    // validateDiograph(this.dioryClient.diograph.toObject())
+
+    // console.log('bau', this.dioryClient.diograph.toObject())
   }
 
   enterRoom = async (param) => {
---
+++ src/react/store/validateDiograph.js
@@ -0,0 +1,78 @@
+// eslint-disable-next-line import/no-unresolved, no-unused-vars
+// import { validateDiograph } from '@diograph/diograph/validator'
+
+import Ajv from 'ajv'
+
+const ajv = new Ajv({ allErrors: true })
+
+const validate = (schema, objectToValidate) => {
+  const validate = ajv.compile(schema)
+
+  const isValid = validate(objectToValidate)
+
+  if (!isValid) {
+    console.log(`Object is not valid based on schema: ${JSON.stringify(validate.errors)}`)
+    throw new Error(`Object is not valid based on schema: ${JSON.stringify(validate.errors)}`)
+  }
+}
+
+const diorySchema = {
+  $schema: 'http://json-schema.org/draft-07/schema#',
+  type: 'object',
+  additionalProperties: false,
+  required: ['id', 'created', 'modified'],
+  properties: {
+    id: { type: 'string' },
+    text: { type: 'string' },
+    date: { type: 'string' /* format: 'date-time' */ },
+    latlng: { type: 'string' /* format: 'geolocation' */ },
+    image: { type: 'string' },
+    modified: { type: 'string' /* format: 'date-time' */ },
+    created: { type: 'string' /* format: 'date-time' */ },
+    links: {
+      type: 'array',
+      items: {
+        type: 'object',
+        properties: {
+          id: { type: 'string' },
+          path: { type: 'string' },
+        },
+        required: ['id', 'path'],
+        additionalProperties: false,
+      },
+    },
+    data: {
+      type: 'array',
+      items: {
+        type: 'object',
+        additionalProperties: false,
+        required: ['@context', '@type', 'contentUrl', 'encodingFormat'],
+        properties: {
+          '@context': { type: 'string' },
+          '@type': { type: 'string' },
+          contentUrl: { type: 'string' },
+          encodingFormat: { type: 'string' },
+          height: { type: 'number' },
+          width: { type: 'number' },
+          duration: { type: 'string' },
+        },
+      },
+    },
+  },
+}
+
+// eslint-disable-next-line import/no-unresolved, no-unused-vars
+const validateDiory = (dioryObject) => {
+  validate(diorySchema, dioryObject)
+}
+
+const diographSchema = {
+  $schema: 'http://json-schema.org/draft-07/schema#',
+  type: 'object',
+  additionalProperties: diorySchema,
+  required: ['/'],
+}
+
+export const validateDiograph = (diographObject) => {
+  validate(diographSchema, diographObject)
+}
